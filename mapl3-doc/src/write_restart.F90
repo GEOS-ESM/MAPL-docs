0e92ee0d5223e3c1f37891da5c6a3b53806f350c
tclune <thomas.l.clune@nasa.gov>
2025-07-08 20:04:56 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@071feb6e61ead3056c10b73c0f04306aac6dda37 🚀

--- mapl3-doc/src/write_restart.F90
+++ mapl3-doc/src/write_restart.F90
@@ -1,48 +1,33 @@
-#include "MAPL_Generic.h"
+#include "MAPL_ErrLog.h"
 
-submodule (mapl3g_OuterMetaComponent) write_restart_smod
-   use mapl3g_RestartHandler
-   use mapl_ErrorHandling
-   implicit none (type, external)
+submodule(mapl3g_GriddedComponentDriver) write_restart_smod
+   use :: mapl_ErrorHandling
+   use :: mapl3g_OuterMetaComponent
+   use :: mapl3g_MethodPhasesMapUtils
+   implicit none
 
 contains
 
-   module recursive subroutine write_restart(this, importState, exportState, clock, unusable, rc)
-      class(OuterMetaComponent), target, intent(inout) :: this
-      type(ESMF_State) :: importState
-      type(ESMF_State) :: exportState
-      type(ESMF_Clock) :: clock
-      ! optional arguments
+   module recursive subroutine write_restart(this, unusable, phase_idx, rc)
+      class(GriddedComponentDriver), intent(inout) :: this
       class(KE), optional, intent(in) :: unusable
+      integer, optional, intent(in) :: phase_idx
       integer, optional, intent(out) :: rc
 
-      ! Locals
-      type(GriddedComponentDriver), pointer :: driver
-      character(:), allocatable :: name
-      type(ESMF_State) :: internal_state
-      type(ESMF_Geom) :: geom
-      type(RestartHandler) :: restart_handler
-      integer :: status
+      integer :: status, user_status
 
-      driver => this%get_user_gc_driver()
-      name = driver%get_name()
-      if (this%has_geom()) then
-         geom = this%get_geom()
-         restart_handler = RestartHandler(name, geom, clock, _RC)
-         call restart_handler%write("import", importState, _RC)
-         internal_state = this%get_internal_state()
-         call restart_handler%write("internal", internal_state, _RC)
-         if (this%component_spec%misc%write_exports) then
-            call restart_handler%write("export", exportState, _RC)
-         end if
-      end if
-      if (name /= "HIST") then
-         call recurse_write_restart_(this, _RC)
-      end if
+      associate ( &
+           importState => this%states%importState, &
+           exportState => this%states%exportState)
 
-      _RETURN(ESMF_SUCCESS)
+        call ESMF_GridCompWriteRestart(this%gridcomp, &
+             importState=importState, exportState=exportState, clock=this%clock, &
+             phase=phase_idx, _USERRC)
+
+      end associate
+
+      _RETURN(_SUCCESS)
       _UNUSED_DUMMY(unusable)
-      _UNUSED_DUMMY(exportState)
    end subroutine write_restart
 
 end submodule write_restart_smod
