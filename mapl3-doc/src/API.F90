fc4b8b6475bac191eb014bb9253daebd0e703117
pchakraborty <pchakraborty@users.noreply.github.com>
2025-07-28 12:54:16 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@8f0b5051f1e2e8c441ce7ca3afbf6b0007bd0f41 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,12 +1,40 @@
-module mapl3g_Field_API
-   use mapl3g_FieldGet, only: MAPL_FieldGet => FieldGet
-   use mapl3g_FieldSet, only: MAPL_FieldSet => FieldSet
-   use mapl3g_FieldCreate
-   use mapl3g_VerticalStaggerLoc
-   ! Internal info should not be exposed to users
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetShared
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetShared
+module mapl3g_Geom_API
 
-end module mapl3g_Field_API
+   use mapl_KeywordEnforcer
+   use mapl3g_MaplGeom, only: MaplGeom
+   use mapl3g_GeomSpec, only: GeomSpec
+   use mapl3g_GeomManager, only: GeomManager, geom_manager, get_geom_manager
+   use mapl3g_GeomUtilities, only: MAPL_SameGeom, MAPL_GeomGetId
+   use esmf, only: ESMF_Grid, ESMF_KIND_R4
+
+   implicit none(type,external)
+
+   private
+
+   ! Available to users
+   public :: MAPL_GridGet
+
+   ! Used internally by MAPL
+   ! Users shouldn't need these
+   public :: MaplGeom
+   public :: MAPL_SameGeom, MAPL_GeomGetId
+   public :: GeomManager, geom_manager, get_geom_manager
+   public :: GeomSpec
+
+   interface MAPL_GridGet
+      procedure ::  grid_get
+   end interface MAPL_GridGet
+
+   interface
+      module subroutine grid_get(grid, unusable, im, jm, latitudes, longitudes, rc)
+         type(ESMF_Grid), intent(in) :: grid
+         class(KeywordEnforcer), optional, intent(in) :: unusable
+         integer, optional, intent(out) :: im
+         integer, optional, intent(out) :: jm
+         real(kind=ESMF_KIND_R4), optional, pointer, intent(out) :: latitudes(:,:)
+         real(kind=ESMF_KIND_R4), optional, pointer, intent(out) :: longitudes(:,:)
+         integer, optional, intent(out) :: rc
+      end subroutine grid_get
+   end interface
+
+end module mapl3g_Geom_API
