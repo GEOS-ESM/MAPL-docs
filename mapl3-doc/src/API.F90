f21b0239b7fce78391f11089ea021933b586d787
mathomp4 <fortran@gmail.com>
2025-08-04 20:14:57 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@9c46f7a55cd71c99dd7ed9689456e9da86337bfc 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,34 +1,40 @@
-module mapl3g_FieldBundle_API
+module mapl3g_Geom_API
 
-   use mapl3g_FieldBundleType_Flag
-   use mapl3g_FieldBundleCreate, only: MAPL_FieldBundleCreate => FieldBundleCreate
-   use mapl3g_FieldBundleGet, only: MAPL_FieldBundleGet => FieldBundleGet
-   use mapl3g_FieldBundleSet, only: MAPL_FieldBundleSet => FieldBundleSet
-   use mapl3g_FieldBundleInfo, only: MAPL_FieldBundleInfoGetInternal => FieldBundleInfoGetInternal
-   use mapl3g_FieldBundleInfo, only: MAPL_FieldBundleInfoSetInternal => FieldBundleInfoSetInternal
+   use mapl_KeywordEnforcer
+   use mapl3g_MaplGeom, only: MaplGeom
+   use mapl3g_GeomSpec, only: GeomSpec
+   use mapl3g_GeomManager, only: GeomManager, geom_manager, get_geom_manager
+   use mapl3g_GeomUtilities, only: MAPL_SameGeom, MAPL_GeomGetId
+   use esmf, only: ESMF_Grid, ESMF_KIND_R4
 
-   implicit none
+   implicit none(type,external)
 
    private
 
    ! Available to users
-   public :: MAPL_FieldBundleCreate
-   public :: MAPL_FieldBundleGet
-   public :: MAPL_FieldBundleSet
-   ! Maybe these should be private?
-   public :: MAPL_FieldBundleInfoGetInternal
-   public :: MAPL_FieldBundleInfoSetInternal
-
-   public :: FieldBundleType_Flag
-   public :: FIELDBUNDLETYPE_INVALID
-   public :: FIELDBUNDLETYPE_BASIC
-   public :: FIELDBUNDLETYPE_VECTOR
-   public :: FIELDBUNDLETYPE_BRACKET
-
-   public :: operator(==)
-   public :: operator(/=)
+   public :: MAPL_GridGet
 
    ! Used internally by MAPL
    ! Users shouldn't need these
+   public :: MaplGeom
+   public :: MAPL_SameGeom, MAPL_GeomGetId
+   public :: GeomManager, geom_manager, get_geom_manager
+   public :: GeomSpec
 
-end module mapl3g_FieldBundle_API
+   interface MAPL_GridGet
+      procedure ::  grid_get
+   end interface MAPL_GridGet
+
+   interface
+      module subroutine grid_get(grid, unusable, im, jm, latitudes, longitudes, rc)
+         type(ESMF_Grid), intent(in) :: grid
+         class(KeywordEnforcer), optional, intent(in) :: unusable
+         integer, optional, intent(out) :: im
+         integer, optional, intent(out) :: jm
+         real(kind=ESMF_KIND_R4), optional, pointer, intent(out) :: latitudes(:,:)
+         real(kind=ESMF_KIND_R4), optional, pointer, intent(out) :: longitudes(:,:)
+         integer, optional, intent(out) :: rc
+      end subroutine grid_get
+   end interface
+
+end module mapl3g_Geom_API
