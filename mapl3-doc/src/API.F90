5784c63d51220c3209262a3ed26583a06bb9ae59
bena-nasa <benjamin.m.auer@nasa.gov>
2025-08-06 19:56:14 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@94b292fab055fc41a873e0856c952614082678e3 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,12 +1,3 @@
-module mapl3g_Field_API
-   use mapl3g_FieldGet, only: MAPL_FieldGet => FieldGet
-   use mapl3g_FieldSet, only: MAPL_FieldSet => FieldSet
-   use mapl3g_FieldCreate
-   use mapl3g_VerticalStaggerLoc
-   ! Internal info should not be exposed to users
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetShared
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetShared
-
-end module mapl3g_Field_API
+module mapl3g_VM_API
+   use mapl3g_vm
+end module mapl3g_VM_API
