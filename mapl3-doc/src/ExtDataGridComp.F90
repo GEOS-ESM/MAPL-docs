6e2daec47ff52c2922d9f0bc7ad18dcca1577404
tclune <thomas.l.clune@nasa.gov>
2025-08-07 16:22:12 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@e42132cbb9b484b6d5e077721f49f3f6572f9aff 🚀

---
+++ mapl3-doc/src/ExtDataGridComp.F90
@@ -0,0 +1,158 @@
+#include "MAPL.h"
+
+module mapl3g_ExtDataGridComp
+   use generic3g
+   use mapl_ErrorHandling
+   use esmf
+   use pfio
+   use mapl3g_ExtDataGridComp_private
+   use mapl3g_Geom_API
+   use MAPL_FieldUtils
+   use mapl3g_FieldBundle_API
+   use mapl3g_ExtDataConfig
+   use mapl3g_PrimaryExportVector
+   use mapl3g_PrimaryExport
+   use mapl3g_geomio
+   use mapl3g_Geom_API
+   use mapl3g_AbstractDataSetFileSelector
+   use MAPL_FileMetadataUtilsMod
+   use gftl2_StringStringMap
+   use mapl3g_ExtDataReader
+   
+   implicit none(type,external)
+   private
+
+   public :: setServices
+
+   ! Private state
+   character(*), parameter :: PRIVATE_STATE = "ExtData"
+   type :: ExtDataGridComp
+      type(PrimaryExportVector) :: export_vector
+   end type ExtDataGridComp
+
+contains
+
+   subroutine setServices(gridcomp, rc)
+      type(ESMF_GridComp) :: gridcomp
+      integer, intent(out) :: rc
+
+      type(ESMF_HConfig) :: hconfig
+      integer :: status
+
+      call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, modify_advertise, phase_name="GENERIC::INIT_MODIFY_ADVERTISED", _RC)
+      call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_RUN, run, phase_name='run', _RC)
+
+      call MAPL_GridCompGet(gridcomp, hconfig=hconfig, _RC)
+      
+      ! ESMF has a bug, for now we will not merge hconfig until fixed
+      !merged_configs = ESMF_HConfigCreate(_RC)
+      ! instead pass hconfig and this will have to traverse the subconfigs for now
+      call add_var_specs(gridcomp, hconfig, _RC)
+
+      _SET_NAMED_PRIVATE_STATE(gridcomp, ExtDataGridComp, PRIVATE_STATE)
+
+      _RETURN(_SUCCESS)
+   end subroutine setServices
+
+   subroutine modify_advertise(gridcomp, importState, exportState, clock, rc)
+      type(ESMF_GridComp)   :: gridcomp
+      type(ESMF_State)      :: importState
+      type(ESMF_State)      :: exportState
+      type(ESMF_Clock)      :: clock      
+      integer, intent(out)  :: rc
+
+      integer :: status
+
+      integer :: rules_for_item
+      type(StringVector) :: active_items
+      type(ExtDataConfig) :: config
+      type(ESMF_Hconfig) :: hconfig
+      type(ESMF_Time) :: current_time
+      type(StringVectorIterator) :: iter
+      character(len=:), pointer :: item_name
+      logical :: has_rule
+      type(ExtDataGridComp), pointer :: extdata_gridcomp
+      type(PrimaryExport) :: primary_export
+      class(logger), pointer :: lgr
+
+      _GET_NAMED_PRIVATE_STATE(gridcomp, ExtDataGridComp, PRIVATE_STATE, extdata_gridcomp)
+
+      call MAPL_GridCompGet(gridcomp, logger=lgr, _RC)
+      call ESMF_ClockGet(clock, currTime=current_time, _RC)
+      call MAPL_GridCompGet(gridcomp, hconfig=hconfig, _RC)
+      active_items = get_active_items(exportState, _RC)
+      call new_ExtDataConfig_from_yaml(config, hconfig, current_time,  _RC)
+      iter = active_items%ftn_begin()
+      do while (iter /= active_items%ftn_end())
+         call iter%next()
+         item_name => iter%of()
+         has_rule = config%has_rule_for(item_name, _RC)
+         _ASSERT(has_rule, 'no rule for extdata item: '//item_name)
+         rules_for_item = config%count_rules_for_item(item_name, _RC)
+         _ASSERT(rules_for_item == 1, 'only 1 rule per item supported now')
+         primary_export = config%make_PrimaryExport(item_name, _RC)
+         call primary_export%complete_export_spec(item_name, exportState, _RC)
+         call extdata_gridcomp%export_vector%push_back(primary_export)
+      end do
+
+      call report_active_items(extdata_gridcomp%export_vector, lgr)
+      
+      _RETURN(_SUCCESS)
+   end subroutine modify_advertise
+
+   ! this is just to do something now. Obviously this is not how it will look...
+   subroutine run(gridcomp, importState, exportState, clock, rc)
+      type(ESMF_GridComp)   :: gridcomp
+      type(ESMF_State)      :: importState
+      type(ESMF_State)      :: exportState
+      type(ESMF_Clock)      :: clock      
+      integer, intent(out)  :: rc
+
+      integer :: status
+
+      type(ExtDataGridComp), pointer :: extdata_gridcomp
+      type(PrimaryExportVectorIterator) :: iter
+      type(PrimaryExport), pointer :: export_item
+      type(ESMF_Time) :: current_time
+      real :: weights(3)
+      character(len=:), allocatable :: export_name
+      type(ExtDataReader) :: reader
+      class(logger), pointer :: lgr
+      type(ESMF_FieldBundle) :: bundle 
+
+      call MAPL_GridCompGet(gridcomp, logger=lgr, _RC)
+      _GET_NAMED_PRIVATE_STATE(gridcomp, ExtDataGridComp, PRIVATE_STATE, extdata_gridcomp)
+      call ESMF_ClockGet(clock, currTime=current_time, _RC) 
+      call reader%initialize_reader(_RC)
+      iter = extdata_gridcomp%export_vector%ftn_begin()
+      do while (iter /= extdata_gridcomp%export_vector%ftn_end())
+         call iter%next()
+         export_item => iter%of() 
+         export_name = export_item%get_export_var_name()
+         call ESMF_StateGet(exportState, export_name, bundle, _RC) 
+         call export_item%update_my_bracket(bundle, current_time, weights, _RC)
+         call set_weights(exportState, export_name, weights, _RC)
+         call export_item%append_state_to_reader(exportState, reader, _RC)
+      end do
+      call reader%read_items(lgr, _RC)
+      call reader%destroy_reader(_RC)
+
+      _RETURN(_SUCCESS)
+   end subroutine run
+
+end module mapl3g_ExtDataGridComp
+
+subroutine setServices(gridcomp,rc)
+   use ESMF
+   use MAPL_ErrorHandlingMod
+   use mapl3g_ExtDataGridComp, only: ExtData_setServices => SetServices    
+   type(ESMF_GridComp)  :: gridcomp
+   integer, intent(out) :: rc
+
+   integer :: status
+
+   call ExtData_setServices(gridcomp,_RC)
+   _RETURN(_SUCCESS)
+
+end subroutine
+
---
+++ mapl3-doc/src/HistoryCollectionGridComp.F90
@@ -0,0 +1,151 @@
+#include "MAPL.h"
+
+module mapl3g_HistoryCollectionGridComp
+   use mapl3
+   use mapl3g_HistoryCollectionGridComp_private
+   use esmf
+   use MAPL_StringTemplate, only: fill_grads_template_esmf
+   implicit none
+   private
+
+   public :: setServices
+
+   ! Private state
+   type :: HistoryCollectionGridComp
+      type(ESMF_FieldBundle) :: output_bundle
+      class(GeomPFIO), allocatable :: writer
+      type(ESMF_Time) :: start_stop_times(2)
+      type(ESMF_Time) :: initial_file_time
+      type(ESMF_TimeInterval) :: timeStep
+      type(ESMF_TimeInterval) :: time_offstep
+      character(len=:), allocatable :: template
+      character(len=:), allocatable :: current_file
+   end type HistoryCollectionGridComp
+
+   character(len=*), parameter :: null_file = 'null_file'
+   character(*), parameter :: PRIVATE_STATE = "HistoryCollection"
+
+contains
+
+   subroutine setServices(gridcomp, rc)
+      type(ESMF_GridComp) :: gridcomp
+      integer, intent(out) :: rc
+
+      type(ESMF_HConfig) :: hconfig
+      integer :: status
+
+      ! Set entry points
+      call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, init_geom, phase_name='GENERIC::INIT_ADVERTISE', _RC)
+      call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, init, _RC)
+      call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_RUN, run, phase_name='run', _RC)
+
+      ! Attach private state
+      _SET_NAMED_PRIVATE_STATE(gridcomp, HistoryCollectionGridComp, PRIVATE_STATE)
+
+      call MAPL_GridCompGet(gridcomp, hconfig=hconfig, _RC)
+      call register_imports(gridcomp,hconfig,_RC)
+
+      _RETURN(_SUCCESS)
+   end subroutine setServices
+
+   subroutine init(gridcomp, importState, exportState, clock, rc)
+      type(ESMF_GridComp)   :: gridcomp
+      type(ESMF_State)      :: importState
+      type(ESMF_State)      :: exportState
+      type(ESMF_Clock)      :: clock
+      integer, intent(out)  :: rc
+
+      integer :: status
+      type(HistoryCollectionGridComp), pointer :: collection_gridcomp
+      type(ESMF_HConfig) :: hconfig
+      type(ESMF_Geom) :: geom
+      character(len=ESMF_MAXSTR) :: name
+      type(FileMetadata) :: metadata
+      type(MaplGeom), pointer :: mapl_geom
+
+      call MAPL_GridCompGet(gridcomp, hconfig=hconfig, _RC)
+      call ESMF_GridCompGet(gridcomp, name=name, _RC)
+
+      _GET_NAMED_PRIVATE_STATE(gridcomp, HistoryCollectionGridComp, PRIVATE_STATE, collection_gridcomp)
+      collection_gridcomp%output_bundle = create_output_bundle(hconfig, importState, _RC)
+
+      geom = detect_geom(collection_gridcomp%output_bundle, name, _RC)
+      metadata = bundle_to_metadata(collection_gridcomp%output_bundle, geom, _RC)
+      mapl_geom => get_mapl_geom(geom, _RC)
+      allocate(collection_gridcomp%writer, source=make_geom_pfio(metadata, rc=status))
+      _VERIFY(STATUS)
+      call collection_gridcomp%writer%initialize(metadata, mapl_geom, _RC)
+
+      collection_gridcomp%start_stop_times = set_start_stop_time(clock, hconfig, _RC)
+      collection_gridcomp%timeStep = get_frequency(hconfig, _RC)
+      collection_gridcomp%current_file = null_file
+      collection_gridcomp%template = ESMF_HConfigAsString(hconfig, keyString='template', _RC)
+      
+
+      _RETURN(_SUCCESS)
+   end subroutine init
+
+
+   subroutine init_geom(gridcomp, importState, exportState, clock, rc)
+      type(ESMF_GridComp)   :: gridcomp
+      type(ESMF_State)      :: importState
+      type(ESMF_State)      :: exportState
+      type(ESMF_Clock)      :: clock
+      integer, intent(out)  :: rc
+
+      integer :: status
+      type(ESMF_HConfig) :: hconfig
+      type(ESMF_Geom) :: geom
+      logical :: has_geom
+
+      call MAPL_GridCompGet(gridcomp, hconfig=hconfig, _RC)
+      has_geom = ESMF_HConfigIsDefined(hconfig, keystring='geom', _RC)
+      if (has_geom) then
+         geom = make_geom(hconfig)
+         call MAPL_GridCompSetGeom(gridcomp, geom, _RC)
+      end if
+
+      _RETURN(_SUCCESS)
+   end subroutine init_geom
+
+   subroutine run(gridcomp, importState, exportState, clock, rc)
+      type(ESMF_GridComp)   :: gridcomp
+      type(ESMF_State)      :: importState
+      type(ESMF_State)      :: exportState
+      type(ESMF_Clock)      :: clock
+      integer, intent(out)  :: rc
+
+      integer :: status, time_index
+      type(HistoryCollectionGridComp), pointer :: collection_gridcomp
+      logical :: run_collection
+      type(ESMF_Time) :: current_time
+      character(len=ESMF_MAXSTR) :: name
+      character(len=128) :: current_file
+      real, allocatable :: current_time_vector(:)
+
+      call ESMF_GridCompGet(gridcomp, name=name, _RC)
+      call ESMF_ClockGet(clock, currTime=current_time, _RC)
+      _GET_NAMED_PRIVATE_STATE(gridcomp, HistoryCollectionGridComp, PRIVATE_STATE, collection_gridcomp)
+
+      run_collection = (current_time >= collection_gridcomp%start_stop_times(1)) .and. &
+                           (current_time <= collection_gridcomp%start_stop_times(2))
+
+      _RETURN_UNLESS(run_collection)
+
+      _GET_NAMED_PRIVATE_STATE(gridcomp, HistoryCollectionGridComp, PRIVATE_STATE, collection_gridcomp)
+
+      call fill_grads_template_esmf(current_file, collection_gridcomp%template, collection_id=name, time=current_time, _RC)
+      if (trim(current_file) /= collection_gridcomp%current_file) then
+         collection_gridcomp%current_file = current_file
+         call collection_gridcomp%writer%update_time_on_server(current_time, _RC)
+         collection_gridcomp%initial_file_time = current_time
+      end if
+
+      call get_current_time_info(collection_gridcomp%initial_file_time, current_time, collection_gridcomp%timeStep, time_index, current_time_vector, _RC)
+      call collection_gridcomp%writer%stage_time_to_file(collection_gridcomp%current_file, current_time_vector,  _RC)
+      call collection_gridcomp%writer%stage_data_to_file(collection_gridcomp%output_bundle, collection_gridcomp%current_file, time_index, _RC)
+      _RETURN(_SUCCESS)
+
+   end subroutine run
+
+end module mapl3g_HistoryCollectionGridComp
