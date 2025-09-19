9cfbd32700910a5dbf559d81459df254a73c3413
mathomp4 <fortran@gmail.com>
2025-09-19 14:32:29 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@18846924e7aa3cbf16440a1434dfad60aca706ea 🚀

--- src/MAPL_Generic.F90
+++ src/MAPL_Generic.F90
@@ -8358,7 +8358,6 @@ contains
       character(len=ESMF_MAXSTR)            :: IAm
       integer                               :: status
       character(len=ESMF_MAXSTR)            :: comp_name
-      character(len=ESMF_MAXSTR)            :: child_comp_name
 
       ! Local variables
       ! ---------------
@@ -8403,8 +8402,6 @@ contains
          call MAPL_GenericConnCheck(gridcomp, RC=status)
          if (status /= ESMF_SUCCESS) then
             err = .true.
-            call ESMF_GridCompGet( gridcomp, NAME=child_comp_name, _RC)
-            CALL WRITE_PARALLEL("MAPL ConnCheck PROBLEM with CHILD GRIDCOMP "//TRIM(child_comp_name))
          end if
       end do
 
