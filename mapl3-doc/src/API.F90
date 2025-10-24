8f096234217efe7cd005f8f6a3bd0bd53706f024
mathomp4 <fortran@gmail.com>
2025-10-24 18:50:48 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@21c5d74eb43d0e5bf1aa0de2a2857fd096e52d8b 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,40 +1,41 @@
-module mapl3g_Geom_API
-
-   use mapl_KeywordEnforcer
-   use mapl3g_MaplGeom, only: MaplGeom
-   use mapl3g_GeomSpec, only: GeomSpec
-   use mapl3g_GeomManager, only: GeomManager, geom_manager, get_geom_manager
-   use mapl3g_GeomUtilities, only: MAPL_SameGeom, MAPL_GeomGetId
-   use esmf, only: ESMF_Grid, ESMF_KIND_R4
-
+module mapl3g_VerticalGrid_API
+   use mapl3g_VerticalGrid, only: VerticalGrid
+   use mapl3g_VerticalGridSpec, only: VerticalGridSpec
+   use mapl3g_VerticalGridFactory, only: VerticalGridFactory
+   use mapl3g_VerticalGridManager, only: VerticalGridManager
+   use mapl3g_VerticalGridManager, only: get_vertical_grid_manager
+   use mapl3g_IntegerPair, only: IntegerPair
+   use mapl3g_VerticalStaggerLoc
+   use mapl3g_BasicVerticalGrid, only: BasicVerticalGrid
+   use mapl3g_BasicVerticalGrid, only: BasicVerticalGridSpec
+   use mapl3g_BasicVerticalGrid, only: BasicVerticalGridFactory
    implicit none(type,external)
-
    private
 
-   ! Available to users
-   public :: MAPL_GridGet
-
-   ! Used internally by MAPL
-   ! Users shouldn't need these
-   public :: MaplGeom
-   public :: MAPL_SameGeom, MAPL_GeomGetId
-   public :: GeomManager, geom_manager, get_geom_manager
-   public :: GeomSpec
-
-   interface MAPL_GridGet
-      procedure ::  grid_get
-   end interface MAPL_GridGet
-
-   interface
-      module subroutine grid_get(grid, unusable, im, jm, latitudes, longitudes, rc)
-         type(ESMF_Grid), intent(in) :: grid
-         class(KeywordEnforcer), optional, intent(in) :: unusable
-         integer, optional, intent(out) :: im
-         integer, optional, intent(out) :: jm
-         real(kind=ESMF_KIND_R4), optional, pointer, intent(out) :: latitudes(:,:)
-         real(kind=ESMF_KIND_R4), optional, pointer, intent(out) :: longitudes(:,:)
-         integer, optional, intent(out) :: rc
-      end subroutine grid_get
-   end interface
-
-end module mapl3g_Geom_API
+   ! Abstract base types
+   public :: VerticalGrid
+   public :: VerticalGridSpec
+   public :: VerticalGridFactory
+   
+   ! Manager
+   public :: VerticalGridManager
+   public :: get_vertical_grid_manager
+   
+   ! Utility types
+   public :: IntegerPair
+   
+   ! Vertical stagger locations
+   public :: VerticalStaggerLoc
+   public :: operator(==), operator(/=)
+   public :: VERTICAL_STAGGER_NONE
+   public :: VERTICAL_STAGGER_EDGE
+   public :: VERTICAL_STAGGER_CENTER
+   public :: VERTICAL_STAGGER_MIRROR
+   public :: VERTICAL_STAGGER_INVALID
+   
+   ! Basic grid types
+   public :: BasicVerticalGrid
+   public :: BasicVerticalGridSpec
+   public :: BasicVerticalGridFactory
+   
+end module mapl3g_VerticalGrid_API
