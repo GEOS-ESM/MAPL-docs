d94cdaf0418d9d5132e715f6fe0a8bc9db8dc61a
pchakraborty <pchakraborty@users.noreply.github.com>
2025-11-14 16:56:20 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@a5b4b7ed18c69c58d67d282f26db2bec1a959ca3 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,13 +1,38 @@
-module mapl3g_State_API
-   use mapl3g_StateGet, only: MAPL_StateGet => StateGet
-   use mapl3g_StateGetPointer, only: MAPL_StateGetPointer => StateGetPointer
-   use MAPL_StateArithmeticParserMod, only: MAPL_ParserVariablesInExpression => parser_variables_in_expression
+module mapl3g_FieldBundle_API
+
+   use ESMF, only: MAPL_FieldBundleAdd => ESMF_FieldBundleAdd
+   use mapl3g_FieldBundleType_Flag
+   use mapl3g_FieldBundleCreate, only: MAPL_FieldBundleCreate => FieldBundleCreate
+   use mapl3g_FieldBundleCreate, only: MAPL_FieldBundlesAreAliased => FieldBundlesAreAliased
+   use mapl3g_FieldBundleGet, only: MAPL_FieldBundleGet => FieldBundleGet
+   use mapl3g_FieldBundleSet, only: MAPL_FieldBundleSet => FieldBundleSet
+   use mapl3g_FieldBundleInfo, only: MAPL_FieldBundleInfoGetInternal => FieldBundleInfoGetInternal
+   use mapl3g_FieldBundleInfo, only: MAPL_FieldBundleInfoSetInternal => FieldBundleInfoSetInternal
+
    implicit none
+
    private
 
    ! Available to users
-   public :: MAPL_StateGet
-   public :: MAPL_StateGetPointer
-   public :: MAPL_ParserVariablesInExpression
+   public :: MAPL_FieldBundleCreate
+   public :: MAPL_FieldBundlesAreAliased
+   public :: MAPL_FieldBundleGet
+   public :: MAPL_FieldBundleSet
+   public :: MAPL_FieldBundleAdd
+   ! Maybe these should be private?
+   public :: MAPL_FieldBundleInfoGetInternal
+   public :: MAPL_FieldBundleInfoSetInternal
 
-end module mapl3g_State_API
+   public :: FieldBundleType_Flag
+   public :: FIELDBUNDLETYPE_INVALID
+   public :: FIELDBUNDLETYPE_BASIC
+   public :: FIELDBUNDLETYPE_VECTOR
+   public :: FIELDBUNDLETYPE_BRACKET
+
+   public :: operator(==)
+   public :: operator(/=)
+
+   ! Used internally by MAPL
+   ! Users shouldn't need these
+
+end module mapl3g_FieldBundle_API
