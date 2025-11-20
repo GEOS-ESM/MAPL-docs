73b984b210e68a090bc17d59a065895dc6fbca1f
mathomp4 <fortran@gmail.com>
2025-11-20 20:10:46 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@c35a2f70d8061850dc0678faadbc34f3378afef6 🚀

--- mapl3-doc/src/ExtDataDerived.F90
+++ mapl3-doc/src/ExtDataDerived.F90
@@ -1,6 +1,6 @@
 #include "MAPL_Exceptions.h"
 #include "MAPL_ErrLog.h"
-module MAPL_ExtDataDerived
+module mapl3g_ExtDataDerived
    use ESMF
    use MAPL_KeywordEnforcerMod
    use MAPL_ExceptionHandling
@@ -33,10 +33,7 @@ contains
       logical :: is_present
       integer :: status
       character(len=:), allocatable :: tempc
-      _UNUSED_DUMMY(unusable)
 
-
-      if (allocated(tempc)) deallocate(tempc)
       is_present = ESMF_HConfigIsDefined(config,keyString="function",_RC)
       _ASSERT(is_present,"no expression found in derived entry")
       if (is_present) then
@@ -44,13 +41,13 @@ contains
          rule%expression=tempc
       end if
 
-      if (allocated(tempc)) deallocate(tempc)
       is_present = ESMF_HConfigIsDefined(config,keyString="sample",_RC)
       if (is_present) then
          tempc = ESMF_HConfigAsString(config,keyString="sample",_RC)
          rule%sample_key=tempc
       end if
 
+      _UNUSED_DUMMY(unusable)
       _RETURN(_SUCCESS)
    end function new_ExtDataDerived
 
@@ -89,26 +86,4 @@ contains
       write(*,*)"function: ",trim(this%expression)
    end subroutine display
 
-end module MAPL_ExtDataDerived
-
-module MAPL_ExtDataDerivedMap
-   use MAPL_ExtDataDerived
-
-#include "types/key_deferredLengthString.inc"
-#define _value type(ExtDataDerived)
-#define _alt
-
-#define _pair ExtDataDerivedPair
-#define _map ExtDataDerivedMap
-#define _iterator ExtDataDerivedMapIterator
-
-#include "templates/map.inc"
-
-#undef _iterator
-#undef _map
-#undef _pair
-
-#undef _alt
-#undef _value
-
-end module MAPL_ExtDataDerivedMap
+end module mapl3g_ExtDataDerived
