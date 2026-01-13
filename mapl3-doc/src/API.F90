8ec121039ff3425d4d1b8db8e921b37baaf320ed
pchakraborty <pchakraborty@users.noreply.github.com>
2026-01-13 18:29:36 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@312b93db732a605f7b0638a90511198f02dd1a0a 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,26 +1,13 @@
-module mapl3g_Geom_API
-
-   use mapl_KeywordEnforcer
-   use mapl3g_MaplGeom, only: MaplGeom
-   use mapl3g_GeomSpec, only: GeomSpec
-   use mapl3g_GeomManager, only: GeomManager, geom_manager, get_geom_manager, get_mapl_geom
-   use mapl3g_GeomUtilities, only: mapl_SameGeom, mapl_GeomGetId
-   use mapl3g_GridGet, only: mapl_GridGet => GridGet, mapl_GridGetCoordinates => GridGetCoordinates
-   use esmf, only: ESMF_Grid, ESMF_Geom, ESMF_KIND_R4
-
-   implicit none(type,external)
-
+module mapl3g_State_API
+   use mapl3g_StateGet, only: MAPL_StateGet => StateGet
+   use mapl3g_StateGetPointer, only: MAPL_StateGetPointer => StateGetPointer
+   use MAPL_StateArithmeticParserMod, only: MAPL_ParserVariablesInExpression => parser_variables_in_expression
+   implicit none
    private
 
    ! Available to users
-   public :: mapl_GridGet
-   public :: mapl_GridGetCoordinates
+   public :: MAPL_StateGet
+   public :: MAPL_StateGetPointer
+   public :: MAPL_ParserVariablesInExpression
 
-   ! Used internally by MAPL
-   ! Users shouldn't need these
-   public :: MaplGeom
-   public :: mapl_SameGeom, mapl_GeomGetId
-   public :: GeomManager, geom_manager, get_geom_manager, get_mapl_geom
-   public :: GeomSpec
-
-end module mapl3g_Geom_API
+end module mapl3g_State_API
