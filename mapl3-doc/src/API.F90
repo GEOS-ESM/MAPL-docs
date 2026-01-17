28fb8d9cacca47c8b2a5fbfb4bea7fbdca4c23b0
tclune <thomas.l.clune@nasa.gov>
2026-01-17 20:48:57 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@e83d2ecd5b66259a8fdec05f181e219f993d7e9d 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,26 +1,9 @@
-module mapl3g_Geom_API
-
-   use mapl_KeywordEnforcer
-   use mapl3g_MaplGeom, only: MaplGeom
-   use mapl3g_GeomSpec, only: GeomSpec
-   use mapl3g_GeomManager, only: GeomManager, geom_manager, get_geom_manager, get_mapl_geom
-   use mapl3g_GeomUtilities, only: mapl_SameGeom, mapl_GeomGetId
-   use mapl3g_GridGet, only: mapl_GridGet => GridGet, mapl_GridGetCoordinates => GridGetCoordinates
-   use esmf, only: ESMF_Grid, ESMF_Geom, ESMF_KIND_R4
-
+module mapl3g_HConfig_API
+   use mapl3g_HConfigAs, only: mapl_HConfigAsItemType => HConfigAsItemType
+   use mapl3g_HConfigAs, only: mapl_HConfigAsStateIntent => HConfigAsStateIntent
+   use mapl3g_HConfigAs, only: mapl_HConfigAsTime => HConfigAstime
+   use mapl3g_HConfigAs, only: mapl_HConfigAsTimeInterval => HConfigAsTimeInterval
+   use mapl3g_HConfigAs, only: mapl_HConfigAsStringVector => HConfigAsStringVector
    implicit none(type,external)
-
-   private
-
-   ! Available to users
-   public :: mapl_GridGet
-   public :: mapl_GridGetCoordinates
-
-   ! Used internally by MAPL
-   ! Users shouldn't need these
-   public :: MaplGeom
-   public :: mapl_SameGeom, mapl_GeomGetId
-   public :: GeomManager, geom_manager, get_geom_manager, get_mapl_geom
-   public :: GeomSpec
-
-end module mapl3g_Geom_API
+end module mapl3g_HConfig_API
+   
