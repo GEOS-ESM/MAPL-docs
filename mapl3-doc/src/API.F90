5525b98ea257bc281145c24fc79463513a658e54
pchakraborty <pchakraborty@users.noreply.github.com>
2026-01-20 19:49:59 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@e6f689b754051133a1d35210d25460912a2ab1bc 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,9 +1,6 @@
-module mapl3g_HConfig_API
-   use mapl3g_HConfigAs, only: mapl_HConfigAsItemType => HConfigAsItemType
-   use mapl3g_HConfigAs, only: mapl_HConfigAsStateIntent => HConfigAsStateIntent
-   use mapl3g_HConfigAs, only: mapl_HConfigAsTime => HConfigAstime
-   use mapl3g_HConfigAs, only: mapl_HConfigAsTimeInterval => HConfigAsTimeInterval
-   use mapl3g_HConfigAs, only: mapl_HConfigAsStringVector => HConfigAsStringVector
-   implicit none(type,external)
-end module mapl3g_HConfig_API
-   
+module mapl3g_Utilities_Comms_API
+   use mapl3g_Comms, only: MAPL_CommsGatherV => CommsGatherV
+   use mapl3g_Comms, only: MAPL_CommsScatterV => CommsScatterV
+   use mapl3g_Comms, only: MAPL_ArrayGather => ArrayGather
+   use mapl3g_Comms, only: MAPL_ArrayScatter => ArrayScatter
+end module mapl3g_Utilities_Comms_API
