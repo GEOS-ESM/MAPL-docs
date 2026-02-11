72f8d52e1f0ebe18d4d564b9a462cbe635cb04e7
mathomp4 <fortran@gmail.com>
2026-02-11 17:38:03 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@ee0d26ea87fafe3b9f8ce24a2fabba97c0a5a86a 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,21 +1,13 @@
-module mapl3g_Utilities_Comms_API
-   use mapl3g_Comms, only: MAPL_Am_I_Root => Am_I_Root
-   use mapl3g_Comms, only: MAPL_NPES => num_pes
+module mapl3g_State_API
+   use mapl3g_StateGet, only: MAPL_StateGet => StateGet
+   use mapl3g_StateGetPointer, only: MAPL_StateGetPointer => StateGetPointer
+   use MAPL_StateArithmeticParserMod, only: MAPL_ParserVariablesInExpression => parser_variables_in_expression
+   implicit none
+   private
 
-   use mapl3g_Comms, only: MAPL_CommsSend => comms_send
-   use mapl3g_Comms, only: MAPL_CommsRecv => comms_recv
-   use mapl3g_Comms, only: MAPL_CommsSendRecv => comms_sendrecv
+   ! Available to users
+   public :: MAPL_StateGet
+   public :: MAPL_StateGetPointer
+   public :: MAPL_ParserVariablesInExpression
 
-   use mapl3g_Comms, only: MAPL_CommsGatherV => comms_gatherv
-   use mapl3g_Comms, only: MAPL_CommsScatterV => comms_scatterv
-
-   use mapl3g_Comms, only: MAPL_CommsAllGather => comms_allgather
-   use mapl3g_Comms, only: MAPL_CommsAllGatherV => comms_allgatherv
-
-   use mapl3g_Comms, only: MAPL_ArrayGather => array_gather
-   use mapl3g_Comms, only: MAPL_ArrayScatter => array_scatter
-
-   use mapl3g_Comms, only: MAPL_CommsAllReduceMin => comms_allreduce_min
-   use mapl3g_Comms, only: MAPL_CommsAllReduceMax => comms_allreduce_max
-   use mapl3g_Comms, only: MAPL_CommsAllReduceSum => comms_allreduce_sum
-end module mapl3g_Utilities_Comms_API
+end module mapl3g_State_API
