895b415bc7945bf8b729e851c52a9237040d4d91
bena-nasa <benjamin.m.auer@nasa.gov>
2026-03-25 13:34:26 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@9e71deb53628470a849fba9e789a88db42445934 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,21 +1,48 @@
-module mapl3g_Utilities_Comms_API
-   use mapl3g_Comms, only: MAPL_Am_I_Root => Am_I_Root
-   use mapl3g_Comms, only: MAPL_NPES => num_pes
+module mapl3g_FieldBundle_API
 
-   use mapl3g_Comms, only: MAPL_CommsSend => comms_send
-   use mapl3g_Comms, only: MAPL_CommsRecv => comms_recv
-   use mapl3g_Comms, only: MAPL_CommsSendRecv => comms_sendrecv
+   use ESMF, only: MAPL_FieldBundleAdd => ESMF_FieldBundleAdd
+   use mapl3g_FieldBundleType_Flag
+   use mapl3g_VectorBasisKind
+   use mapl3g_FieldBundleCreate, only: MAPL_FieldBundleCreate => FieldBundleCreate
+   use mapl3g_FieldBundleCreate, only: MAPL_FieldBundlesAreAliased => FieldBundlesAreAliased
+   use mapl3g_FieldBundleGet, only: MAPL_FieldBundleGet => FieldBundleGet
+   use mapl3g_FieldBundleSet, only: MAPL_FieldBundleSet => FieldBundleSet
+   use mapl3g_FieldBundleInfo, only: MAPL_FieldBundleInfoGetInternal => FieldBundleInfoGetInternal
+   use mapl3g_FieldBundleInfo, only: MAPL_FieldBundleInfoSetInternal => FieldBundleInfoSetInternal
+   use mapl3g_FieldBundleGetPointer, only: MAPL_FieldBundleGetPointer => FieldBundleGetPointerToData
 
-   use mapl3g_Comms, only: MAPL_CommsGatherV => comms_gatherv
-   use mapl3g_Comms, only: MAPL_CommsScatterV => comms_scatterv
+   implicit none
 
-   use mapl3g_Comms, only: MAPL_CommsAllGather => comms_allgather
-   use mapl3g_Comms, only: MAPL_CommsAllGatherV => comms_allgatherv
+   private
 
-   use mapl3g_Comms, only: MAPL_ArrayGather => array_gather
-   use mapl3g_Comms, only: MAPL_ArrayScatter => array_scatter
+   ! Available to users
+   public :: MAPL_FieldBundleCreate
+   public :: MAPL_FieldBundlesAreAliased
+   public :: MAPL_FieldBundleGet
+   public :: MAPL_FieldBundleSet
+   public :: MAPL_FieldBundleAdd
+   public :: MAPL_FieldBundleGetPointer
+   ! Maybe these should be private?
+   public :: MAPL_FieldBundleInfoGetInternal
+   public :: MAPL_FieldBundleInfoSetInternal
 
-   use mapl3g_Comms, only: MAPL_CommsAllReduceMin => comms_allreduce_min
-   use mapl3g_Comms, only: MAPL_CommsAllReduceMax => comms_allreduce_max
-   use mapl3g_Comms, only: MAPL_CommsAllReduceSum => comms_allreduce_sum
-end module mapl3g_Utilities_Comms_API
+   public :: FieldBundleType_Flag
+   public :: FIELDBUNDLETYPE_INVALID
+   public :: FIELDBUNDLETYPE_BASIC
+   public :: FIELDBUNDLETYPE_VECTOR
+   public :: FIELDBUNDLETYPE_BRACKET
+   public :: FIELDBUNDLETYPE_VECTORBRACKET
+
+   public :: operator(==)
+   public :: operator(/=)
+
+   ! VectorBasisKind
+   public :: VectorBasisKind
+   public :: VECTOR_BASIS_KIND_INVALID
+   public :: VECTOR_BASIS_KIND_GRID
+   public :: VECTOR_BASIS_KIND_NS
+
+   ! Used internally by MAPL
+   ! Users shouldn't need these
+
+end module mapl3g_FieldBundle_API
