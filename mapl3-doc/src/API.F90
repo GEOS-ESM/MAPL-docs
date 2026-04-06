b17c8d5464646250ca01bb75dde4c179f07f075f
mathomp4 <fortran@gmail.com>
2026-04-06 17:24:07 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@7355a3fd62495368f5365b51235ec12bb32b16c0 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,54 +1,13 @@
-module mapl3g_VerticalGrid_API
-   use mapl3g_VerticalGrid, only: VerticalGrid
-   use mapl3g_VerticalGrid, only: VERTICAL_GRID_NOT_FOUND
-   use mapl3g_VerticalGridSpec, only: VerticalGridSpec
-   use mapl3g_VerticalGridFactory, only: VerticalGridFactory
-   use mapl3g_VerticalGridManager, only: VerticalGridManager
-   use mapl3g_VerticalGridManager, only: get_vertical_grid_manager
-   use mapl3g_IntegerPair, only: IntegerPair
-   use mapl3g_VerticalStaggerLoc
-   use mapl3g_VerticalAlignment
-   use mapl3g_BasicVerticalGrid, only: BasicVerticalGrid
-   use mapl3g_BasicVerticalGrid, only: BasicVerticalGridSpec
-   use mapl3g_BasicVerticalGrid, only: BasicVerticalGridFactory
-   implicit none(type,external)
+module mapl3g_State_API
+   use mapl3g_StateGet, only: MAPL_StateGet => StateGet
+   use mapl3g_StateGetPointer, only: MAPL_StateGetPointer => StateGetPointer
+   use MAPL_StateArithmeticParserMod, only: MAPL_ParserVariablesInExpression => parser_variables_in_expression
+   implicit none
    private
 
-   ! Abstract base types
-   public :: VerticalGrid
-   public :: VerticalGridSpec
-   public :: VerticalGridFactory
+   ! Available to users
+   public :: MAPL_StateGet
+   public :: MAPL_StateGetPointer
+   public :: MAPL_ParserVariablesInExpression
 
-   ! Manager
-   public :: VerticalGridManager
-   public :: get_vertical_grid_manager
-   
-   ! Utility types
-   public :: IntegerPair
-   
-   ! Vertical stagger locations
-   public :: VerticalStaggerLoc
-   public :: operator(==), operator(/=)
-   public :: VERTICAL_STAGGER_NONE
-   public :: VERTICAL_STAGGER_EDGE
-   public :: VERTICAL_STAGGER_CENTER
-   public :: VERTICAL_STAGGER_MIRROR
-   public :: VERTICAL_STAGGER_INVALID
-   
-   ! Vertical alignment
-   public :: VerticalAlignment
-   public :: VALIGN_WITH_GRID
-   public :: VALIGN_UP
-   public :: VALIGN_DOWN
-   public :: VALIGN_INVALID
-   
-   ! Basic grid types
-   public :: BasicVerticalGrid
-   public :: BasicVerticalGridSpec
-   public :: BasicVerticalGridFactory
-
-   ! Parameters
-   public :: VERTICAL_GRID_NOT_FOUND
-  
-   
-end module mapl3g_VerticalGrid_API
+end module mapl3g_State_API
