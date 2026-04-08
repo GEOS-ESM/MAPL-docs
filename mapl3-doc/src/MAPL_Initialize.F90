e581f126b6270afc658126196cfa4c8177e33c09
mathomp4 <fortran@gmail.com>
2026-04-08 19:55:57 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@2d910a6b8cea8c0e372d80dfb49455c587a00c79 🚀

---
+++ mapl3-doc/src/MAPL_Initialize.F90
@@ -0,0 +1,45 @@
+#include "MAPL.h"
+module MAPL_pFUnit_Initialize
+   implicit none(type,external)
+
+contains
+   subroutine Initialize()
+      use MAPL
+      use fArgParse
+      use MAPL_ThrowMod, only: MAPL_set_throw_method
+      use MAPL_pFUnit_ThrowMod
+      use pflogger, only: pfl_initialize => initialize, WARNING, DEBUG
+      use gFTL2_StringUnlimitedMap
+
+      type(ArgParser), target :: parser
+      type (StringUnlimitedMap), target :: options
+      character(:), allocatable :: level_name
+      class(*), pointer :: option
+
+      call MAPL_set_throw_method(throw)
+
+      call set_command_line_options()
+
+      option => options%at('level')
+      if (associated(option)) then
+         call cast(option, level_name)
+      end if
+
+      call MAPL_initialize(level_name=level_name)
+
+   contains
+
+      subroutine set_command_line_options()
+         
+         parser = ArgParser()
+         call parser%add_argument('--level', '-l', action='store', default='WARNING', help='set logging level')
+         
+#ifndef _GNU
+         options = parser%parse_args()
+#else
+         call parser%parse_args_kludge(option_values=options)
+#endif
+      end subroutine set_command_line_options
+ 
+   end subroutine Initialize
+end module MAPL_pFUnit_Initialize
