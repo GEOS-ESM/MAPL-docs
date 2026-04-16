951abcd279276ef568834e302ded760f43663be8
darianboggs <61847056+darianboggs@users.noreply.github.com>
2026-04-16 15:35:55 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@afeab5309bddde49717315bf6459a0e5a9d4df28 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,18 +1,48 @@
-module mapl3g_Field_API
-   use mapl3g_FieldGet, only: MAPL_FieldGet => FieldGet
-   use mapl3g_FieldSet, only: MAPL_FieldSet => FieldSet
-   use mapl3g_FieldFill, only: MAPL_FieldFill => FieldFill
-   use mapl3g_FieldCreate
-   use mapl3g_StateItemAllocation
-   use mapl3g_RestartModes
-   use mapl_FieldPointerUtilities, only: MAPL_AssignFptr => assign_fptr
-   use mapl_FieldPointerUtilities, only: MAPL_FieldClone => FieldClone
+module mapl3g_FieldBundle_API
 
-   ! Internal info should not be exposed to users
-   use mapl3g_FieldInfo
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetShared
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetShared
+   use ESMF, only: MAPL_FieldBundleAdd => ESMF_FieldBundleAdd
+   use mapl3g_FieldBundleType_Flag
+   use mapl3g_VectorBasisKind
+   use mapl3g_FieldBundleCreate, only: MAPL_FieldBundleCreate => FieldBundleCreate
+   use mapl3g_FieldBundleCreate, only: MAPL_FieldBundlesAreAliased => FieldBundlesAreAliased
+   use mapl3g_FieldBundleGet, only: MAPL_FieldBundleGet => FieldBundleGet
+   use mapl3g_FieldBundleSet, only: MAPL_FieldBundleSet => FieldBundleSet
+   use mapl3g_FieldBundleInfo, only: MAPL_FieldBundleInfoGetInternal => FieldBundleInfoGetInternal
+   use mapl3g_FieldBundleInfo, only: MAPL_FieldBundleInfoSetInternal => FieldBundleInfoSetInternal
+   use mapl3g_FieldBundleGetPointer, only: MAPL_FieldBundleGetPointer => FieldBundleGetPointerToData
 
-end module mapl3g_Field_API
+   implicit none
+
+   private
+
+   ! Available to users
+   public :: MAPL_FieldBundleCreate
+   public :: MAPL_FieldBundlesAreAliased
+   public :: MAPL_FieldBundleGet
+   public :: MAPL_FieldBundleSet
+   public :: MAPL_FieldBundleAdd
+   public :: MAPL_FieldBundleGetPointer
+   ! Maybe these should be private?
+   public :: MAPL_FieldBundleInfoGetInternal
+   public :: MAPL_FieldBundleInfoSetInternal
+
+   public :: FieldBundleType_Flag
+   public :: FIELDBUNDLETYPE_INVALID
+   public :: FIELDBUNDLETYPE_BASIC
+   public :: FIELDBUNDLETYPE_VECTOR
+   public :: FIELDBUNDLETYPE_BRACKET
+   public :: FIELDBUNDLETYPE_VECTORBRACKET
+
+   public :: operator(==)
+   public :: operator(/=)
+
+   ! VectorBasisKind
+   public :: VectorBasisKind
+   public :: VECTOR_BASIS_KIND_INVALID
+   public :: VECTOR_BASIS_KIND_GRID
+   public :: VECTOR_BASIS_KIND_NS
+
+   ! Used internally by MAPL
+   ! Users shouldn't need these
+
+end module mapl3g_FieldBundle_API
