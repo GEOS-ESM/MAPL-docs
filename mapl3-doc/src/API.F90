e37d5f980535bb3deda3e3ce57eaafa5e523842f
tclune <thomas.l.clune@nasa.gov>
2026-04-24 13:11:37 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@ff514cc87114b2ff723dc934e9fccf60202df516 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,9 +1,21 @@
-module mapl3g_HConfig_API
-   use mapl3g_HConfigAs, only: mapl_HConfigAsItemType => HConfigAsItemType
-   use mapl3g_HConfigAs, only: mapl_HConfigAsStateIntent => HConfigAsStateIntent
-   use mapl3g_HConfigAs, only: mapl_HConfigAsTime => HConfigAstime
-   use mapl3g_HConfigAs, only: mapl_HConfigAsTimeInterval => HConfigAsTimeInterval
-   use mapl3g_HConfigAs, only: mapl_HConfigAsStringVector => HConfigAsStringVector
-   implicit none(type,external)
-end module mapl3g_HConfig_API
-   
+module mapl3g_Utilities_Comms_API
+   use mapl3g_Comms, only: MAPL_Am_I_Root => Am_I_Root
+   use mapl3g_Comms, only: MAPL_NPES => num_pes
+
+   use mapl3g_Comms, only: MAPL_CommsSend => comms_send
+   use mapl3g_Comms, only: MAPL_CommsRecv => comms_recv
+   use mapl3g_Comms, only: MAPL_CommsSendRecv => comms_sendrecv
+
+   use mapl3g_Comms, only: MAPL_CommsGatherV => comms_gatherv
+   use mapl3g_Comms, only: MAPL_CommsScatterV => comms_scatterv
+
+   use mapl3g_Comms, only: MAPL_CommsAllGather => comms_allgather
+   use mapl3g_Comms, only: MAPL_CommsAllGatherV => comms_allgatherv
+
+   use mapl3g_Comms, only: MAPL_ArrayGather => array_gather
+   use mapl3g_Comms, only: MAPL_ArrayScatter => array_scatter
+
+   use mapl3g_Comms, only: MAPL_CommsAllReduceMin => comms_allreduce_min
+   use mapl3g_Comms, only: MAPL_CommsAllReduceMax => comms_allreduce_max
+   use mapl3g_Comms, only: MAPL_CommsAllReduceSum => comms_allreduce_sum
+end module mapl3g_Utilities_Comms_API
