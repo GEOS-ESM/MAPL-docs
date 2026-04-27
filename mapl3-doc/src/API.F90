a25baaf6836bc4fe5fc9180f185f028cfe17e4b1
tclune <thomas.l.clune@nasa.gov>
2026-04-27 16:31:14 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@e2d1976ce18f1d404a8e06ced625f7dccb3a2ee7 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,37 +1,18 @@
-module mapl3g_Geom_API
+module mapl3g_Field_API
+   use mapl3g_FieldGet, only: MAPL_FieldGet => FieldGet
+   use mapl3g_FieldSet, only: MAPL_FieldSet => FieldSet
+   use mapl3g_FieldFill, only: MAPL_FieldFill => FieldFill
+   use mapl3g_FieldCreate
+   use mapl3g_StateItemAllocation
+   use mapl3g_RestartModes
+   use mapl_FieldPointerUtilities, only: MAPL_AssignFptr => assign_fptr
+   use mapl_FieldPointerUtilities, only: MAPL_FieldClone => FieldClone
 
-   use mapl_KeywordEnforcer
-   use mapl3g_MaplGeom, only: MaplGeom
-   use mapl3g_GeomSpec, only: GeomSpec
-   use mapl3g_GeomManager, only: GeomManager, geom_manager, get_geom_manager, get_mapl_geom
-   use mapl3g_GeomUtilities, only: mapl_SameGeom, mapl_GeomGetId
-   use mapl3g_GeomGet, only: mapl_GeomGet => GeomGet
-   use mapl3g_GridGet, only: mapl_GridGet => GridGet, mapl_GridGetCoordinates => GridGetCoordinates
-   use mapl3g_GridGetHorzIJIndex, only: mapl_GridGetHorzIJIndex => GridGetHorzIJIndex
-   use mapl3g_GeomGetHorzIJIndex, only: mapl_GeomGetHorzIJIndex => GeomGetHorzIJIndex
-   use mapl3g_Subgrid, only: mapl_Interval => Interval, mapl_make_subgrids => make_subgrids
-   use mapl3g_XYGeomSpec,    only: XYGeomSpec, make_XYGeomSpec, XY_COORD_STANDARD, XY_COORD_ABI
-   use mapl3g_XYGeomFactory, only: XYGeomFactory
-   use esmf, only: ESMF_Grid, ESMF_Geom, ESMF_KIND_R4
+   ! Internal info should not be exposed to users
+   use mapl3g_FieldInfo
+!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetPrivate
+!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetPrivate
+!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetShared
+!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetShared
 
-   implicit none(type,external)
-
-   private
-
-   ! Available to users
-   public :: mapl_GeomGet
-   public :: mapl_GridGet
-   public :: mapl_GridGetCoordinates
-   public :: mapl_GridGetHorzIJIndex, mapl_GeomGetHorzIJIndex
-
-   ! Used internally by MAPL
-   ! Users shouldn't need these
-   public :: MaplGeom
-   public :: mapl_SameGeom, mapl_GeomGetId
-   public :: GeomManager, geom_manager, get_geom_manager, get_mapl_geom
-   public :: GeomSpec
-   public :: mapl_Interval, mapl_make_subgrids
-   public :: XYGeomSpec, make_XYGeomSpec, XY_COORD_STANDARD, XY_COORD_ABI
-   public :: XYGeomFactory
-
-end module mapl3g_Geom_API
+end module mapl3g_Field_API
