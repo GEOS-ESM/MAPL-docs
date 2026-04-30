0a69968c46a66ba01c898d424de7ba816edab493
mathomp4 <fortran@gmail.com>
2026-04-30 15:27:43 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@d1712fede51d3272089b946c19862ddd59106f1c 🚀

--- mapl3-doc/src/typesafe_make_geom.F90
+++ mapl3-doc/src/typesafe_make_geom.F90
@@ -1,18 +1,16 @@
 #include "MAPL.h"
 
-submodule (mapl3g_EASEGeomFactory) typesafe_make_geom_smod
-   use mapl3g_GeomSpec
-   use mapl3g_EASEGeomSpec
+submodule (mapl3g_XYGeomFactory) typesafe_make_geom_smod
    use mapl_ErrorHandlingMod
+   use mapl_InternalConstants
    use esmf
-
-   implicit none (type, external)
+   implicit none
 
 contains
 
    module function typesafe_make_geom(spec, rc) result(geom)
       type(ESMF_Geom) :: geom
-      type(EASEGeomSpec), intent(in) :: spec
+      type(XYGeomSpec), intent(in) :: spec
       integer, optional, intent(out) :: rc
 
       integer :: status
@@ -20,8 +18,17 @@ contains
       character(:), allocatable :: name
 
       if (spec%has_name()) name = spec%get_name()
-      grid = create_basic_grid(spec, name=name, _RC)
-      call fill_coordinates(spec, grid, _RC)
+      grid = create_basic_grid(spec, _RC)
+
+      select case (spec%get_coord_mode())
+      case (XY_COORD_ABI)
+         call fill_coordinates_abi(spec, grid, _RC)
+      case default
+         call fill_coordinates(spec, grid, _RC)
+      end select
+
+      call add_mask(spec, grid, _RC)
+
       geom = ESMF_GeomCreate(grid=grid, _RC)
 
       _RETURN(_SUCCESS)
