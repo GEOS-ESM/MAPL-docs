9362ccb5233c6686cea766ab3df829e42fcda449
pchakraborty <pchakraborty@users.noreply.github.com>
2026-05-01 18:13:14 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@750ca11a3f98aad119259a7d86ccf9e351d37342 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,9 +1,25 @@
-module mapl3g_HConfig_API
-   use mapl3g_HConfigAs, only: mapl_HConfigAsItemType => HConfigAsItemType
-   use mapl3g_HConfigAs, only: mapl_HConfigAsStateIntent => HConfigAsStateIntent
-   use mapl3g_HConfigAs, only: mapl_HConfigAsTime => HConfigAstime
-   use mapl3g_HConfigAs, only: mapl_HConfigAsTimeInterval => HConfigAsTimeInterval
-   use mapl3g_HConfigAs, only: mapl_HConfigAsStringVector => HConfigAsStringVector
-   implicit none(type,external)
-end module mapl3g_HConfig_API
-   
+module mapl3g_State_API
+   use mapl3g_StateGet, only: MAPL_StateGet => StateGet
+   use mapl3g_StateGetPointer, only: MAPL_StateGetPointer => StateGetPointer
+   use MAPL_StateArithmeticParserMod, only: MAPL_ParserVariablesInExpression => parser_variables_in_expression
+   use mapl3g_StateAddMethod, only: mapl_StateAddMethod
+   use mapl3g_StateAddMethod, only: CallbackMap
+   use mapl3g_StateAddMethod, only: CallbackMapIterator
+   use mapl3g_StateAddMethod, only: CallbackMethodWrapper
+   use mapl3g_StateAddMethod, only: get_callbacks
+   use mapl3g_StateGetGeom, only: MAPL_StateGetGeom => StateGetGeom
+   implicit none
+   private
+
+   ! Available to users
+   public :: MAPL_StateGet
+   public :: MAPL_StateGetPointer
+   public :: MAPL_ParserVariablesInExpression
+   public :: mapl_StateAddMethod
+   public :: CallbackMap
+   public :: CallbackMapIterator
+   public :: CallbackMethodWrapper
+   public :: get_callbacks
+   public :: MAPL_StateGetGeom
+
+end module mapl3g_State_API
