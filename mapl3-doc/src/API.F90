b45156fb8dcf62d4b98e865547459a779b2f1851
tclune <thomas.l.clune@nasa.gov>
2026-05-06 12:20:37 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@ec375a81079ef91f98287351c102d3fd268d1db7 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,18 +1,25 @@
-module mapl3g_Field_API
-   use mapl3g_FieldGet, only: MAPL_FieldGet => FieldGet
-   use mapl3g_FieldSet, only: MAPL_FieldSet => FieldSet
-   use mapl3g_FieldFill, only: MAPL_FieldFill => FieldFill
-   use mapl3g_FieldCreate
-   use mapl3g_StateItemAllocation
-   use mapl3g_RestartModes
-   use mapl_FieldPointerUtilities, only: MAPL_AssignFptr => assign_fptr
-   use mapl_FieldPointerUtilities, only: MAPL_FieldClone => FieldClone
+module mapl3g_State_API
+   use mapl3g_StateGet, only: MAPL_StateGet => StateGet
+   use mapl3g_StateGetPointer, only: MAPL_StateGetPointer => StateGetPointer
+   use MAPL_StateArithmeticParserMod, only: MAPL_ParserVariablesInExpression => parser_variables_in_expression
+   use mapl3g_StateAddMethod, only: mapl_StateAddMethod
+   use mapl3g_StateAddMethod, only: CallbackMap
+   use mapl3g_StateAddMethod, only: CallbackMapIterator
+   use mapl3g_StateAddMethod, only: CallbackMethodWrapper
+   use mapl3g_StateAddMethod, only: get_callbacks
+   use mapl3g_StateGetGeom, only: MAPL_StateGetGeom => StateGetGeom
+   implicit none
+   private
 
-   ! Internal info should not be exposed to users
-   use mapl3g_FieldInfo
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetShared
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetShared
+   ! Available to users
+   public :: MAPL_StateGet
+   public :: MAPL_StateGetPointer
+   public :: MAPL_ParserVariablesInExpression
+   public :: mapl_StateAddMethod
+   public :: CallbackMap
+   public :: CallbackMapIterator
+   public :: CallbackMethodWrapper
+   public :: get_callbacks
+   public :: MAPL_StateGetGeom
 
-end module mapl3g_Field_API
+end module mapl3g_State_API
