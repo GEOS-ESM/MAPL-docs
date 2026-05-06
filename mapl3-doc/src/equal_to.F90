645b665c646a673eec790308023eab5f7286c292
tclune <thomas.l.clune@nasa.gov>
2026-05-06 15:38:46 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@15cadbd1d82e55d049fd7cd2bd9d2b6bb33c4396 🚀

--- mapl3-doc/src/equal_to.F90
+++ mapl3-doc/src/equal_to.F90
@@ -1,26 +1,24 @@
-#include "MAPL_ErrLog.h"
+#include "MAPL.h"
 
-submodule (mapl3g_LocStreamDecomposition) equal_to_smod
+submodule (mapl3g_EASEGeomSpec) equal_to_smod
+   use mapl3g_GeomSpec
+   use mapl3g_EASEDecomposition
    implicit none (type, external)
 
 contains
 
-   module procedure equal_to
-      
-      ! Both must have allocated distributions
-      if (.not. allocated(decomp1%point_distribution)) then
-         equal_to = .not. allocated(decomp2%point_distribution)
-         return
-      end if
+   pure logical module function equal_to(a, b)
+      class(EASEGeomSpec), intent(in) :: a
+      class(GeomSpec),     intent(in) :: b
 
-      if (.not. allocated(decomp2%point_distribution)) then
+      select type (b)
+      type is (EASEGeomSpec)
+         equal_to = (a%grid_name == b%grid_name) .and. &
+                    (a%decomposition == b%decomposition)
+      class default
          equal_to = .false.
-         return
-      end if
+      end select
 
-      ! Check if distributions are the same
-      equal_to = all(decomp1%point_distribution == decomp2%point_distribution)
-
-   end procedure equal_to
+   end function equal_to
 
 end submodule equal_to_smod
