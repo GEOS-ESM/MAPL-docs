8ba03e2c1331764ed15dcc5ecd1471d5e0deeebf
mathomp4 <fortran@gmail.com>
2026-05-07 20:51:45 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@19855633ac24a03b73448c2b6b473e04f2c52463 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,18 +1,9 @@
-module mapl3g_Field_API
-   use mapl3g_FieldGet, only: MAPL_FieldGet => FieldGet
-   use mapl3g_FieldSet, only: MAPL_FieldSet => FieldSet
-   use mapl3g_FieldFill, only: MAPL_FieldFill => FieldFill
-   use mapl3g_FieldCreate
-   use mapl3g_StateItemAllocation
-   use mapl3g_RestartModes
-   use mapl_FieldPointerUtilities, only: MAPL_AssignFptr => assign_fptr
-   use mapl_FieldPointerUtilities, only: MAPL_FieldClone => FieldClone
-
-   ! Internal info should not be exposed to users
-   use mapl3g_FieldInfo
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetPrivate
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetShared
-!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetShared
-
-end module mapl3g_Field_API
+module mapl3g_HConfig_API
+   use mapl3g_HConfigAs, only: mapl_HConfigAsItemType => HConfigAsItemType
+   use mapl3g_HConfigAs, only: mapl_HConfigAsStateIntent => HConfigAsStateIntent
+   use mapl3g_HConfigAs, only: mapl_HConfigAsTime => HConfigAstime
+   use mapl3g_HConfigAs, only: mapl_HConfigAsTimeInterval => HConfigAsTimeInterval
+   use mapl3g_HConfigAs, only: mapl_HConfigAsStringVector => HConfigAsStringVector
+   implicit none(type,external)
+end module mapl3g_HConfig_API
+   
