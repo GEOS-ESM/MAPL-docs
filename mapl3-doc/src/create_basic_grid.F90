5b7bd3ef844fcb1d8273c8bf90a377a2d0a28219
tclune <thomas.l.clune@nasa.gov>
2026-05-09 01:07:48 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@cc374b35d75e254d0307e86d3416932ababae62a 🚀

--- mapl3-doc/src/create_basic_grid.F90
+++ mapl3-doc/src/create_basic_grid.F90
@@ -1,59 +1,57 @@
 #include "MAPL.h"
 
-submodule (mapl3g_XYGeomFactory) create_basic_grid_smod
+submodule (mapl3g_EASEGeomFactory) create_basic_grid_smod
+   use mapl3g_GeomSpec
+   use mapl3g_EASEGeomSpec
+   use mapl3g_EASEDecomposition
    use mapl_ErrorHandlingMod
-   use mapl_InternalConstants
    use esmf
-   implicit none
+   use mapl_KeywordEnforcer, only: KE => KeywordEnforcer
+   implicit none (type, external)
 
 contains
 
-   module function create_basic_grid(spec, unusable, rc) result(grid)
+   ! Build a periodic-in-longitude ESMF_Grid for an EASE cylindrical grid.
+   ! EASE grids:
+   !   - Are periodic in longitude ('DE' dateline: dateline on cell edge)
+   !   - Have no poles ('XY': poles outside the grid domain)
+   !   - Need 2D coordinate arrays (coordDep1/2 = [1,2])
+   module function create_basic_grid(spec, unusable, name, rc) result(grid)
       type(ESMF_Grid) :: grid
-      type(XYGeomSpec), intent(in) :: spec
+      type(EASEGeomSpec), intent(in) :: spec
       class(KE), optional, intent(in) :: unusable
+      character(len=*), optional, intent(in) :: name
       integer, optional, intent(out) :: rc
 
       integer :: status
-      type(ESMF_Info) :: infoh
+      integer, allocatable :: ims(:), jms(:)
+      type(EASEDecomposition) :: decomposition
+      type(ESMF_PoleKind_Flag) :: polekindflag(2)
 
-      if (spec%get_n_peri_dim() == 0) then
-         grid = ESMF_GridCreateNoPeriDim( &
-              countsPerDEDim1=spec%get_ims(), &
-              countsPerDEDim2=spec%get_jms(), &
-              indexFlag=ESMF_INDEX_DELOCAL, &
-              gridEdgeLWidth=[0,0], &
-              gridEdgeUWidth=[0,1], &
-              coordDep1=[1,2], &
-              coordDep2=[1,2], &
-              coordSys=ESMF_COORDSYS_SPH_RAD, _RC)
-      else
-         grid = ESMF_GridCreate1PeriDim( &
-              countsPerDEDim1=spec%get_ims(), &
-              countsPerDEDim2=spec%get_jms(), &
-              poleKindFlag=[ESMF_POLEKIND_MONOPOLE, ESMF_POLEKIND_BIPOLE], &
-              indexFlag=ESMF_INDEX_DELOCAL, &
-              gridEdgeLWidth=[0,0], &
-              gridEdgeUWidth=[0,1], &
-              coordDep1=[1,2], &
-              coordDep2=[1,2], &
-              coordSys=ESMF_COORDSYS_SPH_RAD, _RC)
-      end if
+      ! No poles
+      polekindflag = ESMF_POLEKIND_NONE
 
-      ! Allocate centre coordinates
+      ! Use the decomposition stored in the spec.
+      decomposition = spec%get_decomposition()
+      ims = decomposition%get_lon_distribution()
+      jms = decomposition%get_lat_distribution()
+
+      grid = ESMF_GridCreate1PeriDim( &
+           & name            = name,              &
+           & countsPerDEDim1 = ims,               &
+           & countsPerDEDim2 = jms,               &
+           & indexFlag       = ESMF_INDEX_DELOCAL, &
+           & gridEdgeLWidth  = [0, 0],             &
+           & gridEdgeUWidth  = [0, 1],             &
+           & coordDep1       = [1, 2],             &
+           & coordDep2       = [1, 2],             &
+           & coordSys        = ESMF_COORDSYS_SPH_DEG, &
+           & polekindflag    = polekindflag,       &
+           & _RC)
+
+      ! Allocate coordinate storage at center and corner stagger locations
       call ESMF_GridAddCoord(grid, _RC)
-
-      ! Optionally allocate corner coordinates
-      if (spec%get_has_corners()) then
-         call ESMF_GridAddCoord(grid, staggerloc=ESMF_STAGGERLOC_CORNER, _RC)
-      end if
-
-      ! Tag the grid with type metadata for restart identification
-      call ESMF_InfoGetFromHost(grid, infoh, _RC)
-      if (spec%get_lm() /= MAPL_UNDEFINED_INTEGER) then
-         call ESMF_InfoSet(infoh, 'GRID_LM', spec%get_lm(), _RC)
-      end if
-      call ESMF_InfoSet(infoh, 'GridType', 'XY', _RC)
+      call ESMF_GridAddCoord(grid, staggerloc=ESMF_STAGGERLOC_CORNER, _RC)
 
       _RETURN(_SUCCESS)
       _UNUSED_DUMMY(unusable)
