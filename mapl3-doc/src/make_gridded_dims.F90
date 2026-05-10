e39da32aa7a9692b69db3969feebb8e1b2ba5b53
tclune <thomas.l.clune@nasa.gov>
2026-05-10 01:31:22 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@616bd7268ac8262ad66a761ceae7a44e18df978f 🚀

--- mapl3-doc/src/make_gridded_dims.F90
+++ mapl3-doc/src/make_gridded_dims.F90
@@ -1,25 +1,27 @@
 #include "MAPL.h"
 
-submodule (mapl3g_XYGeomFactory) make_gridded_dims_smod
+submodule (mapl3g_EASEGeomFactory) make_gridded_dims_smod
+   use mapl3g_GeomSpec
+   use mapl3g_EASEGeomSpec
    use mapl_ErrorHandlingMod
-   use gftl2_StringVector
-   implicit none
+   use gFTL2_StringVector
+   implicit none (type, external)
 
 contains
 
    module function make_gridded_dims(this, geom_spec, rc) result(gridded_dims)
       type(StringVector) :: gridded_dims
-      class(XYGeomFactory), intent(in) :: this
+      class(EASEGeomFactory), intent(in) :: this
       class(GeomSpec), intent(in) :: geom_spec
       integer, optional, intent(out) :: rc
 
       gridded_dims = StringVector()
       select type (geom_spec)
-      type is (XYGeomSpec)
-         call gridded_dims%push_back('Xdim')
-         call gridded_dims%push_back('Ydim')
+      type is (EASEGeomSpec)
+         call gridded_dims%push_back('lon')
+         call gridded_dims%push_back('lat')
       class default
-         _FAIL('geom_spec is not of dynamic type XYGeomSpec.')
+         _FAIL('make_gridded_dims: geom_spec is not of dynamic type EASEGeomSpec')
       end select
 
       _RETURN(_SUCCESS)
