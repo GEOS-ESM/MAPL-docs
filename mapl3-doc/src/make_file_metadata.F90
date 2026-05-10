b2678561f4b98d94b025eac3a024533384fd72fd
tclune <thomas.l.clune@nasa.gov>
2026-05-10 13:01:39 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@04f00bbc683a3e87fad723616a2a61bf2b36b38a 🚀

--- mapl3-doc/src/make_file_metadata.F90
+++ mapl3-doc/src/make_file_metadata.F90
@@ -1,30 +1,20 @@
 #include "MAPL.h"
 
-submodule (mapl3g_LatLonGeomFactory) make_file_metadata_smod
-
-   use mapl3g_GeomSpec
-   use mapl3g_LonAxis
-   use mapl3g_LatAxis
-   use mapl3g_LatLonDecomposition
-   use mapl3g_LatLonGeomSpec
-   use mapl_MinMaxMod
+submodule (mapl3g_XYGeomFactory) make_file_metadata_smod
    use mapl_ErrorHandlingMod
-   use mapl_Constants
-   use pFIO
-   use gFTL2_StringVector
-   use esmf
    use mapl_KeywordEnforcer, only: KE => KeywordEnforcer
-
-   implicit none (type, external)
+   use pfio
+   use esmf
+   implicit none
 
 contains
 
    module function make_file_metadata(this, geom_spec, unusable, chunksizes, rc) result(file_metadata)
       type(FileMetadata) :: file_metadata
-      class(LatLonGeomFactory), intent(in) :: this
+      class(XYGeomFactory), intent(in) :: this
+      class(GeomSpec), intent(in) :: geom_spec
       class(KE), optional, intent(in) :: unusable
       integer, optional, intent(in) :: chunksizes(:)
-      class(GeomSpec), intent(in) :: geom_spec
       integer, optional, intent(out) :: rc
 
       integer :: status
@@ -32,10 +22,10 @@ contains
       file_metadata = FileMetadata()
 
       select type (geom_spec)
-      type is (LatLonGeomSpec)
+      type is (XYGeomSpec)
          file_metadata = typesafe_make_file_metadata(geom_spec, chunksizes=chunksizes, _RC)
       class default
-         _FAIL('geom_spec is not of dynamic type LatLonGeomSpec.')
+         _FAIL('geom_spec is not of dynamic type XYGeomSpec.')
       end select
 
       _RETURN(_SUCCESS)
--- mapl3-doc/src/supports_metadata.F90
+++ mapl3-doc/src/supports_metadata.F90
@@ -1,38 +1,49 @@
 #include "MAPL.h"
 
-submodule (mapl3g_EASEGeomSpec) supports_metadata_smod
+submodule (mapl3g_LatLonGeomSpec) supports_metadata_smod
+
+   use mapl3g_CoordinateAxis
    use mapl3g_GeomSpec
-   use mapl3g_EASEConversion
    use pfio
    use mapl_ErrorHandling
+   use esmf
+
    implicit none (type, external)
 
 contains
 
-   ! A FileMetadata object is an EASE grid if its 'lon' (or 'longitude')
-   ! dimension count matches one of the known EASE grid column counts.
    logical module function supports_metadata_(this, file_metadata, rc) result(supports)
-      class(EASEGeomSpec), intent(in) :: this
-      type(FileMetadata),  intent(in) :: file_metadata
-      integer, optional,   intent(out) :: rc
+      class(LatLonGeomSpec), intent(in) :: this
+      type(FileMetadata), intent(in) :: file_metadata
+      integer, optional, intent(out) :: rc
 
-      integer :: status, im
-      character(:), allocatable :: grid_name
+      integer :: status
+      type(LonAxis) :: lon_axis
+      type(LatAxis) :: lat_axis
+      character(:), allocatable :: lon_dim, lat_dim
 
       supports = .false.
 
-      ! Try 'lon' first, then 'longitude'
-      if (file_metadata%has_dimension('lon')) then
-         im = file_metadata%get_dimension('lon', _RC)
-      else if (file_metadata%has_dimension('longitude')) then
-         im = file_metadata%get_dimension('longitude', _RC)
-      else
-         _RETURN(_SUCCESS)
-      end if
+      ! Require that both longitude and latitude axes are
+      ! supported in the usual way.
 
-      ! If get_ease_gridname_by_cols succeeds, this is a recognized EASE grid
-      grid_name = get_ease_gridname_by_cols(im, rc=status)
-      supports = (status == _SUCCESS)
+      supports = lon_axis%supports(file_metadata, _RC)
+      _RETURN_UNLESS(supports)
+
+      supports = lat_axis%supports(file_metadata, _RC)
+      _RETURN_UNLESS(supports)
+
+      ! Distinguish regular LatLon grids from LocStreams. For
+      ! LatLon we expect distinct latitude and longitude
+      ! dimensions (e.g. lat x lon), whereas LocStreams share a
+      ! single dimension for both coordinates. If both
+      ! coordinates share the same dimension, consider this not
+      ! a LatLon grid so that LocStream factories can claim it.
+
+      lon_dim = get_dim_name(file_metadata, units='degrees_east', _RC)
+      lat_dim = get_dim_name(file_metadata, units='degrees_north', _RC)
+
+      supports = (lon_dim /= '' .and. lat_dim /= '' .and. lon_dim /= lat_dim)
 
       _RETURN(_SUCCESS)
       _UNUSED_DUMMY(this)
