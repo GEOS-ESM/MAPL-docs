dad170d128dfffcb33c8bcb50fdf22c7a2069958
tclune <thomas.l.clune@nasa.gov>
2026-05-11 14:45:06 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@6202f90d934fc0105830e64cbc13dfdcda499fb8 🚀

--- mapl3-doc/src/equal_to.F90
+++ mapl3-doc/src/equal_to.F90
@@ -1,24 +1,24 @@
 #include "MAPL.h"
 
-submodule (mapl3g_XYGeomSpec) equal_to_smod
-   use mapl_ErrorHandlingMod
-   implicit none
-
+submodule (mapl3g_LatLonGeomSpec) equal_to_smod
+   use mapl3g_CoordinateAxis
+   use mapl3g_GeomSpec
+   use pfio
+   use mapl_ErrorHandling
+   use esmf
+   implicit none (type, external)
+   
 contains
 
    pure logical module function equal_to(a, b)
-      class(XYGeomSpec), intent(in) :: a
-      class(GeomSpec),   intent(in) :: b
+      class(LatLonGeomSpec), intent(in) :: a
+      class(GeomSpec), intent(in) :: b
 
       select type (b)
-      type is (XYGeomSpec)
-         equal_to = (a%im_world    == b%im_world)    .and. &
-                    (a%jm_world    == b%jm_world)    .and. &
-                    (a%lm          == b%lm)          .and. &
-                    (a%n_peri_dim  == b%n_peri_dim)  .and. &
-                    (a%coord_mode  == b%coord_mode)  .and. &
-                    (a%thin_factor == b%thin_factor) .and. &
-                    (a%grid_file_name == b%grid_file_name)
+      type is (LatLonGeomSpec)
+         equal_to = (a%lon_axis == b%lon_axis) .and. (a%lat_axis == b%lat_axis)
+         if (.not. equal_to) return
+         equal_to = (a%decomposition == b%decomposition)
       class default
          equal_to = .false.
       end select
--- mapl3-doc/src/typesafe_make_geom.F90
+++ mapl3-doc/src/typesafe_make_geom.F90
@@ -1,16 +1,18 @@
 #include "MAPL.h"
 
-submodule (mapl3g_XYGeomFactory) typesafe_make_geom_smod
+submodule (mapl3g_EASEGeomFactory) typesafe_make_geom_smod
+   use mapl3g_GeomSpec
+   use mapl3g_EASEGeomSpec
    use mapl_ErrorHandlingMod
-   use mapl_InternalConstants
    use esmf
-   implicit none
+
+   implicit none (type, external)
 
 contains
 
    module function typesafe_make_geom(spec, rc) result(geom)
       type(ESMF_Geom) :: geom
-      type(XYGeomSpec), intent(in) :: spec
+      type(EASEGeomSpec), intent(in) :: spec
       integer, optional, intent(out) :: rc
 
       integer :: status
@@ -18,17 +20,8 @@ contains
       character(:), allocatable :: name
 
       if (spec%has_name()) name = spec%get_name()
-      grid = create_basic_grid(spec, _RC)
-
-      select case (spec%get_coord_mode())
-      case (XY_COORD_ABI)
-         call fill_coordinates_abi(spec, grid, _RC)
-      case default
-         call fill_coordinates(spec, grid, _RC)
-      end select
-
-      call add_mask(spec, grid, _RC)
-
+      grid = create_basic_grid(spec, name=name, _RC)
+      call fill_coordinates(spec, grid, _RC)
       geom = ESMF_GeomCreate(grid=grid, _RC)
 
       _RETURN(_SUCCESS)
