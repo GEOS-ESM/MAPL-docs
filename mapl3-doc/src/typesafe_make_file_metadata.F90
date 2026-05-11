22c5ec089c150d8379fe48775b30a27669d82297
tclune <thomas.l.clune@nasa.gov>
2026-05-11 12:50:39 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@3f78a0636b6557517be1a1b8b7ebe86c96cbf972 🚀

--- mapl3-doc/src/typesafe_make_file_metadata.F90
+++ mapl3-doc/src/typesafe_make_file_metadata.F90
@@ -1,51 +1,83 @@
 #include "MAPL.h"
 
-submodule (mapl3g_EASEGeomFactory) typesafe_make_file_metadata_smod
-   use mapl3g_GeomSpec
-   use mapl3g_EASEGeomSpec
-   use mapl3g_EASECoords
+submodule (mapl3g_XYGeomFactory) typesafe_make_file_metadata_smod
    use mapl_ErrorHandlingMod
    use pfio
-   use mapl_KeywordEnforcer, only: KE => KeywordEnforcer
+   use esmf
    use, intrinsic :: iso_fortran_env, only: REAL64
-   implicit none (type, external)
+   implicit none
 
 contains
 
-   module function typesafe_make_file_metadata(geom_spec, unusable, chunksizes, rc) result(file_metadata)
+   module function typesafe_make_file_metadata(spec, unusable, chunksizes, rc) result(file_metadata)
       type(FileMetadata) :: file_metadata
-      type(EASEGeomSpec), intent(in) :: geom_spec
+      type(XYGeomSpec), intent(in) :: spec
       class(KE), optional, intent(in) :: unusable
       integer, optional, intent(in) :: chunksizes(:)
       integer, optional, intent(out) :: rc
 
-      integer :: status
-      integer :: im, jm
+      integer :: im, jm, i
       type(Variable) :: v
-      real(kind=REAL64), allocatable :: lon_cen(:), lat_cen(:)
+      real(kind=REAL64), allocatable :: fake_coord(:)
 
-      call compute_lons(geom_spec, centers=lon_cen, _RC)
-      call compute_lats(geom_spec, centers=lat_cen, _RC)
+      im = spec%get_im_world()
+      jm = spec%get_jm_world()
 
-      im = geom_spec%get_im_world(_RC)
-      jm = geom_spec%get_jm_world(_RC)
+      call file_metadata%add_dimension('Xdim', im)
+      call file_metadata%add_dimension('Ydim', jm)
 
-      call file_metadata%add_dimension('lon', im)
-      call file_metadata%add_dimension('lat', jm)
+      if (spec%get_has_corners()) then
+         call file_metadata%add_dimension('XCdim', im+1)
+         call file_metadata%add_dimension('YCdim', jm+1)
+      end if
 
-      ! Longitude coordinate variable
-      v = Variable(type=PFIO_REAL64, dimensions='lon', chunksizes=chunksizes)
+      ! Fake 1-D coordinate variables for GrADS/Panoply compatibility
+      allocate(fake_coord(im))
+      do i = 1, im
+         fake_coord(i) = dble(i)
+      end do
+      v = Variable(type=PFIO_REAL64, dimensions='Xdim', chunksizes=chunksizes)
+      call v%add_attribute('long_name', 'Fake Longitude for GrADS Compatibility')
+      call v%add_attribute('units', 'degrees_east')
+      call v%add_const_value(UnlimitedEntity(fake_coord))
+      call file_metadata%add_variable('Xdim', v)
+      deallocate(fake_coord)
+
+      allocate(fake_coord(jm))
+      do i = 1, jm
+         fake_coord(i) = dble(i)
+      end do
+      v = Variable(type=PFIO_REAL64, dimensions='Ydim', chunksizes=chunksizes)
+      call v%add_attribute('long_name', 'Fake Latitude for GrADS Compatibility')
+      call v%add_attribute('units', 'degrees_north')
+      call v%add_const_value(UnlimitedEntity(fake_coord))
+      call file_metadata%add_variable('Ydim', v)
+      deallocate(fake_coord)
+
+      ! 2-D actual coordinate variables
+      v = Variable(type=PFIO_REAL64, dimensions='Xdim,Ydim', chunksizes=chunksizes)
       call v%add_attribute('long_name', 'longitude')
       call v%add_attribute('units', 'degrees_east')
-      call v%add_const_value(UnlimitedEntity(lon_cen))
-      call file_metadata%add_variable('lon', v)
+      call file_metadata%add_variable('lons', v)
 
-      ! Latitude coordinate variable
-      v = Variable(type=PFIO_REAL64, dimensions='lat', chunksizes=chunksizes)
+      v = Variable(type=PFIO_REAL64, dimensions='Xdim,Ydim', chunksizes=chunksizes)
       call v%add_attribute('long_name', 'latitude')
       call v%add_attribute('units', 'degrees_north')
-      call v%add_const_value(UnlimitedEntity(lat_cen))
-      call file_metadata%add_variable('lat', v)
+      call file_metadata%add_variable('lats', v)
+
+      if (spec%get_has_corners()) then
+         v = Variable(type=PFIO_REAL64, dimensions='XCdim,YCdim', chunksizes=chunksizes)
+         call v%add_attribute('long_name', 'longitude')
+         call v%add_attribute('units', 'degrees_east')
+         call file_metadata%add_variable('corner_lons', v)
+
+         v = Variable(type=PFIO_REAL64, dimensions='XCdim,YCdim', chunksizes=chunksizes)
+         call v%add_attribute('long_name', 'latitude')
+         call v%add_attribute('units', 'degrees_north')
+         call file_metadata%add_variable('corner_lats', v)
+      end if
+
+      call file_metadata%add_attribute('grid_type', 'XY')
 
       _RETURN(_SUCCESS)
       _UNUSED_DUMMY(unusable)
--- mapl3-doc/src/typesafe_make_geom.F90
+++ mapl3-doc/src/typesafe_make_geom.F90
@@ -1,16 +1,25 @@
 #include "MAPL.h"
-
-submodule (mapl3g_XYGeomFactory) typesafe_make_geom_smod
+submodule (mapl3g_LatLonGeomFactory) typesafe_make_geom_smod
+   use mapl3g_GeomSpec
+   use mapl3g_LonAxis
+   use mapl3g_LatAxis
+   use mapl3g_LatLonDecomposition
+   use mapl3g_LatLonGeomSpec
+   use mapl_MinMaxMod
    use mapl_ErrorHandlingMod
-   use mapl_InternalConstants
+   use mapl_Constants
+   use pFIO
+   use gFTL2_StringVector
    use esmf
-   implicit none
+   use mapl_KeywordEnforcer, only: KE => KeywordEnforcer
+   implicit none (type, external)
+
 
 contains
 
    module function typesafe_make_geom(spec, rc) result(geom)
       type(ESMF_Geom) :: geom
-      type(XYGeomSpec), intent(in) :: spec
+      class(LatLonGeomSpec), intent(in) :: spec
       integer, optional, intent(out) :: rc
 
       integer :: status
@@ -18,17 +27,8 @@ contains
       character(:), allocatable :: name
 
       if (spec%has_name()) name = spec%get_name()
-      grid = create_basic_grid(spec, _RC)
-
-      select case (spec%get_coord_mode())
-      case (XY_COORD_ABI)
-         call fill_coordinates_abi(spec, grid, _RC)
-      case default
-         call fill_coordinates(spec, grid, _RC)
-      end select
-
-      call add_mask(spec, grid, _RC)
-
+      grid = create_basic_grid(spec, name=name, _RC)
+      call fill_coordinates(spec, grid, _RC)
       geom = ESMF_GeomCreate(grid=grid, _RC)
 
       _RETURN(_SUCCESS)
