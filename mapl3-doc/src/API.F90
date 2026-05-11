0b18fc2bfeab9be3979448a492555077f57f852d
tclune <thomas.l.clune@nasa.gov>
2026-05-11 15:45:16 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@073321b7b88b2517cee29e7b3a7b5be944162a5b 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,9 +1,3 @@
-module mapl3g_HConfig_API
-   use mapl3g_HConfigAs, only: mapl_HConfigAsItemType => HConfigAsItemType
-   use mapl3g_HConfigAs, only: mapl_HConfigAsStateIntent => HConfigAsStateIntent
-   use mapl3g_HConfigAs, only: mapl_HConfigAsTime => HConfigAstime
-   use mapl3g_HConfigAs, only: mapl_HConfigAsTimeInterval => HConfigAsTimeInterval
-   use mapl3g_HConfigAs, only: mapl_HConfigAsStringVector => HConfigAsStringVector
-   implicit none(type,external)
-end module mapl3g_HConfig_API
-   
+module mapl3g_VM_API
+   use mapl3g_vm
+end module mapl3g_VM_API
