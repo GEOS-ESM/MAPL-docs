dc2d633029c9fc94ab2332ae8caac2920a84cdb6
pchakraborty <pchakraborty@users.noreply.github.com>
2026-05-19 16:49:26 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@ab5de00da138eb135479735b496a4410cb285dcc 🚀

--- mapl3-doc/src/supports_metadata.F90
+++ mapl3-doc/src/supports_metadata.F90
@@ -1,41 +1,29 @@
 #include "MAPL.h"
 
-submodule (mapl3g_EASEGeomSpec) supports_metadata_smod
-   use mapl3g_GeomSpec
-   use mapl3g_EASEConversion
-   use pfio
+submodule (mapl3g_LatAxis) supports_metadata_smod
+   use mapl_RangeMod
+!   use hconfig3g
+   use esmf
    use mapl_ErrorHandling
    implicit none (type, external)
 
+   integer, parameter :: R8 = ESMF_KIND_R8
+
 contains
 
-   ! A FileMetadata object is an EASE grid if its 'lon' (or 'longitude')
-   ! dimension count matches one of the known EASE grid column counts.
-   logical module function supports_metadata_(this, file_metadata, rc) result(supports)
-      class(EASEGeomSpec), intent(in) :: this
-      type(FileMetadata),  intent(in) :: file_metadata
-      integer, optional,   intent(out) :: rc
+   logical module function supports_metadata(file_metadata, rc) result(supports)
+      type(FileMetadata), intent(in) :: file_metadata
+      integer, optional, intent(out) :: rc
 
-      integer :: status, im
-      character(:), allocatable :: grid_name
+      integer :: status
+      character(:), allocatable :: dim_name
 
-      supports = .false.
+      supports = .true.
+      dim_name = get_dim_name(file_metadata, units='degrees_north', _RC)
 
-      ! Try 'lon' first, then 'longitude'
-      if (file_metadata%has_dimension('lon')) then
-         im = file_metadata%get_dimension('lon', _RC)
-      else if (file_metadata%has_dimension('longitude')) then
-         im = file_metadata%get_dimension('longitude', _RC)
-      else
-         _RETURN(_SUCCESS)
-      end if
-
-      ! If get_ease_gridname_by_cols succeeds, this is a recognized EASE grid
-      grid_name = get_ease_gridname_by_cols(im, rc=status)
-      supports = (status == _SUCCESS)
-
+      supports = (dim_name /= '')
       _RETURN(_SUCCESS)
-      _UNUSED_DUMMY(this)
-   end function supports_metadata_
+   end function supports_metadata
 
 end submodule supports_metadata_smod
+
