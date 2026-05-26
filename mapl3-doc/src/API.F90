f9c9a22af804dbec68e3e0510cbd0734a98f1bdd
mathomp4 <fortran@gmail.com>
2026-05-26 13:11:25 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@6e9c047022fc724f9a95285d6b86e803f85917e5 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,15 +1,54 @@
-module mapl_Field_API
-   use mapl_FieldGetImpl_mod, only: MAPL_FieldGet => FieldGet
-   use mapl_FieldSetImpl_mod, only: MAPL_FieldSet => FieldSet
-   use mapl_FieldFillImpl_mod, only: MAPL_FieldFill => FieldFill
-   use mapl_FieldCreateImpl_mod
-   use mapl_StateItemAllocation_mod
-   use mapl_RestartModes_mod
-   use mapl_FieldPointerUtilities_mod, only: MAPL_AssignFptr => assign_fptr
-   use mapl_FieldPointerUtilities_mod, only: MAPL_FieldClone => FieldClone
-   ! Internal info should not be exposed to users
-   use mapl_FieldInfo_mod
+module mapl_VerticalGrid_API_mod
+   use mapl_VerticalGrid_mod, only: VerticalGrid
+   use mapl_VerticalGrid_mod, only: VERTICAL_GRID_NOT_FOUND
+   use mapl_VerticalGridSpec_mod, only: VerticalGridSpec
+   use mapl_VerticalGridFactory_mod, only: VerticalGridFactory
+   use mapl_VerticalGridManager_mod, only: VerticalGridManager
+   use mapl_VerticalGridManager_mod, only: get_vertical_grid_manager
+   use mapl_IntegerPair_mod, only: IntegerPair
+   use mapl_VerticalStaggerLoc_mod
+   use mapl_VerticalAlignment_mod
+   use mapl_BasicVerticalGrid_mod, only: BasicVerticalGrid
+   use mapl_BasicVerticalGrid_mod, only: BasicVerticalGridSpec
+   use mapl_BasicVerticalGrid_mod, only: BasicVerticalGridFactory
+   implicit none(type,external)
+   private
 
-   public :: MAPL_FieldClone
+   ! Abstract base types
+   public :: VerticalGrid
+   public :: VerticalGridSpec
+   public :: VerticalGridFactory
 
-end module mapl_Field_API
+   ! Manager
+   public :: VerticalGridManager
+   public :: get_vertical_grid_manager
+   
+   ! Utility types
+   public :: IntegerPair
+   
+   ! Vertical stagger locations
+   public :: VerticalStaggerLoc
+   public :: operator(==), operator(/=)
+   public :: VERTICAL_STAGGER_NONE
+   public :: VERTICAL_STAGGER_EDGE
+   public :: VERTICAL_STAGGER_CENTER
+   public :: VERTICAL_STAGGER_MIRROR
+   public :: VERTICAL_STAGGER_INVALID
+   
+   ! Vertical alignment
+   public :: VerticalAlignment
+   public :: VALIGN_WITH_GRID
+   public :: VALIGN_UP
+   public :: VALIGN_DOWN
+   public :: VALIGN_INVALID
+   
+   ! Basic grid types
+   public :: BasicVerticalGrid
+   public :: BasicVerticalGridSpec
+   public :: BasicVerticalGridFactory
+
+   ! Parameters
+   public :: VERTICAL_GRID_NOT_FOUND
+  
+   
+end module mapl_VerticalGrid_API_mod
