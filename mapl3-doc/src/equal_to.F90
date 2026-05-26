845e907f23684c3b04b9f515e7d82224c994a63e
tclune <thomas.l.clune@nasa.gov>
2026-05-26 18:50:38 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@c837325ed0208108dc1c4bbc96a98fffe8c88d14 🚀

--- mapl3-doc/src/equal_to.F90
+++ mapl3-doc/src/equal_to.F90
@@ -1,24 +1,20 @@
 #include "MAPL.h"
 
-submodule (mapl_XYGeomSpec_mod) equal_to_smod
-   use mapl_ErrorHandling_mod
-   implicit none
+submodule (mapl_EASEGeomSpec_mod) equal_to_smod
+   use mapl_GeomSpec_mod
+   use mapl_EASEDecomposition_mod
+   implicit none (type, external)
 
 contains
 
    pure logical module function equal_to(a, b)
-      class(XYGeomSpec), intent(in) :: a
-      class(GeomSpec),   intent(in) :: b
+      class(EASEGeomSpec), intent(in) :: a
+      class(GeomSpec),     intent(in) :: b
 
       select type (b)
-      type is (XYGeomSpec)
-         equal_to = (a%im_world    == b%im_world)    .and. &
-                    (a%jm_world    == b%jm_world)    .and. &
-                    (a%lm          == b%lm)          .and. &
-                    (a%n_peri_dim  == b%n_peri_dim)  .and. &
-                    (a%coord_mode  == b%coord_mode)  .and. &
-                    (a%thin_factor == b%thin_factor) .and. &
-                    (a%grid_file_name == b%grid_file_name)
+      type is (EASEGeomSpec)
+         equal_to = (a%grid_name == b%grid_name) .and. &
+                    (a%decomposition == b%decomposition)
       class default
          equal_to = .false.
       end select
