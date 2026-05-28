b83cb324735a8a9df635cc5dde6d3083a273c7f2
tclune <thomas.l.clune@nasa.gov>
2026-05-28 15:51:03 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@96000aaf56ebe58a73747468591a3ea8b8d3767f 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,56 +1,25 @@
-module mapl_FieldBundle_API_mod
-
-   use ESMF, only: MAPL_FieldBundleAdd => ESMF_FieldBundleAdd
-   use mapl_FieldBundleType_Flag_mod
-   use mapl_VectorBasisKind_mod
-   use mapl_FieldBundleCreateImpl_mod, only: MAPL_FieldBundleCreate => FieldBundleCreate
-   use mapl_FieldBundleCreateImpl_mod, only: MAPL_FieldBundlesAreAliased => FieldBundlesAreAliased
-   use mapl_FieldBundleDestroyImpl_mod, only: MAPL_FieldBundleDestroy
-   use mapl_FieldBundleGetImpl_mod, only: MAPL_FieldBundleGet => FieldBundleGet
-   use mapl_FieldBundleGetByIndexImpl_mod, only: MAPL_FieldBundleGetByIndex => FieldBundleGetByIndex
-   use mapl_FieldBundleSetImpl_mod, only: MAPL_FieldBundleSet => FieldBundleSet
-   use mapl_FieldBundleInfo_mod, only: MAPL_FieldBundleInfoGetInternal => FieldBundleInfoGetInternal
-   use mapl_FieldBundleInfo_mod, only: MAPL_FieldBundleInfoSetInternal => FieldBundleInfoSetInternal
-   use mapl_FieldBundleGetPointerImpl_mod, only: MAPL_FieldBundleGetPointer => FieldBundleGetPointerToData
-   use mapl_FieldBundleCopyImpl_mod, only: MAPL_FieldBundleCopy => FieldBundleCopy
-   use mapl_FieldBundleMatch_mod, only: MAPL_FieldBundleSameData => FieldBundleSameData
-
+module mapl_State_API_mod
+   use mapl_StateGetImpl_mod, only: MAPL_StateGet => StateGet
+   use mapl_StateGetPointerImpl_mod, only: MAPL_StateGetPointer => StateGetPointer
+   use mapl_StateArithmeticParser_mod, only: MAPL_ParserVariablesInExpression => parser_variables_in_expression
+   use mapl_StateAddMethodImpl_mod, only: mapl_StateAddMethod
+   use mapl_StateAddMethodImpl_mod, only: CallbackMap
+   use mapl_StateAddMethodImpl_mod, only: CallbackMapIterator
+   use mapl_StateAddMethodImpl_mod, only: CallbackMethodWrapper
+   use mapl_StateAddMethodImpl_mod, only: get_callbacks
+   use mapl_StateGetGeomImpl_mod, only: MAPL_StateGetGeom => StateGetGeom
    implicit none
-
    private
 
    ! Available to users
-   public :: MAPL_FieldBundleCreate
-   public :: MAPL_FieldBundlesAreAliased
-   public :: MAPL_FieldBundleDestroy
-   public :: MAPL_FieldBundleGet
-   public :: MAPL_FieldBundleGetByIndex
-   public :: MAPL_FieldBundleSet
-   public :: MAPL_FieldBundleAdd
-   public :: MAPL_FieldBundleGetPointer
-   public :: MAPL_FieldBundleCopy
-   public :: MAPL_FieldBundleSameData
-   ! Maybe these should be private?
-   public :: MAPL_FieldBundleInfoGetInternal
-   public :: MAPL_FieldBundleInfoSetInternal
+   public :: MAPL_StateGet
+   public :: MAPL_StateGetPointer
+   public :: MAPL_ParserVariablesInExpression
+   public :: mapl_StateAddMethod
+   public :: CallbackMap
+   public :: CallbackMapIterator
+   public :: CallbackMethodWrapper
+   public :: get_callbacks
+   public :: MAPL_StateGetGeom
 
-   public :: FieldBundleType_Flag
-   public :: FIELDBUNDLETYPE_INVALID
-   public :: FIELDBUNDLETYPE_BASIC
-   public :: FIELDBUNDLETYPE_VECTOR
-   public :: FIELDBUNDLETYPE_BRACKET
-   public :: FIELDBUNDLETYPE_VECTORBRACKET
-
-   public :: operator(==)
-   public :: operator(/=)
-
-   ! VectorBasisKind
-   public :: VectorBasisKind
-   public :: VECTOR_BASIS_KIND_INVALID
-   public :: VECTOR_BASIS_KIND_GRID
-   public :: VECTOR_BASIS_KIND_NS
-
-   ! Used internally by MAPL
-   ! Users shouldn't need these
-
-end module mapl_FieldBundle_API_mod
+end module mapl_State_API_mod
--- mapl3-doc/src/write_restart.F90
+++ mapl3-doc/src/write_restart.F90
@@ -1,71 +1,30 @@
 #include "MAPL.h"
 
-submodule (mapl_OuterMetaComponent_mod) write_restart_smod
-   use mapl_MultiState_mod
-   use mapl_RestartHandler_mod
-   use mapl_os_mod
-   use mapl_mp_utils, only: MAPL_GetCheckpointSubdir
+submodule(mapl_GriddedComponentDriver_mod) write_restart_smod
    use mapl_ErrorHandling_mod
    implicit none(type,external)
 
 contains
 
-   module recursive subroutine write_restart(this, importState, exportState, clock, unusable, rc)
-      class(OuterMetaComponent), target, intent(inout) :: this
-      type(ESMF_State) :: importState
-      type(ESMF_State) :: exportState
-      type(ESMF_Clock) :: clock
-      ! optional arguments
+   module recursive subroutine write_restart(this, unusable, phase_idx, rc)
+      class(GriddedComponentDriver), target, intent(inout) :: this
       class(KE), optional, intent(in) :: unusable
+      integer, optional, intent(in) :: phase_idx
       integer, optional, intent(out) :: rc
 
-      ! Locals
-      character(*), parameter :: PHASE_NAME = 'GENERIC::WRITE_RESTART'
-      type(GriddedComponentDriver), pointer :: driver
-      type(MultiState) :: states
-      type(RestartHandler) :: restart_handler
-      integer :: status
-      character(:), allocatable :: subdir
-      character(:), allocatable :: filename
-      type(esmf_Time) :: currTime
+      integer :: status, user_status
 
-      call recurse_write_restart_(this, _RC)
-      call this%run_custom(ESMF_METHOD_WRITERESTART, PHASE_NAME, _RC)
-      _RETURN_UNLESS(this%has_geom())
-     
-      driver => this%get_user_gc_driver()
-      call esmf_ClockGet(driver%get_clock(), currTime=currTime, _RC)
+      associate ( &
+           importState => this%states%importState, &
+           exportState => this%states%exportState)
 
-      restart_handler = RestartHandler(this%get_geom(), currTime, this%get_logger())
+        call ESMF_GridCompWriteRestart(this%gridcomp, &
+             importState=importState, exportState=exportState, clock=this%clock, &
+             phase=phase_idx, _USERRC)
 
-      states = driver%get_states()
-      subdir = MAPL_GetCheckpointSubdir(this%hconfig, currTime, _RC)
+      end associate
 
-      if (this%component_spec%misc%checkpoint_controls%import) then
-         filename = mapl_PathJoin(subdir, driver%get_name() // '_import.nc')
-         call this%start_timer("WriteImportCheckpoint", _RC)
-         call restart_handler%write(states%importState, filename, _RC)
-         call this%stop_timer("WriteImportCheckpoint", _RC)
-      end if
-      
-      if (this%component_spec%misc%checkpoint_controls%internal) then
-         filename = mapl_PathJoin(subdir, driver%get_name() // '_internal.nc')
-         call this%start_timer("WriteInternalCheckpoint", _RC)
-         call restart_handler%write(states%internalState, filename, _RC)
-         call this%stop_timer("WriteInternalCheckpoint", _RC)
-      end if
-      
-      if (this%component_spec%misc%checkpoint_controls%export) then
-         filename = mapl_PathJoin(subdir, driver%get_name() // '_export.nc')
-         call this%start_timer("WriteExportCheckpoint", _RC)
-         call restart_handler%write(states%exportState, filename, _RC)
-         call this%stop_timer("WriteExportCheckpoint", _RC)
-      end if
-
-      _RETURN(ESMF_SUCCESS)
-      _UNUSED_DUMMY(importState)
-      _UNUSED_DUMMY(exportState)
-      _UNUSED_DUMMY(clock)
+      _RETURN(_SUCCESS)
       _UNUSED_DUMMY(unusable)
    end subroutine write_restart
 
