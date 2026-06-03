14eb5f25aa3b4872eac0bbb70016536cdfaad687
mathomp4 <fortran@gmail.com>
2026-06-03 19:33:14 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@0d173f1c40e8adb1ee76d6e7d51355e47d88de98 🚀

--- mapl3-doc/src/equal_to.F90
+++ mapl3-doc/src/equal_to.F90
@@ -1,28 +1,26 @@
-#include "MAPL.h"
+#include "MAPL_ErrLog.h"
 
-submodule (mapl_LatLonGeomSpec_mod) equal_to_smod
-   use mapl_CoordinateAxis_mod
-   use mapl_GeomSpec_mod
-   use pfio
-   use mapl_ErrorHandling_mod
-   use esmf
+submodule (mapl_LocStreamDecomposition_mod) equal_to_smod
    implicit none (type, external)
-   
+
 contains
 
-   pure logical module function equal_to(a, b)
-      class(LatLonGeomSpec), intent(in) :: a
-      class(GeomSpec), intent(in) :: b
+   module procedure equal_to
+      
+      ! Both must have allocated distributions
+      if (.not. allocated(decomp1%point_distribution)) then
+         equal_to = .not. allocated(decomp2%point_distribution)
+         return
+      end if
 
-      select type (b)
-      type is (LatLonGeomSpec)
-         equal_to = (a%lon_axis == b%lon_axis) .and. (a%lat_axis == b%lat_axis)
-         if (.not. equal_to) return
-         equal_to = (a%decomposition == b%decomposition)
-      class default
+      if (.not. allocated(decomp2%point_distribution)) then
          equal_to = .false.
-      end select
+         return
+      end if
 
-   end function equal_to
+      ! Check if distributions are the same
+      equal_to = all(decomp1%point_distribution == decomp2%point_distribution)
+
+   end procedure equal_to
 
 end submodule equal_to_smod
