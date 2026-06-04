3646382f5b70b821810838dfb7c93d63ac498d5e
tclune <thomas.l.clune@nasa.gov>
2026-06-04 14:10:04 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@873179e2daa08266e440b184d97e3b3cc3d196cb 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,88 +1,99 @@
-! Export umbrella for the MAPL superstructure/generic layer.
-! Public API exposed to external consumers.
-module mapl_generic_api
+! Export umbrella for the MAPL utils layer.
+! Defines the public API of utils/ for external consumers.
+! Uses mapl_utils_internal and re-exports only the intended public symbols.
+module mapl_utils_api
 
-   use mapl_UserSetServices_mod, only: user_setservices, AbstractUserSetServices, DSOSetServices
-
-   use mapl_OpenMP_Support_mod, only: mapl_find_bounds => find_bounds
-   use mapl_OpenMP_Support_mod, only: mapl_get_num_threads => get_num_threads
-   use mapl_OpenMP_Support_mod, only: mapl_get_current_thread => get_current_thread
-
-   use mapl_Generic_mod
-   use mapl_GenericGridComp_mod
-   use mapl_VariableSpec_mod
-
-   use mapl_ComponentSpec_mod
-   use mapl_ChildSpec_mod
-   use mapl_RestartHandler_mod
+   use mapl_utils_internal
 
    implicit none
    private
 
-   ! These should be available to users
-   public :: mapl_GridCompAddVarSpec
-   public :: mapl_GridCompAddSpec
-   public :: mapl_GridCompAdvertiseVariable
-   public :: mapl_GridCompIsGeneric
-   public :: mapl_GridCompIsUser
+   ! Error handling
+   public :: MAPL_Assert
+   public :: MAPL_Verify
+   public :: MAPL_Return
+   public :: MAPL_Deprecated
+   public :: MAPL_SetFailOnDeprecated
+   public :: MAPL_RTRN
+   public :: MAPL_Vrfy
+   public :: MAPL_ASRT
+   public :: MAPL_abort
+   public :: MAPL_set_abort_handler
+   public :: MAPL_SUCCESS
+   public :: MAPL_UNKNOWN_ERROR
+   public :: MAPL_NO_SUCH_PROPERTY
+   public :: MAPL_NO_SUCH_VARIABLE
+   public :: MAPL_TYPE_MISMATCH
+   public :: MAPL_UNSUPPORTED_TYPE
+   public :: MAPL_VALUE_NOT_SUPPORTED
+   public :: MAPL_NO_DEFAULT_VALUE
+   public :: MAPL_DUPLICATE_KEY
+   public :: MAPL_STRING_TOO_SHORT
 
-   public :: mapl_GridCompGet
-   public :: mapl_GridCompSet
-   public :: mapl_GridCompSetEntryPoint
+   ! Keyword enforcer
+   public :: KeywordEnforcer
 
-   public :: mapl_GridCompAddChild
-   public :: mapl_GridCompGetChildName
-   public :: mapl_GridCompRunChild
-   public :: mapl_GridCompRunChildren
+   ! String types and utilities
+   public :: String
+   public :: split
+   public :: to_lower
+   public :: to_upper
+   public :: capitalize
+   public :: is_alpha
+   public :: is_alpha_only
+   public :: is_numeric
+   public :: is_alphanumeric
+   public :: to_string
+   public :: to_character_array
+   public :: lowercase
+   public :: uppercase
+   public :: is_digit
+   public :: get_ascii_interval
+   public :: is_alphanum_character
+   public :: is_lower_character
+   public :: is_upper_character
 
-   public :: mapl_GridCompGetInternalState
+   ! OS / filesystem
+   public :: mapl_GetCurrentWorkingDirectory
+   public :: mapl_ChangeDirectory
+   public :: mapl_MakeDirectory
+   public :: mapl_DirectoryExists
+   public :: mapl_RemoveDirectory
+   public :: mapl_RemoveFile
+   public :: mapl_PushDirectory
+   public :: mapl_PopDirectory
+   public :: mapl_ClearDirectoryStack
+   public :: mapl_PathJoin
+   public :: mapl_MakeSymbolicLink
+   public :: get_checkpoint_subdir
+   public :: get_file_extension
+   public :: get_file_basename
 
-   public :: mapl_GridCompSetGeometry
+   ! Memory info
+   public :: MemInfo
+   public :: MemInfoWrite
 
-   public :: mapl_GridcompGetResource
+   ! Time utilities
+   public :: PackDate
+   public :: PackDateTime
+   public :: UnpackDate
+   ! UnpackDateTime removed - conflicts with mp_utils version, see issue #5011
+   ! public :: UnpackDateTime
 
-   public :: mapl_ClockGet
+   ! ISO8601 date/time conversion
+   public :: convert_ISO8601_to_integer_time
+   public :: convert_ISO8601_to_integer_date
+   public :: ISO8601Date
+   public :: ISO8601Time
+   public :: ISO8601DateTime
+   public :: ISO8601Duration
+   public :: ISO8601Interval
 
-   ! Accessors
-!!$   public :: mapl_GetOrbit
-!!$   public :: mapl_GetCoordinates
-!!$   public :: mapl_GetLayout
+   ! ESMF info keys
+   ! KEY_UNITS and KEY_TYPEKIND excluded: values differ from mapl_HistoryConstants_mod
+   ! homonyms; consumers needing them should use mapl_esmf_info_keys_mod directly.
 
-   public :: mapl_GridCompSetGeom
-   public :: mapl_GridCompSetVerticalGrid
+   ! Validation
+   public :: is_valid_name
 
-   ! Connections
-   public :: mapl_GridCompAddConnection
-   public :: mapl_GridCompAddConnectivity  ! Legacy name - temporary backward compatibility
-   public :: mapl_GridCompReexport
-   public :: mapl_GridCompConnectAll
-
-   ! Timers
-   public :: mapl_GridCompTimerStart
-   public :: mapl_GridCompTimerStop
-
-   ! Spec types
-   public :: mapl_STATEITEM_STATE, mapl_STATEITEM_FIELDBUNDLE
-   public :: mapl_STATEITEM_SERVICE, mapl_STATEITEM_VECTOR
-
-   public :: mapl_UserCompGetInternalState, MAPL_UserCompSetInternalState
-
-   public :: user_setservices
-   public :: AbstractUserSetServices
-   public :: DSOSetServices
-
-   public :: mapl_find_bounds
-   public :: mapl_get_num_threads
-   public :: mapl_get_current_thread
-
-   public :: mapl_GridCompCreate
-   public :: mapl_GenericSetServices
-
-   public :: VariableSpec
-   public :: make_VariableSpec
-
-   public :: ChildSpec
-   
-   public :: CheckpointControls
-   public :: RestartHandler
-end module mapl_generic_api
+end module mapl_utils_api
