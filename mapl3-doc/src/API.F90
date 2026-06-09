233efac852d40d08ab62b1d8f8081deae067d094
atrayano <atanas.trayanov@gmail.com>
2026-06-09 23:12:17 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@4c929a8e5ada5d6ba42a8b9f3ce71de4bddd9019 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,27 +1,13 @@
-! Export umbrella for the MAPL.field library.
+! Export umbrella for the MAPL.component library
+module mapl_component_api
 
-module mapl_field_api
-
-   use mapl_FieldCreate_mod, only: MAPL_FieldCreate => FieldCreate
-   use mapl_FieldCreate_mod, only: MAPL_FieldEmptyComplete => FieldEmptyComplete
-   use mapl_FieldCreate_mod, only: MAPL_FieldsAreAliased => FieldsAreAliased
-   use mapl_FieldGet_mod, only: MAPL_FieldGet => FieldGet
-   use mapl_FieldSet_mod, only: MAPL_FieldSet => FieldSet
-   use mapl_FieldFill_mod, only: MAPL_FieldFill => FieldFill
-   use mapl_RestartModes_mod, only: MAPL_RESTART_REQUIRED => RESTART_REQUIRED
-   use mapl_RestartModes_mod, only: MAPL_RESTART_SKIP => RESTART_SKIP
+   use mapl_ComponentDriver_mod, only: MAPL_DriverInitializePhases => DriverInitializePhases
+   use mapl_GriddedComponentDriver_mod, only: MAPL_GriddedComponentDriver => GriddedComponentDriver
 
    implicit none
    private
 
-   public :: MAPL_FieldCreate
-   public :: MAPL_FieldEmptyComplete
-   public :: MAPL_FieldsAreAliased
-   public :: MAPL_FieldGet
-   public :: MAPL_FieldSet
-   public :: MAPL_FieldFill
-   public :: MAPL_RESTART_REQUIRED
-   public :: MAPL_RESTART_SKIP
+   public :: MAPL_DriverInitializePhases
+   public :: MAPL_GriddedComponentDriver
 
-end module mapl_field_api
-
+end module mapl_component_api
