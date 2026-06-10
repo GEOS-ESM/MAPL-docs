79161de35af2e6a15e63df0c9ac45aba6cb9eba0
tclune <thomas.l.clune@nasa.gov>
2026-06-10 21:16:21 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@ee2da4953d2fc7cbfac6e39b93377c3a8fd03afa 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,27 +1,35 @@
-! Export umbrella for the MAPL.field library.
+! Export umbrella for the MAPL.field_bundle library.
+module mapl_field_bundle_api
 
-module mapl_field_api
-
-   use mapl_FieldCreate_mod, only: MAPL_FieldCreate => FieldCreate
-   use mapl_FieldCreate_mod, only: MAPL_FieldEmptyComplete => FieldEmptyComplete
-   use mapl_FieldCreate_mod, only: MAPL_FieldsAreAliased => FieldsAreAliased
-   use mapl_FieldGet_mod, only: MAPL_FieldGet => FieldGet
-   use mapl_FieldSet_mod, only: MAPL_FieldSet => FieldSet
-   use mapl_FieldFill_mod, only: MAPL_FieldFill => FieldFill
-   use mapl_RestartModes_mod, only: MAPL_RESTART_REQUIRED => RESTART_REQUIRED
-   use mapl_RestartModes_mod, only: MAPL_RESTART_SKIP => RESTART_SKIP
+   use ESMF, only: MAPL_FieldBundleAdd => ESMF_FieldBundleAdd
+   use mapl_FieldBundleCopy_mod, only: MAPL_FieldBundleCopy => FieldBundleCopy
+   use mapl_FieldBundleCreate_mod, only: MAPL_FieldBundleCreate => FieldBundleCreate
+   use mapl_FieldBundleCreate_mod, only: MAPL_FieldBundlesAreAliased => FieldBundlesAreAliased
+   use mapl_FieldBundleDestroy_mod, only: MAPL_FieldBundleDestroy
+   use mapl_FieldBundleGet_mod, only: MAPL_FieldBundleGet => FieldBundleGet
+   use mapl_FieldBundleGetByIndex_mod, only: MAPL_FieldBundleGetByIndex => FieldBundleGetByIndex
+   use mapl_FieldBundleGetPointer_mod, only: MAPL_FieldBundleGetPointer => FieldBundleGetPointerToData
+   use mapl_FieldBundleInfo_mod, only: MAPL_FieldBundleInfoGetInternal => FieldBundleInfoGetInternal
+   use mapl_FieldBundleInfo_mod, only: MAPL_FieldBundleInfoSetInternal => FieldBundleInfoSetInternal
+   use mapl_FieldBundleMatch_mod, only: MAPL_FieldBundleSameData => FieldBundleSameData
+   use mapl_FieldBundleSet_mod, only: MAPL_FieldBundleSet => FieldBundleSet
+   use mapl_FieldBundleFilter_mod, only: MAPL_FieldBundleFilter => FieldBundleFilter
 
    implicit none
    private
 
-   public :: MAPL_FieldCreate
-   public :: MAPL_FieldEmptyComplete
-   public :: MAPL_FieldsAreAliased
-   public :: MAPL_FieldGet
-   public :: MAPL_FieldSet
-   public :: MAPL_FieldFill
-   public :: MAPL_RESTART_REQUIRED
-   public :: MAPL_RESTART_SKIP
+   public :: MAPL_FieldBundleAdd
+   public :: MAPL_FieldBundleCopy
+   public :: MAPL_FieldBundleCreate
+   public :: MAPL_FieldBundleDestroy
+   public :: MAPL_FieldBundleGet
+   public :: MAPL_FieldBundleGetByIndex
+   public :: MAPL_FieldBundleGetPointer
+   public :: MAPL_FieldBundleInfoGetInternal
+   public :: MAPL_FieldBundleInfoSetInternal
+   public :: MAPL_FieldBundleSameData
+   public :: MAPL_FieldBundlesAreAliased
+   public :: MAPL_FieldBundleSet
+   public :: MAPL_FieldBundleFilter
 
-end module mapl_field_api
-
+end module mapl_field_bundle_api
