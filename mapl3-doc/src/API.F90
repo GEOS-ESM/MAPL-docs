9b8a2b85e94d918c0f716bac74b62e14a522d9ba
tclune <thomas.l.clune@nasa.gov>
2026-06-11 22:51:04 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@3cb7b1d5c7a159db84eb75f879d7156e750502bc 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,31 +1,134 @@
-module mapl_pfio_api
-  use pfio, only: FileMetadata
-  use pfio, only: i_clients
-  use pfio, only: o_clients
-  use pfio, only: NetCDF4_FileFormatter
-  use pfio, only: pfio_read
-  use pfio, only: ArrayReference
-  use pfio, only: string_in_stringvector
+! Export umbrella for the MAPL superstructure/generic layer.
+! Public API exposed to external consumers.
+module mapl_generic_api
 
-  use pfio, only: Variable
-  use pfio, only: StringVariableMap
-  use pfio, only: StringVariableMapIterator
-  use pfio, only: operator(==), operator(/=)
+   use mapl_UserSetServices_mod, only: user_setservices, AbstractUserSetServices, DSOSetServices
 
-  implicit none
-  private
+   use mapl_OpenMP_Support_mod, only: mapl_find_bounds => find_bounds
+   use mapl_OpenMP_Support_mod, only: mapl_get_num_threads => get_num_threads
+   use mapl_OpenMP_Support_mod, only: mapl_get_current_thread => get_current_thread
 
-  public :: FileMetadata
-  public :: i_clients
-  public :: o_clients
-  public :: NetCDF4_FileFormatter
-  public :: pfio_read
-  public :: ArrayReference
-  public :: string_in_stringvector
+   use mapl_Generic_mod, &
+       mapl_GridCompAddVarSpec => GridCompAddVarSpec, &
+       mapl_GridCompAddSpec => GridCompAddSpec, &
+       mapl_GridCompAdvertiseVariable => GridCompAdvertiseVariable, &
+       mapl_GridCompGet => GridCompGet, &
+       mapl_GridCompSet => GridCompSet, &
+       mapl_GridCompSetCheckpointControls => GridCompSetCheckpointControls, &
+       mapl_GridCompSetEntryPoint => GridCompSetEntryPoint, &
+       mapl_GridCompAddChild => GridCompAddChild, &
+       mapl_GridCompGetChildName => GridCompGetChildName, &
+       mapl_GridCompRunChild => GridCompRunChild, &
+       mapl_GridCompRunChildren => GridCompRunChildren, &
+       mapl_GridCompGetInternalState => GridCompGetInternalState, &
+       mapl_GridCompSetGeometry => GridCompSetGeometry, &
+       mapl_GridcompGetResource => GridCompGetResource, &
+       mapl_ClockGet => ClockGet, &
+       mapl_GridCompSetGeom => GridCompSetGeom, &
+       mapl_GridCompSetVerticalGrid => GridCompSetVerticalGrid, &
+       mapl_GridCompAddConnection => GridCompAddConnection, &
+       mapl_GridCompAddConnectivity => GridCompAddConnectivity, &
+       mapl_GridCompReexport => GridCompReexport, &
+       mapl_GridCompConnectAll => GridCompConnectAll, &
+       mapl_GridCompTimerStart => GridCompTimerStart, &
+       mapl_GridCompTimerStop => GridCompTimerStop, &
+       mapl_GridCompGetCheckpointDir => GridCompGetCheckpointDir, &
+       mapl_GridCompSetCheckpointControls => GridCompSetCheckpointControls, &
+   !    mapl_STATEITEM_STATE => STATEITEM_STATE, &
+   !    mapl_STATEITEM_FIELDBUNDLE => STATEITEM_FIELDBUNDLE, &
+   !    mapl_STATEITEM_SERVICE => STATEITEM_SERVICE, &
+   !    mapl_STATEITEM_VECTOR => STATEITEM_VECTOR, &
+   !    mapl_UserCompGetInternalState => UserCompGetInternalState, &
+   !    MAPL_UserCompSetInternalState => UserCompSetInternalState, &
+       MAPL_GridCompGetOuterMeta => GridCompGetOuterMeta, &
+       MAPL_GridCompGetRegistry => GridCompGetRegistry, &
+       MAPL_GridCompIsGeneric => GridCompIsGeneric, &
+       MAPL_GridCompIsUser => GridCompIsUser
 
-  public :: Variable
-  public :: StringVariableMap
-  public :: StringVariableMapIterator
-  public :: operator(==), operator(/=)
+   use mapl_GenericGridComp_mod,  &
+       mapl_GridCompCreate => GridCompCreate, &
+       mapl_GenericSetServices => GenericSetServices
 
-end module mapl_pfio_api
+    use mapl_VariableSpec_mod
+    use mapl_ComponentSpec_mod
+    use mapl_CheckpointControls_mod
+    use mapl_ChildSpec_mod
+    use mapl_RestartHandler_mod
+
+   implicit none
+   private
+
+   ! These should be available to users
+   public :: mapl_GridCompAddVarSpec
+   public :: mapl_GridCompAddSpec
+   public :: mapl_GridCompAdvertiseVariable
+
+   public :: mapl_GridCompGet
+   public :: mapl_GridCompGetRegistry
+   public :: mapl_GridCompSet
+   public :: mapl_GridCompSetCheckpointControls
+   public :: mapl_GridCompSetEntryPoint
+
+   public :: MAPL_GridCompIsGeneric
+   public :: MAPL_GridCompIsUser
+   public :: MAPL_GridCompGetOuterMeta
+
+   public :: mapl_GridCompAddChild
+   public :: mapl_GridCompGetChildName
+   public :: mapl_GridCompRunChild
+   public :: mapl_GridCompRunChildren
+
+   public :: mapl_GridCompGetInternalState
+
+   public :: mapl_GridCompSetGeometry
+
+   public :: mapl_GridcompGetResource
+
+   public :: mapl_ClockGet
+
+   ! Accessors
+!!$   public :: mapl_GetOrbit
+!!$   public :: mapl_GetCoordinates
+!!$   public :: mapl_GetLayout
+
+   public :: mapl_GridCompSetGeom
+   public :: mapl_GridCompSetVerticalGrid
+
+   ! Connections
+   public :: mapl_GridCompAddConnection
+   public :: mapl_GridCompAddConnectivity  ! Legacy name - temporary backward compatibility
+   public :: mapl_GridCompReexport
+   public :: mapl_GridCompConnectAll
+
+   ! Timers
+   public :: mapl_GridCompTimerStart
+   public :: mapl_GridCompTimerStop
+
+   ! Checkpoint directory
+   public :: mapl_GridCompGetCheckpointDir
+
+   ! Spec types
+   public :: mapl_STATEITEM_STATE, mapl_STATEITEM_FIELDBUNDLE
+   public :: mapl_STATEITEM_SERVICE, mapl_STATEITEM_VECTOR
+
+   public :: mapl_UserCompGetInternalState, MAPL_UserCompSetInternalState
+
+   public :: user_setservices
+   public :: AbstractUserSetServices
+   public :: DSOSetServices
+
+   public :: mapl_find_bounds
+   public :: mapl_get_num_threads
+   public :: mapl_get_current_thread
+
+   public :: mapl_GridCompCreate
+   public :: mapl_GenericSetServices
+
+   public :: VariableSpec
+   public :: make_VariableSpec
+
+   public :: ChildSpec
+
+   public :: CheckpointControls
+   public :: RestartHandler
+end module mapl_generic_api
--- mapl3-doc/src/make_variable_attributes.F90
+++ mapl3-doc/src/make_variable_attributes.F90
@@ -1,17 +1,27 @@
 #include "MAPL.h"
 
-submodule (mapl_EASEGeomFactory_mod) make_variable_attributes_smod
+submodule (mapl_LatLonGeomFactory_mod) make_variable_attributes_smod
+
    use mapl_GeomSpec_mod
-   use mapl_EASEGeomSpec_mod
+   use mapl_LonAxis_mod
+   use mapl_LatAxis_mod
+   use mapl_LatLonDecomposition_mod
+   use mapl_LatLonGeomSpec_mod
+   use mapl_MinMax_mod
    use mapl_ErrorHandling_mod
+   use MAPL_Constants
+   use pFIO
    use mapl_StringDictionary_mod
+   use esmf
+   use mapl_KeywordEnforcer_mod, only: KE => KeywordEnforcer
+
    implicit none (type, external)
 
 contains
 
    module function make_variable_attributes(this, geom_spec, rc) result(variable_attributes)
       type(StringDictionary) :: variable_attributes
-      class(EASEGeomFactory), intent(in) :: this
+      class(LatLonGeomFactory), intent(in) :: this
       class(GeomSpec), intent(in) :: geom_spec
       integer, optional, intent(out) :: rc
 
