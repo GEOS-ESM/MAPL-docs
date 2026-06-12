940fea1962a3422bae396148ae4e9a13afb01c4c
mathomp4 <fortran@gmail.com>
2026-06-12 17:13:21 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@b59a0ed5c25aa6f192127787f5e90c1f7df09752 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,93 +1,27 @@
-! Public Export umbrella for the MAPL.mp_utils layer.
-! Re-exports only the symbols that are part of MAPL's public API
-! (i.e., entities carrying the MAPL_ prefix).
-! This is what mapl/MAPL.F90 imports from.
-module mapl_mp_utils_api
-   use mapl_ArrayReductions_mod, only: MAPL_AreaMean => AreaMean
-   use mapl_ArrayReductions_mod, only: MAPL_MaxMin => MaxMin
-   ! Import all PackedTime functions (both prefixed and unprefixed)
-   use mapl_PackedTime_mod,      only: MAPL_PackedDateCreate => PackedDateCreate, &
-                                       MAPL_PackedTimeCreate => PackedTimeCreate, &
-                                       MAPL_PackedDateTimeCreate => PackedDateTimeCreate, &
-                                       MAPL_ESMFTimeFromPacked => ESMFTimeFromPacked, &
-                                       MAPL_UnpackDate => UnpackDate, &
-                                       MAPL_UnpackTime => UnpackTime, &
-                                       MAPL_UnpackDateTime => UnpackDateTime
+! Export umbrella for the MAPL.field library.
 
-   use mapl_StringTemplate_mod,  only: mapl_StrTemplate => StrTemplate
-   use mapl_StringTemplate_mod,  only: mapl_fill_grads_template => fill_grads_template
-   use mapl_StringTemplate_mod,  only: mapl_fill_grads_template_esmf => fill_grads_template_esmf
+module mapl_field_api
 
-   use mapl_Shmem_mod, only: MAPL_GetNodeInfo => GetNodeInfo
-   use mapl_Shmem_mod, only: MAPL_CoresPerNodeGet => CoresPerNodeGet
-   use mapl_Shmem_mod, only: MAPL_InitializeShmem => InitializeShmem
-   use mapl_Shmem_mod, only: MAPL_FinalizeShmem => FinalizeShmem
-   use mapl_Shmem_mod, only: MAPL_AllocNodeArray => AllocNodeArray
-   use mapl_Shmem_mod, only: MAPL_DeAllocNodeArray => DeAllocNodeArray
-   use mapl_Shmem_mod, only: MAPL_ShmemAmOnFirstNode => ShmemAmOnFirstNode
-   use mapl_Shmem_mod, only: MAPL_SyncSharedMemory => SyncSharedMemory
-   use mapl_Shmem_mod, only: MAPL_BroadcastToNodes => BroadcastToNodes
-   use mapl_Shmem_mod, only: MAPL_AllocateShared => AllocateShared
-   use mapl_Shmem_mod, only: mapl_GetSharedMemory => GetSharedMemory
-   use mapl_Shmem_mod, only: mapl_ReleaseSharedMemory => ReleaseSharedMemory
-   use mapl_Shmem_mod, only: MAPL_GetNewRank => GetNewRank
-   use mapl_Shmem_mod, only: MAPL_NodeComm
-   use mapl_Shmem_mod, only: MAPL_NodeRootsComm
-   use mapl_Shmem_mod, only: MAPL_MyNodeNum
-   use mapl_Shmem_mod, only: MAPL_AmNodeRoot
-   use mapl_Shmem_mod, only: MAPL_ShmInitialized
-   use mapl_LoadBalance_mod, only: MAPL_BalanceWork => BalanceWork, &
-                                   MAPL_BalanceCreate => BalanceCreate, &
-                                   MAPL_BalanceDestroy => BalanceDestroy, &
-                                   MAPL_BalanceGet => BalanceGet
+   use mapl_FieldCreate_mod, only: MAPL_FieldCreate => FieldCreate
+   use mapl_FieldCreate_mod, only: MAPL_FieldEmptyComplete => FieldEmptyComplete
+   use mapl_FieldCreate_mod, only: MAPL_FieldsAreAliased => FieldsAreAliased
+   use mapl_FieldGet_mod, only: MAPL_FieldGet => FieldGet
+   use mapl_FieldSet_mod, only: MAPL_FieldSet => FieldSet
+   use mapl_FieldFill_mod, only: MAPL_FieldFill => FieldFill
+   use mapl_RestartModes_mod, only: MAPL_RESTART_REQUIRED => RESTART_REQUIRED
+   use mapl_RestartModes_mod, only: MAPL_RESTART_SKIP => RESTART_SKIP
+
    implicit none
    private
 
-   ! Statistics
-   public :: MAPL_MaxMin
-   public :: MAPL_AreaMean
+   public :: MAPL_FieldCreate
+   public :: MAPL_FieldEmptyComplete
+   public :: MAPL_FieldsAreAliased
+   public :: MAPL_FieldGet
+   public :: MAPL_FieldSet
+   public :: MAPL_FieldFill
+   public :: MAPL_RESTART_REQUIRED
+   public :: MAPL_RESTART_SKIP
 
-   ! Time packing
-   public :: MAPL_UnpackTime
-   public :: MAPL_UnpackDate
-   public :: MAPL_UnpackDateTime
+end module mapl_field_api
 
-   ! PackedTime functions with MAPL_ prefix
-   public :: MAPL_PackedDateCreate
-   public :: MAPL_PackedTimeCreate
-   public :: MAPL_PackedDateTimeCreate
-   public :: MAPL_ESMFTimeFromPacked
-
-   public :: mapl_StrTemplate
-
-   public :: mapl_fill_grads_template
-   public :: mapl_fill_grads_template_esmf
-   
-   public :: MAPL_GetNodeInfo
-   public :: MAPL_CoresPerNodeGet
-   public :: MAPL_InitializeShmem
-   public :: MAPL_FinalizeShmem
-   
-   public :: MAPL_AllocNodeArray
-   public :: MAPL_DeAllocNodeArray
-   public :: MAPL_ShmemAmOnFirstNode
-   public :: MAPL_SyncSharedMemory
-   public :: MAPL_BroadcastToNodes
-   
-   public :: MAPL_AllocateShared
-   public :: MAPL_GetSharedMemory
-   public :: MAPL_ReleaseSharedMemory
-
-   public :: MAPL_GetNewRank
-   public :: MAPL_NodeComm
-   public :: MAPL_NodeRootsComm
-   public :: MAPL_MyNodeNum
-   public :: MAPL_AmNodeRoot
-   public :: MAPL_ShmInitialized
-
-   public :: MAPL_BalanceWork
-   public :: MAPL_BalanceCreate
-   public :: MAPL_BalanceDestroy
-   public :: MAPL_BalanceGet
-
-end module mapl_mp_utils_api
