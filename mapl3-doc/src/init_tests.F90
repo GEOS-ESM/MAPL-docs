e74f18f0a2139cf82c236abb2a45cd848c52c636
bena-nasa <benjamin.m.auer@nasa.gov>
2026-06-23 18:01:33 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@fed4e1a70edb61fd2c91a407e9a217d99a679a5b 🚀

--- mapl3-doc/src/init_tests.F90
+++
@@ -1,8 +0,0 @@
-subroutine init_tests()
-   use MAPL_pFUnit_ThrowMod, only: pFUnit_throw
-   use mapl_Throw_mod
-   implicit none
-
-   call MAPL_set_throw_method(pfunit_throw)
-
-end subroutine init_tests
