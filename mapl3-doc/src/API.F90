be87f60e76f3d37c5ae4e304c7908ebe52db1196
bena-nasa <benjamin.m.auer@nasa.gov>
2026-07-01 14:25:20 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@bce15130eb535447120f5fa98cd77139d649bb4f 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,28 +1,34 @@
-! Export umbrella for the MAPL.state library.
-module mapl_state_api
+module mapl_geomio_api
 
-   use mapl_StateArithmeticParser_mod, only: MAPL_ParserVariablesInExpression => parser_variables_in_expression
-   use mapl_StateArithmeticParser_mod, only: mapl_StateEval => StateEval
-   use mapl_StateAddMethod_mod, only: mapl_StateAddMethod => StateAddMethod
-   use mapl_StateDestroy_mod, only: MAPL_StateDestroy => StateDestroy
-   use mapl_StateGet_mod, only: MAPL_StateGet => StateGet
-   use mapl_StateSet_mod, only: MAPL_StateSet => StateSet
-   use mapl_StateGetGeom_mod, only: MAPL_StateGetGeom => StateGetGeom
-   use mapl_StateGetPointer_mod, only: MAPL_StateGetPointer => StateGetPointer
-   use mapl_StateMask_mod, only: MAPL_StateMask => StateMask
-   
+   use mapl_CompressionSettings_mod, only: mapl_CompressionSettings => CompressionSettings
+   use mapl_SharedIO_mod, only: mapl_bundle_to_metadata => bundle_to_metadata
+   use mapl_SharedIO_mod, only: mapl_esmf_to_pfio_type => esmf_to_pfio_type
+   use mapl_GeomPFIO_mod, only: mapl_GeomPFIO => GeomPFIO
+   use mapl_GeomCategorizer_mod, only: mapl_make_geom_pfio => make_geom_pfio
+   use mapl_pFIOServerBounds_mod, only: mapl_pFIOServerBounds => pFIOServerBounds
+   use mapl_pFIOServerBounds_mod, only: mapl_PFIO_BOUNDS_READ => PFIO_BOUNDS_READ
+   use mapl_DataCollection_mod, only: mapl_DataCollection => DataCollection
+   use mapl_DataCollectionManager_mod, only: MAPL_AddDataCollection => AddDataCollection
+   use mapl_DataCollectionManager_mod, only: mapl_DataCollections => DataCollections
 
+   use mapl_FieldBundleWrite_mod, only: mapl_FieldBundleWriter => FieldBundleWriter
+   use mapl_FieldBundleRead_mod, only: mapl_read_bundle => read_bundle
    implicit none
    private
 
-   public :: MAPL_ParserVariablesInExpression
-   public :: mapl_StateAddMethod
-   public :: MAPL_StateDestroy
-   public :: MAPL_StateGet
-   public :: MAPL_StateSet
-   public :: MAPL_StateGetGeom
-   public :: MAPL_StateGetPointer
-   public :: mapl_StateEval
 
-   public :: MAPL_StateMask
-end module mapl_state_api
+   public :: mapl_CompressionSettings
+   public :: mapl_GeomPFIO
+   public :: mapl_pFIOServerBounds
+   public :: MAPL_PFIO_BOUNDS_READ
+   public :: mapl_DataCollection
+   public :: mapl_DataCollections
+   public :: mapl_AddDataCollection
+   public :: mapl_FieldBundleWriter
+
+   public :: mapl_make_geom_pfio
+   public :: mapl_bundle_to_metadata
+   public :: mapl_esmf_to_pfio_type
+   public :: mapl_read_bundle
+   
+end module mapl_geomio_api
