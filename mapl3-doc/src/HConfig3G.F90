85afec1570c69dac8b79a30486c18920cec98c67
mathomp4 <fortran@gmail.com>
2026-07-10 12:44:22 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@7e88f2dd1f02458261330080137b6647a480158f 🚀

--- mapl3-doc/src/HConfig3G.F90
+++
@@ -1,3 +0,0 @@
-module hconfig3g
-   use mapl_hconfig_get_mod
-end module hconfig3g
--- mapl3-doc/src/hconfig_get.F90
+++
@@ -1,8 +0,0 @@
-module mapl_hconfig_get_mod
-
-   use mapl_hconfig_get_private_mod, only: HConfigGet
-   use mapl_hconfig_params_mod, only: HConfigParams
-   implicit none
-   public :: HConfigGet
-
-end module mapl_hconfig_get_mod
