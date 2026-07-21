b23a203040d395e4313e46a475331a4b0de612fa
bena-nasa <benjamin.m.auer@nasa.gov>
2026-07-21 16:01:00 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@319f9a534a21b111139f3c6c94730d9f47085eff 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,34 +1,72 @@
-module mapl_geomio_api
+module mapl_base_api
+   use mapl_FileMetadataUtils_mod
+   use mapl_FileMetadataUtilsVector_mod
+   ! StringTemplate is in mp_utils/ - should be exported from mapl_mp_utils_export
+   use mapl_MemUtils_mod, only: mapl_MemUtilsInit => MemUtilsInit
+   use mapl_MemUtils_mod, only: mapl_MemUtilsDisable => MemUtilsDisable
+   use mapl_MemUtils_mod, only: mapl_MemUtilsWrite => MemUtilsWrite
+   use mapl_MemUtils_mod, only: mapl_MemUtilsIsDisabled => MemUtilsIsDisabled
+   use mapl_MemUtils_mod, only: mapl_MemUtilsFree => MemUtilsFree
+   use mapl_MemUtils_mod, only: mapl_MemCommited => MemCommited
+   use mapl_MemUtils_mod, only: mapl_MemUsed => MemUsed
+   use mapl_MemUtils_mod, only: mapl_MemReport => MemReport
+   use mapl_MemUtils_mod, only: mapl_MemUtilsModeNode => MemUtilsModeNode
+   use mapl_MemUtils_mod, only: mapl_MemUtilsModeFull => MemUtilsModeFull
+   use mapl_MemUtils_mod, only: mapl_MemUtilsModeBase => MemUtilsModeBase
+   use mapl_Sun_mod, only: MAPL_SunOrbitCreate, MAPL_SunOrbitCreateFromConfig, &
+         MAPL_SunOrbitCreated, MAPL_SunOrbitDestroy, MAPL_SunOrbitQuery, &
+         MAPL_SunGetInsolation, MAPL_SunGetSolarConstant, &
+          MAPL_SunGetDaylightDuration, MAPL_SunGetDaylightDurationMax, &
+          MAPL_SunGetLocalSolarHourAngle, MAPL_SunOrbit
+   use mapl_FileIO_mod, only: WRITE_PARALLEL
+   use mapl_SimpleBundleMod_impl_mod, only: mapl_SimpleBundleCreate => SimpleBundleCreate
+   use mapl_SimpleBundleMod_impl_mod, only: mapl_SimpleBundlePrint => SimpleBundlePrint
+   use mapl_SimpleBundleMod_impl_mod, only: mapl_SimpleBundleGetIndex => SimpleBundleGetIndex
+   use mapl_SimpleBundleMod_impl_mod, only: mapl_SimpleBundleDestroy => SimpleBundleDestroy
+   use mapl_SimpleBundleMod_impl_mod, only:  mapl_SimpleBundle => SimpleBundle
 
-   use mapl_CompressionSettings_mod, only: mapl_CompressionSettings => CompressionSettings
-   use mapl_SharedIO_mod, only: mapl_bundle_to_metadata => bundle_to_metadata
-   use mapl_SharedIO_mod, only: mapl_esmf_to_pfio_type => esmf_to_pfio_type
-   use mapl_GeomPFIO_mod, only: mapl_GeomPFIO => GeomPFIO
-   use mapl_GeomCategorizer_mod, only: mapl_make_geom_pfio => make_geom_pfio
-   use mapl_pFIOServerBounds_mod, only: mapl_pFIOServerBounds => pFIOServerBounds
-   use mapl_pFIOServerBounds_mod, only: mapl_PFIO_BOUNDS_READ => PFIO_BOUNDS_READ
-   use mapl_DataCollection_mod, only: mapl_DataCollection => DataCollection
-   use mapl_DataCollectionManager_mod, only: MAPL_AddDataCollection => AddDataCollection
-   use mapl_DataCollectionManager_mod, only: mapl_DataCollections => DataCollections
+   use mapl_FileIOShared_mod, only: ArrDescr, ArrDescrInit, ArrDescrSet
+   use mapl_FileIOShared_mod, only: mapl_TileMaskGet => TileMaskGet
+   use mapl_NCIO_mod, only: mapl_VarRead => VarRead
+   use mapl_NCIO_mod, only: mapl_VarWrite => VarWrite
+   use mapl_NCIO_mod, only: mapl_NCIOGetFileType => NCIOGetFileType
+   use mapl_NCIO_mod, only: mapl_IOGetNonDimVars => IOGetNonDimVars
+   use mapl_NCIO_mod, only: mapl_IOCountNonDimVars => IOCountNonDimVars 
+   use mapl_NCIO_mod, only: mapl_IOChangeRes => IOChangeRes
+   use mapl_NCIO_mod, only: mapl_IOCountLevels => IOCountLevels
+   use mapl_locstreammod, only: mapl_LocStreamCreate => LocStreamCreate
+   use mapl_locstreammod, only: mapl_LocStreamAdjustNsubtiles => LocStreamAdjustNsubtiles
+   use mapl_locstreammod, only: mapl_LocStreamTransform => LocStreamTransform
+   use mapl_locstreammod, only: mapl_LocStreamIsAssociated => LocStreamIsAssociated
+   use mapl_locstreammod, only: mapl_LocStreamXformIsAssociated => LocStreamXformIsAssociated
+   use mapl_locstreammod, only: mapl_LocStreamGet => LocStreamGet
+   use mapl_locstreammod, only: mapl_LocStreamCreateXform => LocStreamCreateXform
+   use mapl_locstreammod, only: mapl_LocStreamFracArea => LocStreamFracArea
+   use mapl_locstreammod, only: mapl_GridCoordAdjust => GridCoordAdjust
+   use mapl_locstreammod, only: mapl_LocStreamTileWeight => LocStreamTileWeight
 
-   use mapl_FieldBundleWrite_mod, only: mapl_FieldBundleWriter => FieldBundleWriter
-   use mapl_FieldBundleRead_mod, only: mapl_read_bundle => read_bundle
-   implicit none
+
+
+   implicit none(type,external)
    private
 
+   ! StrTemplate moved to mapl_mp_utils_export
+   public :: mapl_MemUtilsInit, mapl_MemUtilsDisable
+   public :: mapl_MemUtilsWrite, mapl_MemUtilsIsDisabled, mapl_MemUtilsFree
+   public :: mapl_MemCommited, mapl_MemUsed, mapl_MemReport
+   public :: mapl_SunOrbitCreate, mapl_SunOrbitCreateFromConfig
+   public :: mapl_SunOrbitCreated, mapl_SunOrbitDestroy, mapl_SunOrbitQuery
+   public :: mapl_SunGetInsolation, mapl_SunGetSolarConstant
+   public :: mapl_SunGetDaylightDuration, mapl_SunGetDaylightDurationMax
+   public :: mapl_SunGetLocalSolarHourAngle, mapl_SunOrbit
+   public :: WRITE_PARALLEL
+   public :: mapl_SimpleBundleCreate, mapl_SimpleBundlePrint
+   public :: mapl_SimpleBundleGetIndex, mapl_SimpleBundleDestroy, mapl_SimpleBundle
+   public :: ArrDescr, ArrDescrInit, ArrDescrSet
+   public :: mapl_VarRead, mapl_VarWrite, mapl_NCIOGetFileType
+   public :: mapl_IOGetNonDimVars, mapl_IOCountNonDimVars
+   public :: mapl_IOChangeRes, mapl_IOCountLevels
 
-   public :: mapl_CompressionSettings
-   public :: mapl_GeomPFIO
-   public :: mapl_pFIOServerBounds
-   public :: MAPL_PFIO_BOUNDS_READ
-   public :: mapl_DataCollection
-   public :: mapl_DataCollections
-   public :: mapl_AddDataCollection
-   public :: mapl_FieldBundleWriter
+   public :: FileMetaDataUtils
 
-   public :: mapl_make_geom_pfio
-   public :: mapl_bundle_to_metadata
-   public :: mapl_esmf_to_pfio_type
-   public :: mapl_read_bundle
-   
-end module mapl_geomio_api
+end module mapl_base_api
