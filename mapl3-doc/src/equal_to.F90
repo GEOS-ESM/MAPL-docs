34b7afdd959456ef0ec99e639799e7b927bdfa9a
mathomp4 <fortran@gmail.com>
2026-08-17 13:21:36 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@b112e7e1d4408647d41115a30c7e4af2e2c2492f 🚀

--- mapl3-doc/src/equal_to.F90
+++ mapl3-doc/src/equal_to.F90
@@ -1,24 +1,28 @@
 #include "MAPL.h"
 
-submodule (mapl_CoordinateAxis_mod) equal_to_smod
+submodule (mapl_LatLonGeomSpec_mod) equal_to_smod
+   use mapl_CoordinateAxis_mod
+   use mapl_GeomSpec_mod
+   use pfio
    use mapl_ErrorHandling_mod
-   use gftl2_StringVector
-   implicit none(type,external)
-
-contains
+   use esmf
+   implicit none (type, external)
    
-   elemental logical module function equal_to(a, b)
-      type(CoordinateAxis), intent(in) :: a, b
+contains
 
-      ! Do the fast checks first
-      equal_to = size(a%centers) == size(b%centers)
-      if (.not. equal_to) return
-      equal_to = size(a%corners) == size(b%corners)
-      if (.not. equal_to) return
+   pure logical module function equal_to(a, b)
+      class(LatLonGeomSpec), intent(in) :: a
+      class(GeomSpec), intent(in) :: b
 
-      equal_to = all(a%centers == b%centers)
-      if (.not. equal_to) return
-      equal_to = all(a%corners == b%corners)
+      select type (b)
+      type is (LatLonGeomSpec)
+         equal_to = (a%lon_axis == b%lon_axis) .and. (a%lat_axis == b%lat_axis)
+         if (.not. equal_to) return
+         equal_to = (a%decomposition == b%decomposition)
+      class default
+         equal_to = .false.
+      end select
+
    end function equal_to
 
 end submodule equal_to_smod
