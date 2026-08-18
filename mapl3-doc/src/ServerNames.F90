c7ecd96a4f8c4ed4f75e961014e10be2ab95b902
mathomp4 <fortran@gmail.com>
2026-08-18 16:10:00 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@eecf0d88c3f1f407bb9f8c7899dddd472d7db97e 🚀

--- mapl3-doc/src/ServerNames.F90
+++
@@ -1,7 +0,0 @@
-module mapl_DefaultServerNames_mod
-   implicit none
-
-   character(*), parameter :: MAPL_DEFAULT_INPUT_SERVER  = 'mapl_default_input_server'
-   character(*), parameter :: MAPL_DEFAULT_OUTPUT_SERVER = 'mapl_default_output_server'
-
-end module mapl_DefaultServerNames_mod
