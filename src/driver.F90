38d440818b0f2301a0bb569ff3b3859c77d32caf
mathomp4 <fortran@gmail.com>
2026-08-18 17:39:22 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@53b2499f68d3f98da5bb34ceaa29e6301c221c63 🚀

--- src/driver.F90
+++ src/driver.F90
@@ -1,20 +1,50 @@
-#define I_AM_MAIN
-
-#include "MAPL_Generic.h"
-
-program driver_GetHorzIJIndex
-   use MAPL
-   use GridComp, only: SetServices
+program main
+   use ESMF
+   use mapl_MaplGenericComponent
+   use mapl_ConcreteComposite
+   use UserComponent_mod
+   use mapl_AbstractComponent
+   use mapl_AbstractFrameworkComponent
+   use mapl_CompositeComponent
    implicit none
 
-   type (MAPL_Cap) :: cap
-   type (MAPL_FargparseCLI) :: cli
-   type (MAPL_CapOptions) :: cap_options
+   type(MaplGenericComponent), target :: tmp
+   class(AbstractFrameworkComponent), pointer :: child_o => null()
+   class(AbstractFrameworkComponent), pointer :: child_a => null()
+   class(AbstractFrameworkComponent), pointer :: grandchild
+   type(UserComponent) :: gcm
+   type(UserComponent) :: agcm, ogcm
+   type(UserComponent) :: dynamics, physics
+   type(ESMF_Clock) :: clock
    integer :: status
 
-   cli = MAPL_FargparseCLI()
-   cap_options = MAPL_CapOptions(cli)
-   cap = MAPL_Cap('GetHorzIJIndex', SetServices, cap_options = cap_options, _RC)
-   call cap%run(_RC)
+   type(ConcreteComposite), target :: root_composite
+   class(AbstractFrameworkComponent), pointer :: root
 
-end program driver_GetHorzIJIndex
+   call gcm%set_name('GCM')
+   call agcm%set_name('AGCM')
+   call ogcm%set_name('OGCM')
+   call dynamics%set_name('DYN')
+   call physics%set_name('PHYSICS')
+
+
+   root_composite = ConcreteComposite(tmp)
+   root => root_composite%get_component()
+   call root%set_composite(root_composite) ! close the circular structure
+
+   child_a => root%add_child_component('agcm', agcm)
+   grandchild => child_a%add_child_component('dynamics', dynamics)
+   grandchild => child_a%add_child_component('physics', physics)
+
+   child_o => root%add_child_component('ogcm', ogcm)
+
+   call root%run_child('agcm', clock, 'phase', rc=status)
+
+   call child_a%run_child('dynamics', clock, 'phase', rc=status)
+   call child_a%run_child('physics', clock, 'phase', rc=status)
+
+   call root%run_child('ogcm', clock, 'phase', rc=status)
+   
+end program main
+
+   
