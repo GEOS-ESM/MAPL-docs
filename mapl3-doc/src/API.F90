65e4082b625dd85321dd3e6f5b0275c0c7412790
bena-nasa <benjamin.m.auer@nasa.gov>
2026-08-26 16:23:13 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@3bf2bf1a9d7d4acf21b462edb2a3621a6c61fa05 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,124 +1,202 @@
-! Export umbrella for the MAPL superstructure/generic layer.
-! Public API exposed to external consumers.
-module mapl_generic_api
+! Public Export umbrella for the MAPL.enums layer.
+! All enum entities carry the MAPL_ prefix.
+! This is what mapl/MAPL.F90 imports from.
+module mapl_enums_api
+   ! ConservationType
+   use mapl_ConservationType_mod, only: MAPL_CONSERVE_NONE     => CONSERVE_NONE
+   use mapl_ConservationType_mod, only: MAPL_CONSERVE_MASS     => CONSERVE_MASS
+   use mapl_ConservationType_mod, only: MAPL_CONSERVE_ENERGY   => CONSERVE_ENERGY
+   use mapl_ConservationType_mod, only: MAPL_CONSERVE_MOMENTUM => CONSERVE_MOMENTUM
+   use mapl_ConservationType_mod, only: MAPL_ConservationType   => ConservationType
 
-   use mapl_UserSetServices_mod, only: UserSetServices, ProcSetServices, DsoSetServices
+   ! NormalizationType
+   use mapl_NormalizationType_mod, only: MAPL_NORMALIZE_NONE => NORMALIZE_NONE
+   use mapl_NormalizationType_mod, only: MAPL_NORMALIZE_DELP => NORMALIZE_DELP
+   use mapl_NormalizationType_mod, only: MAPL_NORMALIZE_DZ   => NORMALIZE_DZ
+   use mapl_NormalizationType_mod, only: MAPL_NormalizationType => NormalizationType
+   use mapl_NormalizationType_mod, only: operator(==), operator(/=)
 
-   use mapl_OpenMP_Support_mod, only: mapl_find_bounds => find_bounds
-   use mapl_OpenMP_Support_mod, only: mapl_get_num_threads => get_num_threads
-   use mapl_OpenMP_Support_mod, only: mapl_get_current_thread => get_current_thread
+   ! QuantityType
+   use mapl_QuantityType_mod, only: MAPL_QuantityType            => QuantityType
+   use mapl_QuantityType_mod, only: MAPL_MixingRatioBasis        => MixingRatioBasis
+   use mapl_QuantityType_mod, only: MAPL_QUANTITY_UNKNOWN        => QUANTITY_UNKNOWN
+   use mapl_QuantityType_mod, only: MAPL_QUANTITY_MIXING_RATIO   => QUANTITY_MIXING_RATIO
+   use mapl_QuantityType_mod, only: MAPL_QUANTITY_CONCENTRATION  => QUANTITY_CONCENTRATION
+   use mapl_QuantityType_mod, only: MAPL_QUANTITY_TEMPERATURE    => QUANTITY_TEMPERATURE
+   use mapl_QuantityType_mod, only: MAPL_QUANTITY_PRESSURE       => QUANTITY_PRESSURE
+   use mapl_QuantityType_mod, only: MAPL_QUANTITY_EXTENSIVE      => QUANTITY_EXTENSIVE
+   use mapl_QuantityType_mod, only: MAPL_BASIS_NONE              => BASIS_NONE
+   use mapl_QuantityType_mod, only: MAPL_BASIS_WET_MASS          => BASIS_WET_MASS
+   use mapl_QuantityType_mod, only: MAPL_BASIS_DRY_MASS          => BASIS_DRY_MASS
+   use mapl_QuantityType_mod, only: MAPL_BASIS_VOLUME            => BASIS_VOLUME
+   use mapl_QuantityType_mod, only: operator(==), operator(/=)
 
-   use mapl_Generic_mod, &
-       mapl_GridCompAddVarSpec => GridCompAddVarSpec, &
-       mapl_GridCompAddSpec => GridCompAddSpec, &
-       mapl_GridCompAdvertiseVariable => GridCompAdvertiseVariable, &
-       mapl_GridCompGet => GridCompGet, &
-       mapl_GridCompSet => GridCompSet, &
-       mapl_GridCompSetCheckpointControls => GridCompSetCheckpointControls, &
-       mapl_GridCompSetEntryPoint => GridCompSetEntryPoint, &
-       mapl_GridCompAddChild => GridCompAddChild, &
-       mapl_GridCompGetChildName => GridCompGetChildName, &
-       mapl_GridCompRunChild => GridCompRunChild, &
-       mapl_GridCompRunChildren => GridCompRunChildren, &
-       mapl_GridCompGetInternalState => GridCompGetInternalState, &
-       mapl_GridCompSetGeometry => GridCompSetGeometry, &
-       mapl_GridcompGetResource => GridCompGetResource, &
-       mapl_ClockGet => ClockGet, &
-       mapl_GridCompSetGeom => GridCompSetGeom, &
-       mapl_GridCompSetVerticalGrid => GridCompSetVerticalGrid, &
-       mapl_GridCompAddConnection => GridCompAddConnection, &
-       mapl_GridCompAddConnectivity => GridCompAddConnectivity, &
-       mapl_GridCompReexport => GridCompReexport, &
-       mapl_GridCompConnectAll => GridCompConnectAll, &
-       mapl_GridCompTimerStart => GridCompTimerStart, &
-       mapl_GridCompTimerStop => GridCompTimerStop, &
-       mapl_GridCompGetCheckpointDir => GridCompGetCheckpointDir, &
-       mapl_GridCompSetCheckpointControls => GridCompSetCheckpointControls, &
-   !    mapl_STATEITEM_STATE => STATEITEM_STATE, &
-   !    mapl_STATEITEM_FIELDBUNDLE => STATEITEM_FIELDBUNDLE, &
-   !    mapl_STATEITEM_SERVICE => STATEITEM_SERVICE, &
-   !    mapl_STATEITEM_VECTOR => STATEITEM_VECTOR, &
-       MAPL_GridCompGetOuterMeta => GridCompGetOuterMeta, &
-       MAPL_GridCompGetRegistry => GridCompGetRegistry, &
-       MAPL_GridCompIsGeneric => GridCompIsGeneric, &
-       MAPL_GridCompIsUser => GridCompIsUser
+   ! VectorBasisKind
+   use mapl_VectorBasisKind_mod, only: MAPL_VECTOR_BASIS_KIND_INVALID => VECTOR_BASIS_KIND_INVALID
+   use mapl_VectorBasisKind_mod, only: MAPL_VECTOR_BASIS_KIND_GRID    => VECTOR_BASIS_KIND_GRID
+   use mapl_VectorBasisKind_mod, only: MAPL_VECTOR_BASIS_KIND_NS      => VECTOR_BASIS_KIND_NS
+   use mapl_VectorBasisKind_mod, only: MAPL_VectorBasisKind            => VectorBasisKind
+   use mapl_VectorBasisKind_mod, only: operator(==), operator(/=)
 
-   use mapl_GenericGridComp_mod,  &
-       mapl_GridCompCreate => GridCompCreate, &
-       mapl_GenericSetServices => GenericSetServices
+   ! ValidationMode
+   use mapl_ValidationMode_mod, only: MAPL_VALIDATION_MODE_PERMISSIVE => VALIDATION_MODE_PERMISSIVE
+   use mapl_ValidationMode_mod, only: MAPL_VALIDATION_MODE_STRICT     => VALIDATION_MODE_STRICT
+   use mapl_ValidationMode_mod, only: MAPL_ValidationMode              => ValidationMode
 
-    use mapl_VariableSpec_mod
-    use mapl_ComponentSpec_mod
-    use mapl_CheckpointControls_mod, only: mapl_CheckpointControls => CheckpointControls
-    use mapl_ChildSpec_mod
-    use mapl_RestartHandler_mod, only: mapl_RestartHandler => RestartHandler
+   ! VerificationStatus
+   use mapl_VerificationStatus_mod, only: MAPL_VERIFICATION_STATUS_UNVERIFIED   => VERIFICATION_STATUS_UNVERIFIED
+   use mapl_VerificationStatus_mod, only: MAPL_VERIFICATION_STATUS_VERIFIED     => VERIFICATION_STATUS_VERIFIED
+   use mapl_VerificationStatus_mod, only: MAPL_VERIFICATION_STATUS_CF_COMPLIANT => VERIFICATION_STATUS_CF_COMPLIANT
+   use mapl_VerificationStatus_mod, only: MAPL_VerificationStatus                => VerificationStatus
 
-   implicit none
+   ! GenericPhases
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_INIT_PHASE_SEQUENCE  => GENERIC_INIT_PHASE_SEQUENCE
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_INIT_SET_CLOCK       => GENERIC_INIT_SET_CLOCK
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_INIT_GEOM_A          => GENERIC_INIT_GEOM_A
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_INIT_GEOM_B          => GENERIC_INIT_GEOM_B
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_INIT_ADVERTISE       => GENERIC_INIT_ADVERTISE
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_INIT_MODIFY_ADVERTISED => GENERIC_INIT_MODIFY_ADVERTISED
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_INIT_REALIZE         => GENERIC_INIT_REALIZE
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_INIT_READ_RESTART    => GENERIC_INIT_READ_RESTART
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_INIT_USER            => GENERIC_INIT_USER
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_RUN_OFFSET           => GENERIC_RUN_OFFSET
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_RUN_CLOCK_ADVANCE    => GENERIC_RUN_CLOCK_ADVANCE
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_RUN_USER             => GENERIC_RUN_USER
+   use mapl_GenericPhases_mod, only: MAPL_GENERIC_FINALIZE_USER        => GENERIC_FINALIZE_USER
+
+   ! CouplerPhases
+   use mapl_CouplerPhases_mod, only: MAPL_GENERIC_COUPLER_INITIALIZE    => GENERIC_COUPLER_INITIALIZE
+   use mapl_CouplerPhases_mod, only: MAPL_GENERIC_COUPLER_UPDATE        => GENERIC_COUPLER_UPDATE
+   use mapl_CouplerPhases_mod, only: MAPL_GENERIC_COUPLER_INVALIDATE    => GENERIC_COUPLER_INVALIDATE
+   use mapl_CouplerPhases_mod, only: MAPL_GENERIC_COUPLER_CLOCK_ADVANCE => GENERIC_COUPLER_CLOCK_ADVANCE
+
+   ! StateItemAllocation
+   use mapl_StateItemAllocation_mod, only: MAPL_StateItemAllocation              => StateItemAllocation
+   use mapl_StateItemAllocation_mod, only: MAPL_STATEITEM_ALLOCATION_INVALID   => STATEITEM_ALLOCATION_INVALID
+   use mapl_StateItemAllocation_mod, only: MAPL_STATEITEM_ALLOCATION_CREATED   => STATEITEM_ALLOCATION_CREATED
+   use mapl_StateItemAllocation_mod, only: MAPL_STATEITEM_ALLOCATION_INACTIVE  => STATEITEM_ALLOCATION_INACTIVE
+   use mapl_StateItemAllocation_mod, only: MAPL_STATEITEM_ALLOCATION_ACTIVE    => STATEITEM_ALLOCATION_ACTIVE
+   use mapl_StateItemAllocation_mod, only: MAPL_STATEITEM_ALLOCATION_CONNECTED => STATEITEM_ALLOCATION_CONNECTED
+   use mapl_StateItemAllocation_mod, only: MAPL_STATEITEM_ALLOCATION_ALLOCATED => STATEITEM_ALLOCATION_ALLOCATED
+   use mapl_StateItemAllocation_mod, only: operator(==), operator(/=), operator(<), operator(>=)
+
+   ! FieldBundleType_Flag
+   use mapl_FieldBundleType_Flag_mod, only: MAPL_FieldBundleType_Flag               => FieldBundleType_Flag
+   use mapl_FieldBundleType_Flag_mod, only: MAPL_FIELDBUNDLETYPE_BASIC              => FIELDBUNDLETYPE_BASIC
+   use mapl_FieldBundleType_Flag_mod, only: MAPL_FIELDBUNDLETYPE_VECTOR             => FIELDBUNDLETYPE_VECTOR
+   use mapl_FieldBundleType_Flag_mod, only: MAPL_FIELDBUNDLETYPE_BRACKET            => FIELDBUNDLETYPE_BRACKET
+   use mapl_FieldBundleType_Flag_mod, only: MAPL_FIELDBUNDLETYPE_VECTORBRACKET      => FIELDBUNDLETYPE_VECTORBRACKET
+   use mapl_FieldBundleType_Flag_mod, only: MAPL_FIELDBUNDLETYPE_SERVICE            => FIELDBUNDLETYPE_SERVICE
+   use mapl_FieldBundleType_Flag_mod, only: MAPL_FIELDBUNDLETYPE_SERVICE_AGGREGATE  => FIELDBUNDLETYPE_SERVICE_AGGREGATE
+   use mapl_FieldBundleType_Flag_mod, only: MAPL_FIELDBUNDLETYPE_SERVICE_SEPARATE   => FIELDBUNDLETYPE_SERVICE_SEPARATE
+   use mapl_FieldBundleType_Flag_mod, only: MAPL_FIELDBUNDLETYPE_INVALID            => FIELDBUNDLETYPE_INVALID
+   use mapl_FieldBundleType_Flag_mod, only: operator(==), operator(/=)
+
+   ! Vertical enums
+   ! VerticalStaggerLoc
+   ! TODO: pchakrab - remove aliases once GEOS is updated to use the prefixed enums
+   use mapl_VerticalStaggerLoc_mod, only: MAPL_VerticalStaggerLoc       => VerticalStaggerLoc
+   use mapl_VerticalStaggerLoc_mod, only: MAPL_VERTICAL_STAGGER_NONE    => VERTICAL_STAGGER_NONE
+   use mapl_VerticalStaggerLoc_mod, only: MAPL_VERTICAL_STAGGER_EDGE    => VERTICAL_STAGGER_EDGE
+   use mapl_VerticalStaggerLoc_mod, only: MAPL_VERTICAL_STAGGER_CENTER  => VERTICAL_STAGGER_CENTER
+   use mapl_VerticalStaggerLoc_mod, only: MAPL_VERTICAL_STAGGER_MIRROR  => VERTICAL_STAGGER_MIRROR
+   use mapl_VerticalStaggerLoc_mod, only: MAPL_VERTICAL_STAGGER_INVALID => VERTICAL_STAGGER_INVALID
+
+   ! VerticalAlignment
+   use mapl_VerticalAlignment_mod, only: MAPL_VerticalAlignment => VerticalAlignment
+   use mapl_VerticalAlignment_mod, only: MAPL_VALIGN_WITH_GRID => VALIGN_WITH_GRID
+   use mapl_VerticalAlignment_mod, only: MAPL_VALIGN_UP => VALIGN_UP
+   use mapl_VerticalAlignment_mod, only: MAPL_VALIGN_DOWN => VALIGN_DOWN
+   use mapl_VerticalAlignment_mod, only: MAPL_VALIGN_INVALID => VALIGN_INVALID
+
+   use mapl_QuantityTypeMetadata_mod, mapl_QuantityTypeMetadata => QuantityTypeMetadata, &
+        mapl_MakeQuantityTypeMetadata => make_QuantityTypeMetadata
+   use mapl_NormalizationMetadata_mod, mapl_NormalizationMetadata => NormalizationMetadata, &
+        mapl_MakeNormalizationMetadata => make_NormalizationMetadata
+   use mapl_ConservationMetadata_mod, mapl_ConservationMetadata => ConservationMetadata, &
+        mapl_MakeConservationMetadata => make_ConservationMetadata
+   implicit none(type, external)
    private
 
-   ! These should be available to users
-   public :: mapl_GridCompAddVarSpec
-   public :: mapl_GridCompAddSpec
-   public :: mapl_GridCompAdvertiseVariable
+   ! CouplerPhases
+   public :: MAPL_GENERIC_COUPLER_INITIALIZE, MAPL_GENERIC_COUPLER_UPDATE
+   public :: MAPL_GENERIC_COUPLER_INVALIDATE, MAPL_GENERIC_COUPLER_CLOCK_ADVANCE
 
-   public :: mapl_GridCompGet
-   public :: mapl_GridCompGetRegistry
-   public :: mapl_GridCompSet
-   public :: mapl_GridCompSetCheckpointControls
-   public :: mapl_GridCompSetEntryPoint
+   ! GenericPhases
+   public :: MAPL_GENERIC_INIT_PHASE_SEQUENCE
+   public :: MAPL_GENERIC_INIT_SET_CLOCK
+   public :: MAPL_GENERIC_INIT_GEOM_A
+   public :: MAPL_GENERIC_INIT_GEOM_B
+   public :: MAPL_GENERIC_INIT_ADVERTISE
+   public :: MAPL_GENERIC_INIT_MODIFY_ADVERTISED
+   public :: MAPL_GENERIC_INIT_REALIZE
+   public :: MAPL_GENERIC_INIT_READ_RESTART
+   public :: MAPL_GENERIC_INIT_USER
 
-   public :: MAPL_GridCompIsGeneric
-   public :: MAPL_GridCompIsUser
-   public :: MAPL_GridCompGetOuterMeta
+   ! Run phases
+   public :: MAPL_GENERIC_RUN_OFFSET
+   public :: MAPL_GENERIC_RUN_CLOCK_ADVANCE
+   public :: MAPL_GENERIC_RUN_USER
 
-   public :: mapl_GridCompAddChild
-   public :: mapl_GridCompGetChildName
-   public :: mapl_GridCompRunChild
-   public :: mapl_GridCompRunChildren
+   ! Finalize phases
+   public :: MAPL_GENERIC_FINALIZE_USER
 
-   public :: mapl_GridCompGetInternalState
+   ! StateItemAllocation
+   public :: MAPL_StateItemAllocation
+   public :: MAPL_STATEITEM_ALLOCATION_INVALID, MAPL_STATEITEM_ALLOCATION_CREATED
+   public :: MAPL_STATEITEM_ALLOCATION_INACTIVE, MAPL_STATEITEM_ALLOCATION_ACTIVE
+   public :: MAPL_STATEITEM_ALLOCATION_CONNECTED, MAPL_STATEITEM_ALLOCATION_ALLOCATED
+   public :: operator(==), operator(/=), operator(<), operator(>=)
 
-   public :: mapl_GridCompSetGeometry
+   ! FieldBundleType_Flag
+   public :: MAPL_FieldBundleType_Flag
+   public :: MAPL_FIELDBUNDLETYPE_BASIC, MAPL_FIELDBUNDLETYPE_VECTOR
+   public :: MAPL_FIELDBUNDLETYPE_BRACKET, MAPL_FIELDBUNDLETYPE_VECTORBRACKET
+   public :: MAPL_FIELDBUNDLETYPE_SERVICE, MAPL_FIELDBUNDLETYPE_SERVICE_AGGREGATE
+   public :: MAPL_FIELDBUNDLETYPE_SERVICE_SEPARATE, MAPL_FIELDBUNDLETYPE_INVALID
 
-   public :: mapl_GridcompGetResource
+   ! VectorBasisKind
+   public :: MAPL_VectorBasisKind
+   public :: MAPL_VECTOR_BASIS_KIND_INVALID, MAPL_VECTOR_BASIS_KIND_GRID
+   public :: MAPL_VECTOR_BASIS_KIND_NS
 
-   public :: mapl_ClockGet
+   ! Vertical stagger locations
+   ! TODO: pchakrab - remove aliases once GEOS is updated to use the prefixed enums
+   public :: MAPL_VerticalStaggerLoc
+   public :: MAPL_VERTICAL_STAGGER_NONE
+   public :: MAPL_VERTICAL_STAGGER_EDGE
+   public :: MAPL_VERTICAL_STAGGER_CENTER
+   public :: MAPL_VERTICAL_STAGGER_MIRROR
+   public :: MAPL_VERTICAL_STAGGER_INVALID
 
-   ! Accessors
-!!$   public :: mapl_GetOrbit
-!!$   public :: mapl_GetCoordinates
-!!$   public :: mapl_GetLayout
+   ! Vertical alignment
+   public :: MAPL_VerticalAlignment
+   public :: MAPL_VALIGN_WITH_GRID
+   public :: MAPL_VALIGN_UP
+   public :: MAPL_VALIGN_DOWN
+   public :: MAPL_VALIGN_INVALID
 
-   public :: mapl_GridCompSetGeom
-   public :: mapl_GridCompSetVerticalGrid
+   ! ConservationType
+   public :: MAPL_ConservationType
+   public :: MAPL_CONSERVE_NONE, MAPL_CONSERVE_MASS
+   public :: MAPL_CONSERVE_ENERGY, MAPL_CONSERVE_MOMENTUM
 
-   ! Connections
-   public :: mapl_GridCompAddConnection
-   public :: mapl_GridCompAddConnectivity  ! Legacy name - temporary backward compatibility
-   public :: mapl_GridCompReexport
-   public :: mapl_GridCompConnectAll
+   ! NormalizationType
+   public :: MAPL_NormalizationType
+   public :: MAPL_NORMALIZE_NONE, MAPL_NORMALIZE_DELP, MAPL_NORMALIZE_DZ
 
-   ! Timers
-   public :: mapl_GridCompTimerStart
-   public :: mapl_GridCompTimerStop
+   ! QuantityType
+   public :: MAPL_QuantityType, MAPL_MixingRatioBasis
+   public :: MAPL_QUANTITY_UNKNOWN, MAPL_QUANTITY_MIXING_RATIO
+   public :: MAPL_QUANTITY_CONCENTRATION, MAPL_QUANTITY_TEMPERATURE
+   public :: MAPL_QUANTITY_PRESSURE, MAPL_QUANTITY_EXTENSIVE
+   public :: MAPL_BASIS_NONE, MAPL_BASIS_WET_MASS, MAPL_BASIS_DRY_MASS, MAPL_BASIS_VOLUME
 
-   ! Checkpoint directory
-   public :: mapl_GridCompGetCheckpointDir
+   public :: mapl_QuantityTypeMetadata
+   public :: mapl_MakeQuantityTypeMetadata
 
-   ! Spec types
-   public :: mapl_STATEITEM_STATE, mapl_STATEITEM_FIELDBUNDLE
-   public :: mapl_STATEITEM_SERVICE, mapl_STATEITEM_VECTOR
-
-   public :: mapl_find_bounds
-   public :: mapl_get_num_threads
-   public :: mapl_get_current_thread
-
-   public :: mapl_GridCompCreate
-   public :: mapl_GenericSetServices
-   public :: UserSetServices
-   public :: ProcSetServices
-   public :: DsoSetServices
-
-   public :: mapl_CheckpointControls
-   public :: mapl_RestartHandler
-end module mapl_generic_api
+   public :: mapl_NormalizationMetadata
+   public :: mapl_MakeNormalizationMetadata
+   public :: mapl_ConservationMetadata
+   public :: mapl_MakeConservationMetadata
+end module mapl_enums_api
--- mapl3-doc/src/make_geom.F90
+++ mapl3-doc/src/make_geom.F90
@@ -1,37 +1,27 @@
 #include "MAPL.h"
 
-submodule (mapl_LatLonGeomFactory_mod) make_geom_smod
-
+submodule (mapl_EASEGeomFactory_mod) make_geom_smod
    use mapl_GeomSpec_mod
-   use mapl_LonAxis_mod
-   use mapl_LatAxis_mod
-   use mapl_LatLonDecomposition_mod
-   use mapl_LatLonGeomSpec_mod
-   use mapl_MinMax_mod
+   use mapl_EASEGeomSpec_mod
    use mapl_ErrorHandling_mod
-   use MAPL_Constants
-   use pFIO
-   use gFTL2_StringVector
    use esmf
-   use mapl_KeywordEnforcer_mod, only: KE => KeywordEnforcer
-
    implicit none (type, external)
 
 contains
 
    module function make_geom(this, geom_spec, rc) result(geom)
       type(ESMF_Geom) :: geom
-      class(LatLonGeomFactory), intent(in) :: this
+      class(EASEGeomFactory), intent(in) :: this
       class(GeomSpec), intent(in) :: geom_spec
       integer, optional, intent(out) :: rc
 
       integer :: status
 
       select type (geom_spec)
-      type is (LatLonGeomSpec)
+      type is (EASEGeomSpec)
          geom = typesafe_make_geom(geom_spec, _RC)
       class default
-         _FAIL("geom_spec type not supported")
+         _FAIL("make_geom: geom_spec is not of dynamic type EASEGeomSpec")
       end select
 
       _RETURN(_SUCCESS)
