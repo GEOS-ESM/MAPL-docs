b50ff674fc271e00b4ad852f45196c08367f0780
darianboggs <61847056+darianboggs@users.noreply.github.com>
2026-08-27 18:32:02 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@269b1075137667facbabc98190d02e82634fe53a 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,35 +1,93 @@
-! Export umbrella for the MAPL infrastructure/vertical/vertical_grid layer.
-! Public API exposed to external consumers.
-module mapl_vertical_grid_api
+! Public Export umbrella for the MAPL.mp_utils layer.
+! Re-exports only the symbols that are part of MAPL's public API
+! (i.e., entities carrying the MAPL_ prefix).
+! This is what mapl/MAPL.F90 imports from.
+module mapl_mp_utils_api
+   use mapl_ArrayReductions_mod, only: MAPL_AreaMean => AreaMean
+   use mapl_ArrayReductions_mod, only: MAPL_MaxMin => MaxMin
+   ! Import all PackedTime functions (both prefixed and unprefixed)
+   use mapl_PackedTime_mod,      only: MAPL_PackedDateCreate => PackedDateCreate, &
+                                       MAPL_PackedTimeCreate => PackedTimeCreate, &
+                                       MAPL_PackedDateTimeCreate => PackedDateTimeCreate, &
+                                       MAPL_ESMFTimeFromPacked => ESMFTimeFromPacked, &
+                                       MAPL_UnpackDate => UnpackDate, &
+                                       MAPL_UnpackTime => UnpackTime, &
+                                       MAPL_UnpackDateTime => UnpackDateTime
 
-   use mapl_VerticalGrid_mod, only: mapl_VerticalGrid => VerticalGrid
-   use mapl_VerticalGrid_mod, only: MAPL_VERTICAL_GRID_NOT_FOUND => VERTICAL_GRID_NOT_FOUND
-   use mapl_VerticalGridSpec_mod, only: mapl_VerticalGridSpec => VerticalGridSpec
-   use mapl_VerticalGridFactory_mod, only: mapl_VerticalGridFactory => VerticalGridFactory
-   use mapl_BasicVerticalGrid_mod, only: mapl_BasicVerticalGrid => BasicVerticalGrid
-   use mapl_BasicVerticalGrid_mod, only: mapl_BasicVerticalGridSpec => BasicVerticalGridSpec
-   use mapl_BasicVerticalGrid_mod, only: mapl_BasicVerticalGridFactory => BasicVerticalGridFactory
-   use mapl_VerticalGridManager_mod, only: mapl_VerticalGridManager => VerticalGridManager
-   use mapl_VerticalGridManager_mod, only: mapl_get_vertical_grid_manager => get_vertical_grid_manager
+   use mapl_StringTemplate_mod,  only: mapl_StrTemplate => StrTemplate
+   use mapl_StringTemplate_mod,  only: mapl_fill_grads_template => fill_grads_template
+   use mapl_StringTemplate_mod,  only: mapl_fill_grads_template_esmf => fill_grads_template_esmf
 
+   use mapl_Shmem_mod, only: MAPL_GetNodeInfo => GetNodeInfo
+   use mapl_Shmem_mod, only: MAPL_CoresPerNodeGet => CoresPerNodeGet
+   use mapl_Shmem_mod, only: MAPL_InitializeShmem => InitializeShmem
+   use mapl_Shmem_mod, only: MAPL_FinalizeShmem => FinalizeShmem
+   use mapl_Shmem_mod, only: MAPL_AllocNodeArray => AllocNodeArray
+   use mapl_Shmem_mod, only: MAPL_DeAllocNodeArray => DeAllocNodeArray
+   use mapl_Shmem_mod, only: MAPL_ShmemAmOnFirstNode => ShmemAmOnFirstNode
+   use mapl_Shmem_mod, only: MAPL_SyncSharedMemory => SyncSharedMemory
+   use mapl_Shmem_mod, only: MAPL_BroadcastToNodes => BroadcastToNodes
+   use mapl_Shmem_mod, only: MAPL_AllocateShared => AllocateShared
+   use mapl_Shmem_mod, only: mapl_GetSharedMemory => GetSharedMemory
+   use mapl_Shmem_mod, only: mapl_ReleaseSharedMemory => ReleaseSharedMemory
+   use mapl_Shmem_mod, only: MAPL_GetNewRank => GetNewRank
+   use mapl_Shmem_mod, only: MAPL_NodeComm
+   use mapl_Shmem_mod, only: MAPL_NodeRootsComm
+   use mapl_Shmem_mod, only: MAPL_MyNodeNum
+   use mapl_Shmem_mod, only: MAPL_AmNodeRoot
+   use mapl_Shmem_mod, only: MAPL_ShmInitialized
+   use mapl_LoadBalance_mod, only: MAPL_BalanceWork => BalanceWork, &
+                                   MAPL_BalanceCreate => BalanceCreate, &
+                                   MAPL_BalanceDestroy => BalanceDestroy, &
+                                   MAPL_BalanceGet => BalanceGet
    implicit none
    private
 
-   ! Abstract base types
-   public :: mapl_VerticalGrid
-   public :: mapl_VerticalGridSpec
-   public :: mapl_VerticalGridFactory
+   ! Statistics
+   public :: MAPL_MaxMin
+   public :: MAPL_AreaMean
 
-   ! Concrete basic implementation
-   public :: mapl_BasicVerticalGrid
-   public :: mapl_BasicVerticalGridSpec
-   public :: mapl_BasicVerticalGridFactory
+   ! Time packing
+   public :: MAPL_UnpackTime
+   public :: MAPL_UnpackDate
+   public :: MAPL_UnpackDateTime
 
-   ! Manager
-   public :: mapl_VerticalGridManager
-   public :: mapl_get_vertical_grid_manager
+   ! PackedTime functions with MAPL_ prefix
+   public :: MAPL_PackedDateCreate
+   public :: MAPL_PackedTimeCreate
+   public :: MAPL_PackedDateTimeCreate
+   public :: MAPL_ESMFTimeFromPacked
 
-   ! Parameters
-   public :: MAPL_VERTICAL_GRID_NOT_FOUND
+   public :: mapl_StrTemplate
 
-end module mapl_vertical_grid_api
+   public :: mapl_fill_grads_template
+   public :: mapl_fill_grads_template_esmf
+   
+   public :: MAPL_GetNodeInfo
+   public :: MAPL_CoresPerNodeGet
+   public :: MAPL_InitializeShmem
+   public :: MAPL_FinalizeShmem
+   
+   public :: MAPL_AllocNodeArray
+   public :: MAPL_DeAllocNodeArray
+   public :: MAPL_ShmemAmOnFirstNode
+   public :: MAPL_SyncSharedMemory
+   public :: MAPL_BroadcastToNodes
+   
+   public :: MAPL_AllocateShared
+   public :: MAPL_GetSharedMemory
+   public :: MAPL_ReleaseSharedMemory
+
+   public :: MAPL_GetNewRank
+   public :: MAPL_NodeComm
+   public :: MAPL_NodeRootsComm
+   public :: MAPL_MyNodeNum
+   public :: MAPL_AmNodeRoot
+   public :: MAPL_ShmInitialized
+
+   public :: MAPL_BalanceWork
+   public :: MAPL_BalanceCreate
+   public :: MAPL_BalanceDestroy
+   public :: MAPL_BalanceGet
+
+end module mapl_mp_utils_api
