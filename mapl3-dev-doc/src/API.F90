9e849b46c6b2f118235596c81906ff9b365a77b6
mathomp4 <fortran@gmail.com>
2026-08-28 17:30:31 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@8bff952fa7f7cb5c00ef7b5a111442fee08aee91 🚀

--- mapl3-dev-doc/src/API.F90
+++ mapl3-dev-doc/src/API.F90
@@ -1,7 +1,35 @@
-module mapl_superstructure_api
+! Export umbrella for the MAPL infrastructure/vertical/vertical_grid layer.
+! Public API exposed to external consumers.
+module mapl_vertical_grid_api
 
-   use mapl_generic_api
-   use mapl_state_api
-   use mapl_component_api
+   use mapl_VerticalGrid_mod, only: mapl_VerticalGrid => VerticalGrid
+   use mapl_VerticalGrid_mod, only: MAPL_VERTICAL_GRID_NOT_FOUND => VERTICAL_GRID_NOT_FOUND
+   use mapl_VerticalGridSpec_mod, only: mapl_VerticalGridSpec => VerticalGridSpec
+   use mapl_VerticalGridFactory_mod, only: mapl_VerticalGridFactory => VerticalGridFactory
+   use mapl_BasicVerticalGrid_mod, only: mapl_BasicVerticalGrid => BasicVerticalGrid
+   use mapl_BasicVerticalGrid_mod, only: mapl_BasicVerticalGridSpec => BasicVerticalGridSpec
+   use mapl_BasicVerticalGrid_mod, only: mapl_BasicVerticalGridFactory => BasicVerticalGridFactory
+   use mapl_VerticalGridManager_mod, only: mapl_VerticalGridManager => VerticalGridManager
+   use mapl_VerticalGridManager_mod, only: mapl_get_vertical_grid_manager => get_vertical_grid_manager
 
-end module mapl_superstructure_api
+   implicit none
+   private
+
+   ! Abstract base types
+   public :: mapl_VerticalGrid
+   public :: mapl_VerticalGridSpec
+   public :: mapl_VerticalGridFactory
+
+   ! Concrete basic implementation
+   public :: mapl_BasicVerticalGrid
+   public :: mapl_BasicVerticalGridSpec
+   public :: mapl_BasicVerticalGridFactory
+
+   ! Manager
+   public :: mapl_VerticalGridManager
+   public :: mapl_get_vertical_grid_manager
+
+   ! Parameters
+   public :: MAPL_VERTICAL_GRID_NOT_FOUND
+
+end module mapl_vertical_grid_api
