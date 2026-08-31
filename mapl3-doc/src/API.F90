aec0805acc789dfbaf04abe010ea8a8ae13ea4c4
mathomp4 <fortran@gmail.com>
2026-08-31 13:57:39 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@d3dd7a3a3a3b58861adc4990647fae00bc623336 🚀

--- mapl3-doc/src/API.F90
+++ mapl3-doc/src/API.F90
@@ -1,30 +1,10 @@
-! Export umbrella for the MAPL.state library.
-module mapl_state_api
+module mapl_infrastructure_api
+   use mapl_esmf_api
+   use mapl_field_api
+   use mapl_field_bundle_api
+   use mapl_geom_api
+   use mapl_vertical_grid_api
+   use mapl_regridder_mgr_api
+   use mapl_geomio_api
 
-   use mapl_StateArithmeticParser_mod, only: MAPL_ParserVariablesInExpression => parser_variables_in_expression
-   use mapl_StateArithmeticParser_mod, only: mapl_StateEval => StateEval
-   use mapl_StateAddMethod_mod, only: mapl_StateAddMethod => StateAddMethod
-   use mapl_StateDestroy_mod, only: MAPL_StateDestroy => StateDestroy
-   use mapl_StateGet_mod, only: MAPL_StateGet => StateGet
-   use mapl_StateSet_mod, only: MAPL_StateSet => StateSet
-   use mapl_StateGetGeom_mod, only: MAPL_StateGetGeom => StateGetGeom
-   use mapl_StateGetPointer_mod, only: MAPL_StateGetPointer => StateGetPointer
-   use mapl_StateMask_mod, only: MAPL_StateMask => StateMask
-   use mapl_StateMerge_mod, only: MAPL_StateMerge => StateMerge
-   
-
-   implicit none
-   private
-
-   public :: MAPL_ParserVariablesInExpression
-   public :: mapl_StateAddMethod
-   public :: MAPL_StateDestroy
-   public :: MAPL_StateGet
-   public :: MAPL_StateSet
-   public :: MAPL_StateGetGeom
-   public :: MAPL_StateGetPointer
-   public :: mapl_StateEval
-
-   public :: MAPL_StateMask
-   public :: MAPL_StateMerge
-end module mapl_state_api
+end module mapl_infrastructure_api
