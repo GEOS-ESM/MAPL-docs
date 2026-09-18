ccc3bcb5f8bb6be9e4dbf3bd42c73e631b08e1d6
bena-nasa <benjamin.m.auer@nasa.gov>
2026-09-18 20:44:15 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@c1da54dc572d5ecd50475cc5dbc47806eba8b898 🚀

--- mapl3-doc/src/supports_hconfig.F90
+++ mapl3-doc/src/supports_hconfig.F90
@@ -1,31 +1,35 @@
 #include "MAPL.h"
 
-submodule (mapl_XYGeomSpec_mod) supports_hconfig_smod
+submodule (mapl_LonAxis_mod) supports_hconfig_smod
+   use mapl_Range_mod
    use mapl_ErrorHandling_mod
    use esmf
-   implicit none
+   implicit none (type, external)
+   integer, parameter :: R8 = ESMF_KIND_R8
 
 contains
 
-   ! An hconfig block is an XY grid if it has:
-   !   class: xy
-   !   grid_file_name: <path>
-   logical module function supports_hconfig_(this, hconfig, rc) result(supports)
-      class(XYGeomSpec), intent(in) :: this
+   logical module function supports_hconfig(hconfig, rc) result(supports)
       type(ESMF_HConfig), intent(in) :: hconfig
       integer, optional, intent(out) :: rc
 
       integer :: status
-      character(len=:), allocatable :: geom_class
+      logical :: has_im_world
+      logical :: has_lon_range
+      logical :: has_dateline
 
-      supports = ESMF_HConfigIsDefined(hconfig, keystring='class', _RC)
-      _RETURN_UNLESS(supports)
+      supports = .true.
 
-      geom_class = ESMF_HConfigAsString(hconfig, keyString='class', _RC)
-      supports = (geom_class == 'xy')
+      has_im_world = ESMF_HConfigIsDefined(hconfig, keystring='lon_range', _RC)
+      _RETURN_UNLESS(has_im_world)
+
+      has_lon_range = ESMF_HConfigIsDefined(hconfig, keystring='lon_range', _RC)
+      has_dateline = ESMF_HConfigIsDefined(hconfig, keystring='dateline', _RC)
+      _RETURN_UNLESS(has_lon_range .neqv. has_dateline)
+      supports = .true.
 
       _RETURN(_SUCCESS)
-      _UNUSED_DUMMY(this)
-   end function supports_hconfig_
+   end function supports_hconfig
 
 end submodule supports_hconfig_smod
+
