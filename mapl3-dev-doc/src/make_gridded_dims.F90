11c12fcfe4f6b845a8fb016d5f1cc0a6e1105e27
bena-nasa <benjamin.m.auer@nasa.gov>
2026-09-18 14:43:05 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@622f3ce79881b670aecc224e311e4c4cc96dc120 🚀

--- mapl3-dev-doc/src/make_gridded_dims.F90
+++ mapl3-dev-doc/src/make_gridded_dims.F90
@@ -1,42 +1,29 @@
 #include "MAPL.h"
 
-submodule (mapl_LatLonGeomFactory_mod) make_gridded_dims_smod
-
-   use mapl_GeomSpec_mod
-   use mapl_LonAxis_mod
-   use mapl_LatAxis_mod
-   use mapl_LatLonDecomposition_mod
-   use mapl_LatLonGeomSpec_mod
-   use mapl_MinMax_mod
+submodule (mapl_XYGeomFactory_mod) make_gridded_dims_smod
    use mapl_ErrorHandling_mod
-   use MAPL_Constants
-   use pFIO
-   use gFTL2_StringVector
-   use esmf
-   use mapl_KeywordEnforcer_mod, only: KE => KeywordEnforcer
-
-   implicit none (type, external)
+   use gftl2_StringVector
+   implicit none
 
 contains
 
    module function make_gridded_dims(this, geom_spec, rc) result(gridded_dims)
       type(StringVector) :: gridded_dims
-      class(LatLonGeomFactory), intent(in) :: this
+      class(XYGeomFactory), intent(in) :: this
       class(GeomSpec), intent(in) :: geom_spec
       integer, optional, intent(out) :: rc
 
       gridded_dims = StringVector()
       select type (geom_spec)
-      type is (LatLonGeomSpec)
-         call gridded_dims%push_back('lon')
-         call gridded_dims%push_back('lat')
+      type is (XYGeomSpec)
+         call gridded_dims%push_back('Xdim')
+         call gridded_dims%push_back('Ydim')
       class default
-         _FAIL('geom_spec is not of dynamic type LatLonGeomSpec.')
+         _FAIL('geom_spec is not of dynamic type XYGeomSpec.')
       end select
 
       _RETURN(_SUCCESS)
       _UNUSED_DUMMY(this)
    end function make_gridded_dims
-
 
 end submodule make_gridded_dims_smod
