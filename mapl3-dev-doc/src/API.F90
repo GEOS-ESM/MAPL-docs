8b82e525b478607a87b34f5ff33ce33fc9a1839a
tclune <thomas.l.clune@nasa.gov>
2026-09-24 15:00:26 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@1a2fa05c9576617461c249932f837a93feef7929 🚀

--- mapl3-dev-doc/src/API.F90
+++ mapl3-dev-doc/src/API.F90
@@ -1,71 +1,7 @@
-! Export umbrella for the MAPL infrastructure/geom layer.
-! Public API exposed to external consumers.
-module mapl_geom_api
+module mapl_superstructure_api
 
-   use mapl_GeomId_mod, only: mapl_GeomId => GeomId
-   use mapl_GeomId_mod, only: mapl_GeomIdManager => GeomIdManager
-   use mapl_GeomId_mod, only: mapl_get_geom_id_manager => get_geom_id_manager
-   use mapl_GeomId_mod, only: mapl_new_GeomId => GeomId
-   use mapl_MaplGeom_mod, only: mapl_MaplGeom => MaplGeom
-   use mapl_GeomSpec_mod, only: mapl_GeomSpec => GeomSpec
-   use mapl_GeomManager_mod, only: mapl_GeomManager => GeomManager
-   use mapl_GeomManager_mod, only: mapl_get_geom_manager => get_geom_manager
-   use mapl_GeomManager_mod, only: mapl_get_mapl_geom => get_mapl_geom
-   use mapl_GeomUtilities_mod, only: mapl_SameGeom => SameGeom, mapl_GeomGetId => GeomGetId
-   use mapl_GeomAccessors_mod, only: mapl_GeomGet => GeomGet
-   use mapl_GeomAccessors_mod, only: mapl_GeomGetHorzIJIndex => GeomGetHorzIJIndex
-   use mapl_GeomAccessors_mod, only: mapl_GridGetHorzIJIndex => GridGetHorzIJIndex
-   use mapl_GridAccessors_mod, only: mapl_GridGet => GridGet
-   use mapl_GridAccessors_mod, only: mapl_GridGetCoordinates => GridGetCoordinates
-   use mapl_GridAccessors_mod, only: mapl_GridHasDE => grid_has_DE
-   use mapl_GridGetGlobal_mod, only: mapl_GridGetGlobalCellCountPerDim => GridGetGlobalCellCountPerDim
-   use mapl_GridComms_mod, only: MAPL_CollectiveGather3D => mapl_CollectiveGather3D
-   use mapl_GridComms_mod, only: MAPL_CollectiveScatter3D => mapl_CollectiveScatter3D
-   use mapl_Subgrid_mod, only: mapl_Interval => Interval
-   use mapl_Subgrid_mod, only: mapl_make_subgrids => make_subgrids
-   use mapl_Subgrid_mod, only: mapl_find_bounds => find_bounds
-   use mapl_CubedSphereGeomSpec_mod, only: mapl_CubedSphereGeomSpec => CubedSphereGeomSpec
-   use mapl_CubedSphereGeomSpec_mod, only: mapl_make_CubedSphereGeomSpec => make_CubedSphereGeomSpec
-   use mapl_CubedSphereDecomposition_mod, only: mapl_CubedSphereDecomposition => CubedSphereDecomposition
-   use mapl_CubedSphereDecomposition_mod, only: mapl_make_CubedSphereDecomposition => make_CubedSphereDecomposition
+   use mapl_generic_api
+   use mapl_state_api
+   use mapl_component_api
 
-   implicit none
-   private
-
-   ! Geom types and manager
-   public :: mapl_GeomId
-   public :: mapl_GeomIdManager
-   public :: mapl_get_geom_id_manager
-   public :: mapl_new_GeomId
-   public :: mapl_MaplGeom
-   public :: mapl_GeomSpec
-   public :: mapl_GeomManager
-   public :: mapl_get_geom_manager
-   public :: mapl_get_mapl_geom
-
-   ! Geom utilities
-   public :: mapl_SameGeom
-   public :: mapl_GeomGetId
-   public :: mapl_GeomGet
-   public :: mapl_GeomGetHorzIJIndex
-   public :: mapl_GridGetHorzIJIndex
-   public :: mapl_GridGet
-   public :: mapl_GridGetCoordinates
-   public :: mapl_GridHasDE
-   public :: mapl_GridGetGlobalCellCountPerDim
-
-   ! Collective comms
-   public :: MAPL_CollectiveGather3D
-   public :: MAPL_CollectiveScatter3D
-
-   ! Subgrid
-   public :: mapl_Interval
-   public :: mapl_make_subgrids
-
-   ! CubedSphere geom specs
-   public :: mapl_CubedSphereGeomSpec
-   public :: mapl_make_CubedSphereGeomSpec
-   public :: mapl_CubedSphereDecomposition
-   public :: mapl_make_CubedSphereDecomposition
-
-end module mapl_geom_api
+end module mapl_superstructure_api
--- mapl3-dev-doc/src/supports_hconfig.F90
+++ mapl3-dev-doc/src/supports_hconfig.F90
@@ -1,36 +1,43 @@
 #include "MAPL.h"
 
-submodule (mapl_LatAxis_mod) supports_hconfig_smod
-   use mapl_Range_mod
-!   use hconfig3g
-   use esmf
-   use mapl_ErrorHandling_mod
-   implicit none (type, external)
+submodule (mapl_LatLonGeomSpec_mod) supports_hconfig_smod
 
-   integer, parameter :: R8 = ESMF_KIND_R8
+   use mapl_CoordinateAxis_mod
+   use mapl_GeomSpec_mod
+   use pfio
+   use mapl_ErrorHandling_mod
+   use esmf
+
+   implicit none (type, external)
 
 contains
 
-   logical module function supports_hconfig(hconfig, rc) result(supports)
+   logical module function supports_hconfig_(this, hconfig, rc) result(supports)
+      class(LatLonGeomSpec), intent(in) :: this
       type(ESMF_HConfig), intent(in) :: hconfig
       integer, optional, intent(out) :: rc
 
       integer :: status
-      logical :: has_jm_world
-      logical :: has_lat_range
-      logical :: has_pole
-      supports = .true.
+      type(LonAxis) :: lon_axis
+      type(LatAxis) :: lat_axis
+      character(:), allocatable :: geom_class
 
-      has_jm_world = ESMF_HConfigIsDefined(hconfig, keystring='jm_world', _RC)
-      _RETURN_UNLESS(has_jm_world)
+      ! Mandatory entry: "class: latlon"
+      supports = ESMF_HConfigIsDefined(hconfig, keystring='class', _RC)
+      _RETURN_UNLESS(supports)
 
-      has_lat_range = ESMF_HConfigIsDefined(hconfig, keystring='lat_range', _RC)
-      has_pole = ESMF_HConfigIsDefined(hconfig, keystring='pole', _RC)
-      _RETURN_UNLESS(has_lat_range .neqv. has_pole)
-      supports = .true.
+      geom_class = ESMF_HConfigAsString(hconfig, keyString='class', _RC)
+      supports = (geom_class == 'latlon')
+      _RETURN_UNLESS(supports)
+
+      supports = lon_axis%supports(hconfig, _RC)
+      _RETURN_UNLESS(supports)
+
+      supports = lat_axis%supports(hconfig, _RC)
+      _RETURN_UNLESS(supports)
 
       _RETURN(_SUCCESS)
-   end function supports_hconfig
+      _UNUSED_DUMMY(this)
+   end function supports_hconfig_
 
 end submodule supports_hconfig_smod
-
