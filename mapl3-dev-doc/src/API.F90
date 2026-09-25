df623ac2adda7f052ec6c272dce407db37740433
mathomp4 <fortran@gmail.com>
2026-09-25 17:23:51 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@067c1a63eec6b16aa3933fd31c25f97a6f7eb65b 🚀

--- mapl3-dev-doc/src/API.F90
+++ mapl3-dev-doc/src/API.F90
@@ -1,7 +1,10 @@
-module mapl_superstructure_api
+module mapl_infrastructure_api
+   use mapl_esmf_api
+   use mapl_field_api
+   use mapl_field_bundle_api
+   use mapl_geom_api
+   use mapl_vertical_grid_api
+   use mapl_regridder_mgr_api
+   use mapl_geomio_api
 
-   use mapl_generic_api
-   use mapl_state_api
-   use mapl_component_api
-
-end module mapl_superstructure_api
+end module mapl_infrastructure_api
